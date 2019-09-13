82d8b9e3758159def9cb8418206056920fdb5a6e
Dave Syer <david_syer@hotmail.com>
2019-09-13 11:00:40 +0000

    Switch vanilla-orm to @PersistenceContext style

--- samples/vanilla-orm/src/main/java/app/main/SampleApplication.java
+++ samples/vanilla-orm/src/main/java/app/main/SampleApplication.java
@@ -1,57 +1,59 @@
 package app.main;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
-import app.main.model.Foo;
-import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Schedulers;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.RouterFunction;
 
-import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
-import static org.springframework.web.reactive.function.server.RouterFunctions.route;
-import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+import app.main.model.Foo;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 @SpringBootApplication(proxyBeanMethods = false)
 public class SampleApplication {
 
-	private EntityManagerFactory entities;
-
-	public SampleApplication(EntityManagerFactory entities) {
-		this.entities = entities;
-	}
-
-	@Bean
-	public CommandLineRunner runner() {
-		return args -> {
-			EntityManager manager = entities.createEntityManager();
-			EntityTransaction transaction = manager.getTransaction();
-			transaction.begin();
-			Foo foo = manager.find(Foo.class, 1L);
-			if (foo == null) {
-				manager.persist(new Foo("Hello"));
-			}
-			transaction.commit();
-		};
-	}
+	@PersistenceContext
+	private EntityManager manager;
 
 	@Bean
 	public RouterFunction<?> userEndpoints() {
 		return route(GET("/"),
 				request -> ok().body(Mono
 						.fromCallable(
-								() -> entities.createEntityManager().find(Foo.class, 1L))
+								() -> manager.find(Foo.class, 1L))
 						.subscribeOn(Schedulers.elastic()), Foo.class));
 	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleApplication.class, args);
+	}
+
+}
+
+@Transactional
+@Component
+class Bootstrap implements CommandLineRunner {
+
+	@PersistenceContext
+	private EntityManager manager;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Foo foo = manager.find(Foo.class, 1L);
+		if (foo == null) {
+			manager.persist(new Foo("Hello"));
+		}
 	}
 
 }
