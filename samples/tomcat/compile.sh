a18b098acb634fa93969a14fd5b00f41496702a0
Andy Clement <aclement@pivotal.io>
2019-09-18 12:20:41 -0700

    tomcat sample working

--- samples/tomcat/compile.sh
+++ samples/tomcat/compile.sh
@@ -18,9 +18,14 @@ export CP=.:$LIBPATH
 export CP=$CP:../../../../../target/spring-graal-feature-0.6.0.BUILD-SNAPSHOT.jar
 
 printf "\n\nCompile\n"
+
 native-image \
   -Dio.netty.noUnsafe=true \
   --no-server \
+  --initialize-at-build-time=org.eclipse.jdt,org.apache.el.parser.SimpleNode,javax.servlet.jsp.JspFactory,org.apache.jasper.servlet.JasperInitializer,org.apache.jasper.runtime.JspFactoryImpl -H:+JNI \
+  -H:EnableURLProtocols=http,https,jar \
+  -H:ReflectionConfigurationFiles=../../../tomcat-reflection.json -H:ResourceConfigurationFiles=../../../tomcat-resource.json -H:JNIConfigurationFiles=../../../tomcat-jni.json \
+  --enable-https \
   -H:+TraceClassInitialization \
   -H:IncludeResourceBundles=javax.servlet.http.LocalStrings \
   -H:Name=tc \
@@ -28,9 +33,25 @@ native-image \
   --no-fallback \
   --allow-incomplete-classpath \
   --report-unsupported-elements-at-runtime \
+  -Dsun.rmi.transport.tcp.maxConnectionThreads=0 \
+  -DremoveUnusedAutoconfig=true \
   -cp $CP com.example.tomcat.TomcatApplication
 
+#native-image \
+#  -Dio.netty.noUnsafe=true \
+#  --enable-https \
+#  --no-server \
+#  -H:+TraceClassInitialization \
+#  -H:IncludeResourceBundles=javax.servlet.http.LocalStrings \
+#  -H:Name=tc \
+#  -H:+ReportExceptionStackTraces \
+#  --no-fallback \
+#  --allow-incomplete-classpath \
+#  --report-unsupported-elements-at-runtime \
+#-Dsun.rmi.transport.tcp.maxConnectionThreads=0 \
 #  -DremoveUnusedAutoconfig=true \
+#  -cp $CP com.example.tomcat.TomcatApplication
+#
 mv tc ../../..
 
 printf "\n\nCompiled app (tc)\n"
