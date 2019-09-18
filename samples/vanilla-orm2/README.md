d95b626317bc0e89cf398bdf7f60e2d69d5e46d7
Andy Clement <aclement@pivotal.io>
2019-09-18 07:51:26 -0700

    keep old vanilla-orm sample as vanilla-orm2

---
+++ samples/vanilla-orm2/README.md
@@ -0,0 +1,2 @@
+
+Copy of Daves app from git@github.com:dsyer/vanilla-orm.git
---
+++ samples/vanilla-orm2/compile.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+../../mvnw -DskipTests clean package
+
+export JAR="orm-0.0.1.BUILD-SNAPSHOT.jar"
+rm orm
+printf "Unpacking $JAR"
+rm -rf unpack
+mkdir unpack
+cd unpack
+jar -xvf ../target/$JAR >/dev/null 2>&1
+cp -R META-INF BOOT-INF/classes
+
+cd BOOT-INF/classes
+export LIBPATH=`find ../../BOOT-INF/lib | tr '\n' ':'`
+export CP=.:$LIBPATH
+
+# This would run it here... (as an exploded jar)
+#java -classpath $CP com.example.demo.DemoApplication
+
+# Our feature being on the classpath is what triggers it
+export CP=$CP:../../../../../target/spring-graal-feature-0.6.0.BUILD-SNAPSHOT.jar
+
+printf "\n\nCompile\n"
+native-image \
+  -Dio.netty.noUnsafe=true \
+  --no-server \
+  -H:Name=orm \
+  -H:+ReportExceptionStackTraces \
+  --no-fallback \
+  --allow-incomplete-classpath \
+  --report-unsupported-elements-at-runtime \
+  -DremoveUnusedAutoconfig=true \
+  -cp $CP app.main.SampleApplication
+
+  #--debug-attach \
+mv orm ../../..
+
+printf "\n\nCompiled app (demo)\n"
+cd ../../..
+time ./orm -Dhibernate.dialect=org.hibernate.dialect.H2Dialect
+
