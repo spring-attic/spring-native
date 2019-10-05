98e2ccd06449c4547e40eb85a2936af54bfc9368
Andy Clement <aclement@pivotal.io>
2019-10-04 17:32:16 -0700

    polish

--- spring-graal-native-image-samples/webflux-netty/src/main/java/com/example/demo/DemoApplication.java
+++ spring-graal-native-image-samples/webflux-netty/src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,5 @@
 package com.example.demo;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
