e00db959c0247b5af692fc7b6d69df929d25a7d1
Sebastien Deleuze <sdeleuze@pivotal.io>
2019-10-06 23:55:22 -0500

    Rename the project to spring-graal-native

--- spring-graal-native-samples/vanilla-rabbit/compile.sh
+++ spring-graal-native-samples/vanilla-rabbit/compile.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-../../mvnw clean install
+../../mvnw -DskipTests clean package
 
-export JAR="commandlinerunner-0.0.1-SNAPSHOT.jar"
-rm -f clr
+export JAR="vanilla-rabbit-0.0.1.BUILD-SNAPSHOT.jar"
+rm rabbit
 printf "Unpacking $JAR"
 rm -rf unpack
 mkdir unpack
@@ -14,27 +14,28 @@ cd BOOT-INF/classes
 export LIBPATH=`find ../../BOOT-INF/lib | tr '\n' ':'`
 export CP=.:$LIBPATH
 
+# This would run it here... (as an exploded jar)
+#java -classpath $CP app.main.SampleApplication
+
 # Our feature being on the classpath is what triggers it
-export CP=$CP:../../../../../spring-graal-native-image-feature/target/spring-graal-native-image-feature-0.6.0.BUILD-SNAPSHOT.jar
+export CP=$CP:../../../../../spring-graal-native-feature/target/spring-graal-native-feature-0.6.0.BUILD-SNAPSHOT.jar
 
 printf "\n\nCompile\n"
 native-image \
   -Dio.netty.noUnsafe=true \
   --no-server \
-  -H:Name=clr \
+  -H:Name=rabbit\
   -H:+ReportExceptionStackTraces \
   --no-fallback \
   --allow-incomplete-classpath \
   --report-unsupported-elements-at-runtime \
   -DremoveUnusedAutoconfig=true \
-  -cp $CP com.example.commandlinerunner.CommandlinerunnerApplication
+  -cp $CP app.main.SampleApplication
 
-mv clr ../../..
+  #--debug-attach \
+mv rabbit ../../..
 
-printf "\n\nJava exploded jar\n"
-time java -classpath $CP com.example.commandlinerunner.CommandlinerunnerApplication
-
-printf "\n\nCompiled app (clr)\n"
+printf "\n\nCompiled app (demo)\n"
 cd ../../..
-time ./clr
+time ./rabbit
 
