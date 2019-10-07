6934c4d4f101ea0cf50705f1a805c4003b71e05d
Sebastien Deleuze <sdeleuze@pivotal.io>
2019-10-06 23:30:33 -0500

    Add kotlin-webmvc sample

---
+++ spring-graal-native-image-samples/kotlin-webmvc/compile.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+../../mvnw -DskipTests clean package
+
+export JAR="kotlin-webmvc-0.0.1-SNAPSHOT.jar"
+rm -f tc
+printf "Unpacking $JAR"
+rm -rf unpack
+mkdir unpack
+cd unpack
+jar -xvf ../target/$JAR >/dev/null 2>&1
+cp -R META-INF BOOT-INF/classes
+
+cd BOOT-INF/classes
+export LIBPATH=`find ../../BOOT-INF/lib | tr '\n' ':'`
+export CP=.:$LIBPATH
+
+# Our feature being on the classpath is what triggers it
+export CP=$CP:../../../../../spring-graal-native-image-feature/target/spring-graal-native-image-feature-0.6.0.BUILD-SNAPSHOT.jar
+
+printf "\n\nCompile\n"
+
+native-image \
+  --no-server \
+  --initialize-at-build-time=org.eclipse.jdt,org.apache.el.parser.SimpleNode,javax.servlet.jsp.JspFactory,org.apache.jasper.servlet.JasperInitializer,org.apache.jasper.runtime.JspFactoryImpl -H:+JNI \
+  -H:EnableURLProtocols=http,https,jar \
+  -H:ReflectionConfigurationFiles=../../../tomcat-reflection.json -H:ResourceConfigurationFiles=../../../tomcat-resource.json -H:JNIConfigurationFiles=../../../tomcat-jni.json \
+  --enable-https \
+  -H:+TraceClassInitialization \
+  -H:IncludeResourceBundles=javax.servlet.http.LocalStrings \
+  -H:Name=kotlin-webmvc \
+  -H:+ReportExceptionStackTraces \
+  --no-fallback \
+  --allow-incomplete-classpath \
+  --report-unsupported-elements-at-runtime \
+  -Dsun.rmi.transport.tcp.maxConnectionThreads=0 \
+  -DremoveUnusedAutoconfig=true \
+  -cp $CP com.example.demo.DemoApplicationKt
+
+mv kotlin-webmvc ../../..
+
+printf "\n\nCompiled app (kotlin-webmvc)\n"
+cd ../../..
+time ./kotlin-webmvc
+
---
+++ spring-graal-native-image-samples/kotlin-webmvc/src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -0,0 +1,21 @@
+package com.example.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication(proxyBeanMethods = false)
+class KotlinCoroutinesApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinCoroutinesApplication>(*args)
+}
+
+@RestController
+class Foo {
+	@GetMapping("/")
+	fun greet(): String {
+		return "hi!"
+	}
+}
