cf6f2594f2cca01146a0592e9877fd47cc28709d
Andy Clement <aclement@pivotal.io>
2019-10-25 10:40:52 -0700

    Upgrade to Boot 2.2.0.RELEASE

--- spring-graal-native-samples/commandlinerunner-maven/src/main/java/com/example/commandlinerunner/CommandlinerunnerApplication.java
+++ spring-graal-native-samples/commandlinerunner-maven/src/main/java/com/example/commandlinerunner/CommandlinerunnerApplication.java
@@ -7,29 +7,9 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(proxyBeanMethods=false)
 public class CommandlinerunnerApplication {
-
-//	  private static final Logger LOGGER;
-//	  static {
-//	    try {
-//			LogManager.getLogManager().readConfiguration(CommandlinerunnerApplication.class.getResourceAsStream("logging.properties"));
-//		} catch (SecurityException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//	    LOGGER = Logger.getLogger(CommandlinerunnerApplication.class.getName());
-//	  }
-	  
+ 
 	public static void main(String[] args) {
-//		LOGGER.fine("foo");
 		SpringApplication.run(CommandlinerunnerApplication.class, args);
 	}
-	
-//	@Bean
-//	public CommandLineRunner clr1() {
-//		return new CLR();
-//	}
 
 }
