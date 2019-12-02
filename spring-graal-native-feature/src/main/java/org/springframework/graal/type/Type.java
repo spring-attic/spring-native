5d8c93c7d455683ed89b7c2d2fda2e50118e1ff8
Andy Clement <aclement@pivotal.io>
2019-12-02 10:31:31 -0800

    Further reduce reflect.json

--- spring-graal-native-feature/src/main/java/org/springframework/graal/type/Type.java
+++ spring-graal-native-feature/src/main/java/org/springframework/graal/type/Type.java
@@ -1114,6 +1114,34 @@ public class Type {
 				 	"org.springframework.boot.context.properties.ConfigurationPropertiesBindingPostProcessorRegistrar:REGISTRAR"}
 				));
 		
+		proposedHints.put("Lorg/springframework/boot/autoconfigure/web/servlet/DispatcherServletAutoConfiguration;", 
+				new CompilationHint(false,false, new String[] {
+						// All from DispatcherServlet.properties
+//org.springframework.web.servlet.LocaleResolver=
+						"org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver",
+//org.springframework.web.servlet.ThemeResolver=
+						"org.springframework.web.servlet.theme.FixedThemeResolver",
+//org.springframework.web.servlet.HandlerMapping=
+						"org.springframework.web.servlet.handler.BeanNameUrlHandlerMapping",
+						"org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping",
+						"org.springframework.web.servlet.function.support.RouterFunctionMapping",
+//org.springframework.web.servlet.HandlerAdapter=
+						"org.springframework.web.servlet.mvc.HttpRequestHandlerAdapter",
+						"org.springframework.web.servlet.mvc.SimpleControllerHandlerAdapter",
+						"org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter",
+						"org.springframework.web.servlet.function.support.HandlerFunctionAdapter",
+//org.springframework.web.servlet.HandlerExceptionResolver=
+						"org.springframework.web.servlet.mvc.method.annotation.ExceptionHandlerExceptionResolver",
+						"org.springframework.web.servlet.mvc.annotation.ResponseStatusExceptionResolver",
+						"org.springframework.web.servlet.mvc.support.DefaultHandlerExceptionResolver",
+//org.springframework.web.servlet.RequestToViewNameTranslator=
+						"org.springframework.web.servlet.view.DefaultRequestToViewNameTranslator",
+//org.springframework.web.servlet.ViewResolver=
+						"org.springframework.web.servlet.view.InternalResourceViewResolver",
+//org.springframework.web.servlet.FlashMapManager=
+						"org.springframework.web.servlet.support.SessionFlashMapManager"
+				}));
+		
 				
 		// Not quite right... this is a superclass of a selector we've already added...
 		proposedHints.put(AdviceModeImportSelector,
