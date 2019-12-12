d9e64af6e3b01b7080ddf56fd96b24e3283ab0c6
Andy Clement <aclement@pivotal.io>
2019-12-12 08:45:51 -0800

    add script to just run native-image step

---
+++ spring-graal-native-samples/spring-petclinic-jpa/ni.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+cd unpack/BOOT-INF/classes
+export LIBPATH=`find ../../BOOT-INF/lib | tr '\n' ':'`
+export CP=.:$LIBPATH
+
+# Our feature being on the classpath is what triggers it
+export CP=$CP:../../../../../spring-graal-native-feature/target/spring-graal-native-feature-0.6.0.BUILD-SNAPSHOT.jar
+
+printf "\n\nCompile\n"
+
+#-H:+PrintUniverse \
+native-image \
+  -DavoidLogback=true \
+  -Ddebug=true \
+  -Dio.netty.noUnsafe=true \
+  --no-server \
+  -H:Name=petclinic \
+  -H:+ReportExceptionStackTraces \
+  -H:+TraceClassInitialization \
+  -H:IncludeResourceBundles=messages/messages \
+  --no-fallback \
+  --allow-incomplete-classpath \
+  --report-unsupported-elements-at-runtime \
+  -cp $CP \
+  --initialize-at-build-time=org.springframework.boot.validation.MessageInterpolatorFactory,org.hsqldb.jdbc.JDBCDriver,com.mysql.cj.jdbc.Driver,org.springframework.samples.petclinic.owner.PetRepository,org.springframework.samples.petclinic.owner.OwnerRepository,org.springframework.samples.petclinic.visit.VisitRepository,org.springframework.samples.petclinic.vet.VetRepository,org.springframework.samples.petclinic.owner.Pet,org.springframework.samples.petclinic.owner.Owner,org.springframework.samples.petclinic.model.NamedEntity,org.springframework.samples.petclinic.model.Person,org.springframework.samples.petclinic.model.BaseEntity,org.springframework.samples.petclinic.model.NamedEntity,org.springframework.samples.petclinic.visit.Visit,org.springframework.samples.petclinic.vet.Vet \
+  org.springframework.samples.petclinic.PetClinicApplication
+
+mv petclinic ../../..
