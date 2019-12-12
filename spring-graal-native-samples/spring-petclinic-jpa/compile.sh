62caf60829bfb192c6b5e17d6d306ab67b00262c
Andy Clement <aclement@pivotal.io>
2019-12-11 17:25:38 -0800

    petclinic sample compiling with graal master

--- spring-graal-native-samples/spring-petclinic-jpa/compile.sh
+++ spring-graal-native-samples/spring-petclinic-jpa/compile.sh
@@ -13,6 +13,7 @@ cp -R META-INF BOOT-INF/classes
 cd BOOT-INF/classes
 export LIBPATH=`find ../../BOOT-INF/lib | tr '\n' ':'`
 export CP=.:$LIBPATH
+#:/Users/aclement/gits/spring-graal-native/spring-graal-native-samples/spring-petclinic-jpa/graal
 
 # This would run it here... (as an exploded jar)
 #java -classpath $CP com.example.demo.DemoApplication
@@ -27,7 +28,13 @@ printf "\n\nCompile\n"
 #  --allow-incomplete-classpath --report-unsupported-elements-at-runtime -DremoveUnusedAutoconfig=true \
 #  -cp $CP org.springframework.samples.petclinic.PetClinicApplication
 
-native-image -DavoidLogback=true -Ddebug=true -Dio.netty.noUnsafe=true --no-server -H:Name=petclinic -H:+ReportExceptionStackTraces --no-fallback --allow-incomplete-classpath --report-unsupported-elements-at-runtime -cp $CP --initialize-at-build-time=org.springframework.boot.validation.MessageInterpolatorFactory,org.hsqldb.jdbc.JDBCDriver,com.mysql.cj.jdbc.Driver,org.springframework.samples.petclinic.owner.PetRepository,org.springframework.samples.petclinic.owner.OwnerRepository,org.springframework.samples.petclinic.visit.VisitRepository,org.springframework.samples.petclinic.vet.VetRepository,org.springframework.samples.petclinic.owner.Pet,org.springframework.samples.petclinic.owner.Owner,org.springframework.samples.petclinic.model.NamedEntity,org.springframework.samples.petclinic.model.Person,org.springframework.samples.petclinic.model.BaseEntity,org.springframework.samples.petclinic.model.NamedEntity,org.springframework.samples.petclinic.visit.Visit,org.springframework.samples.petclinic.vet.Vet org.springframework.samples.petclinic.PetClinicApplication
+#-H:+PrintUniverse \
+native-image -DavoidLogback=true -Ddebug=true -Dio.netty.noUnsafe=true --no-server \
+  -H:Name=petclinic -H:+ReportExceptionStackTraces -H:+TraceClassInitialization \
+-H:IncludeResourceBundles=messages/messages \
+  --no-fallback --allow-incomplete-classpath --report-unsupported-elements-at-runtime -cp $CP \
+  --initialize-at-build-time=org.springframework.boot.validation.MessageInterpolatorFactory,org.hsqldb.jdbc.JDBCDriver,com.mysql.cj.jdbc.Driver,org.springframework.samples.petclinic.owner.PetRepository,org.springframework.samples.petclinic.owner.OwnerRepository,org.springframework.samples.petclinic.visit.VisitRepository,org.springframework.samples.petclinic.vet.VetRepository,org.springframework.samples.petclinic.owner.Pet,org.springframework.samples.petclinic.owner.Owner,org.springframework.samples.petclinic.model.NamedEntity,org.springframework.samples.petclinic.model.Person,org.springframework.samples.petclinic.model.BaseEntity,org.springframework.samples.petclinic.model.NamedEntity,org.springframework.samples.petclinic.visit.Visit,org.springframework.samples.petclinic.vet.Vet \
+  org.springframework.samples.petclinic.PetClinicApplication
 
 
 mv petclinic ../../..
