33b6d7f706c9a39180083ed63c22f1e858474e96
Sébastien Deleuze <sdeleuze@pivotal.io>
2020-01-06 14:26:32 +0100

    Polish commandlinerunner scripts

--- spring-graal-native-samples/commandlinerunner/test.sh
+++ spring-graal-native-samples/commandlinerunner/test.sh
@@ -4,6 +4,12 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m'
 
+if [[ $1 != "-s" ]]; then  # enable silent mode to avoid the below 
+    BUILDTIME=`cat target/native-image/output.txt | grep "\[total\]" | sed 's/^.*\[total\]: *//' | tr -d -c 0-9\.`
+    BUILDTIME=`bc <<< "scale=1; ${BUILDTIME}/1024"`
+    echo "Image build time: ${BUILDTIME}s"
+fi
+
 EXECUTABLE=${PWD##*/}
 echo "Testing $EXECUTABLE output"
 
@@ -11,28 +17,24 @@ echo "Testing $EXECUTABLE output"
 PID=$!
 sleep 3
 
-if [[ $1 != "-s" ]]; then  # enable silent mode to avoid the below 
-    BUILDTIME=`cat target/native-image/output.txt | grep "\[total\]" | sed 's/^.*\[total\]: *//' | tr -d -c 0-9\.`
-    echo "Image build time: ${BUILDTIME}ms"
-    RSS=`ps -o rss ${PID} | tail -n1`
-    RSS=`bc <<< "scale=1; ${RSS}/1024"`
-    echo "RSS memory: ${RSS}M"
-    SIZE=`wc -c <"./target/${EXECUTABLE}"`/1024
-    SIZE=`bc <<< "scale=1; ${SIZE}/1024"`
-    echo "Image size: ${SIZE}M"
-    STARTUP=`cat target/native-image/test-output.txt | grep "JVM running for"`
-    REGEXP="Started .* in ([0-9\.]*) seconds \(JVM running for ([0-9\.]*)\).*$"
-    if [[ ${STARTUP} =~ ${REGEXP} ]]; then
-      STIME=${BASH_REMATCH[1]}
-      JTIME=${BASH_REMATCH[2]}
-      echo "Startup time: ${STIME} (JVM running for ${JTIME})"
-    fi
-fi
-
-
 if [[ `cat target/native-image/test-output.txt | grep "commandlinerunner running!"` ]]
 then
   printf "${GREEN}SUCCESS${NC}\n"
+  if [[ $1 != "-s" ]]; then  # enable silent mode to avoid the below 
+      RSS=`ps -o rss ${PID} | tail -n1`
+      RSS=`bc <<< "scale=1; ${RSS}/1024"`
+      echo "RSS memory: ${RSS}M"
+      SIZE=`wc -c <"./target/${EXECUTABLE}"`/1024
+      SIZE=`bc <<< "scale=1; ${SIZE}/1024"`
+      echo "Image size: ${SIZE}M"
+      STARTUP=`cat target/native-image/test-output.txt | grep "JVM running for"`
+      REGEXP="Started .* in ([0-9\.]*) seconds \(JVM running for ([0-9\.]*)\).*$"
+      if [[ ${STARTUP} =~ ${REGEXP} ]]; then
+        STIME=${BASH_REMATCH[1]}
+        JTIME=${BASH_REMATCH[2]}
+        echo "Startup time: ${JTIME}s"
+      fi
+  fi
   echo `date +%Y%m%d-%H%M`,$EXECUTABLE,$BUILDTIME,${RSS},${SIZE},${STIME},${JTIME}  > target/native-image/summary.csv
   kill ${PID}
   exit 0
