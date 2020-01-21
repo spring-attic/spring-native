5a2f64cf2ff3fbecd4916f64f1f3dce75b270684
Sébastien Deleuze <sdeleuze@pivotal.io>
2020-01-21 13:27:19 +0100

    Polish build script
    See gh-31

--- ci/build.sh
+++ ci/build.sh
@@ -5,7 +5,7 @@ set -e
 yum install -y unzip wget
 wget https://repo1.maven.org/maven2/org/apache/maven/apache-maven/3.5.3/apache-maven-3.5.3-bin.zip
 unzip apache-maven-3.5.3-bin.zip
-export PATH=$PATH:/apache-maven-3.5.3/bin
+export PATH=$PATH:`pwd`/apache-maven-3.5.3/bin
 
 gu install native-image
 cd spring-graal-native
