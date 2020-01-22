c2363b302e12ba3c599de7340946e0eeb431fd0c
Andy Clement <aclement@pivotal.io>
2020-01-22 13:06:09 -0800

    Add grpcurl to get vanilla-grpc working on CI

--- ci/build.sh
+++ ci/build.sh
@@ -3,7 +3,7 @@
 set -e
 
 npm install tty-table -g
-yum install -y unzip wget procps bc perl util-linux 
+yum install -y unzip wget procps bc perl util-linux grpcurl
 wget https://repo1.maven.org/maven2/org/apache/maven/apache-maven/3.5.3/apache-maven-3.5.3-bin.zip
 unzip apache-maven-3.5.3-bin.zip
 export PATH=$PATH:`pwd`/apache-maven-3.5.3/bin
