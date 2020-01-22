dd60714e6ea05bafbd3bf0e932145cfaf0aa694e
Andy Clement <aclement@pivotal.io>
2020-01-22 08:20:45 -0800

    more ci polish

--- build-key-samples.sh
+++ build-key-samples.sh
@@ -2,7 +2,7 @@
 
 echo "Graal: `native-image --version`" > samples-summary.csv
 echo "Date,Sample,Build Time (ms),Build Mem (GB),RSS Mem (M),Image Size (M),Startup Time (s),JVM Uptime (s)" >> samples-summary.csv
-for i in commandlinerunner webflux-netty springmvc-tomcat jafu jafu-webmvc vanilla-thymeleaf vanilla-grpc
+for i in commandlinerunner webflux-netty springmvc-tomcat jafu jafu-webmvc vanilla-thymeleaf vanilla-grpc vanilla-tx
 do
   (cd spring-graal-native-samples/$i && ./build.sh)
   if [ -f "spring-graal-native-samples/$i/target/native-image/summary.csv" ]; then
@@ -23,7 +23,7 @@ done
 
 head -1 samples-summary.csv
 if ! [ -x "$(command -v tty-table)" ]; then
-  tail +2 samples-summary.csv | perl -pe 's/((?<=,)|(?<=^)),/ ,/g;'  | column -t -s, 
+  tail -n +2 samples-summary.csv | perl -pe 's/((?<=,)|(?<=^)),/ ,/g;'  | column -t -s, 
 else
-  tail +2 samples-summary.csv | tty-table
+  tail -n +2 samples-summary.csv | tty-table
 fi
