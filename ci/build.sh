27f901b776f46f6667d2e3cdde27b760fcfb43d8
Andy Clement <aclement@pivotal.io>
2020-01-22 13:20:45 -0800

    remove use of go from build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -5,9 +5,6 @@ set -e
 npm install tty-table -g
 yum install -y unzip wget procps bc perl util-linux 
 
-go get github.com/fullstorydev/grpcurl
-go install github.com/fullstorydev/grpcurl/cmd/grpcurl
-
 wget https://repo1.maven.org/maven2/org/apache/maven/apache-maven/3.5.3/apache-maven-3.5.3-bin.zip
 unzip apache-maven-3.5.3-bin.zip
 export PATH=$PATH:`pwd`/apache-maven-3.5.3/bin
