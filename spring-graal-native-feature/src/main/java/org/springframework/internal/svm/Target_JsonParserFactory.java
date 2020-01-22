2b7a4b161c06cd3f5b783d3adf2616b8a56b0271
Sébastien Deleuze <sdeleuze@pivotal.io>
2020-01-22 17:43:00 +0100

    Make it possible to remove Snakeyaml
    Closes gh-28

---
+++ spring-graal-native-feature/src/main/java/org/springframework/internal/svm/Target_JsonParserFactory.java
@@ -0,0 +1,25 @@
+package org.springframework.internal.svm;
+
+import com.oracle.svm.core.annotate.Substitute;
+import com.oracle.svm.core.annotate.TargetClass;
+
+import org.springframework.boot.json.BasicJsonParser;
+import org.springframework.boot.json.GsonJsonParser;
+import org.springframework.boot.json.JacksonJsonParser;
+import org.springframework.boot.json.JsonParser;
+import org.springframework.util.ClassUtils;
+
+@TargetClass(className = "org.springframework.boot.json.JsonParserFactory", onlyWith = YamlSupportRemoved.class)
+public final class Target_JsonParserFactory {
+
+	@Substitute
+	public static JsonParser getJsonParser() {
+		if (ClassUtils.isPresent("com.fasterxml.jackson.databind.ObjectMapper", null)) {
+			return new JacksonJsonParser();
+		}
+		if (ClassUtils.isPresent("com.google.gson.Gson", null)) {
+			return new GsonJsonParser();
+		}
+		return new BasicJsonParser();
+	}
+}
--- spring-graal-native-samples/commandlinerunner/compile.sh
+++ spring-graal-native-samples/commandlinerunner/compile.sh
@@ -37,6 +37,7 @@ echo "Compiling $ARTIFACT with $GRAALVM_VERSION"
   --allow-incomplete-classpath \
   --report-unsupported-elements-at-runtime \
   -DremoveUnusedAutoconfig=true \
+  -DremoveYamlSupport=true \
   -cp $CP $MAINCLASS >> output.txt ; } 2>> output.txt
 
 if [[ -f $ARTIFACT ]]
