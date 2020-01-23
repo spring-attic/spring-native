5ac172cd4a534a9a8039fbbd582804ae8096ad49
Sébastien Deleuze <sdeleuze@pivotal.io>
2020-01-23 08:01:40 +0100

    Fix CI build script
    grpcurl and tty-table should work correctly now.

--- ci/build.sh
+++ ci/build.sh
@@ -3,11 +3,14 @@
 set -e
 
 npm install tty-table -g
-yum install -y unzip wget procps bc perl util-linux 
+yum install -y unzip wget procps bc perl util-linux golang git
+
+go get github.com/fullstorydev/grpcurl
+go install github.com/fullstorydev/grpcurl/cmd/grpcurl
 
 wget https://repo1.maven.org/maven2/org/apache/maven/apache-maven/3.5.3/apache-maven-3.5.3-bin.zip
 unzip apache-maven-3.5.3-bin.zip
-export PATH=$PATH:`pwd`/apache-maven-3.5.3/bin
+export PATH=$PATH:`pwd`/apache-maven-3.5.3/bin:/root/go/bin:/opt/graalvm-ce-java8-19.3.1/jre/languages/js/bin
 
 gu install native-image
 cd spring-graal-native
