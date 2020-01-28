b1f8e47b30774bf901412a2e3e1446b743301fd7
Sébastien Deleuze <sdeleuze@pivotal.io>
2020-01-28 11:56:46 +0100

    Use GraalVM 20.0.0 dev builds
    See gh-45

--- docker.sh
+++ docker.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -v `pwd`:/home -it --entrypoint /bin/bash springci/graalvm-ce-java8:20.1.0-dev_20200125-1203
+docker run -v `pwd`:/home -it --entrypoint /bin/bash springci/graalvm-ce-java8:20.0.0-dev_20200121-1000
