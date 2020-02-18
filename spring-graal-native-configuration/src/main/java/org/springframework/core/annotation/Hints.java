4dfe882e58c4bce590ff61aceb80e9f0a670d6fa
Andy Clement <aclement@pivotal.io>
2020-02-18 13:00:24 -0800

    Pushing more config from reflect.json

--- spring-graal-native-configuration/src/main/java/org/springframework/core/annotation/Hints.java
+++ spring-graal-native-configuration/src/main/java/org/springframework/core/annotation/Hints.java
@@ -5,5 +5,5 @@ import org.springframework.graal.extension.NativeImageConfiguration;
 import org.springframework.graal.extension.TypeInfo;
 import org.springframework.graal.type.AccessBits;
 
-@ConfigurationHint( typeInfos = {@TypeInfo(types= {Order.class },access = AccessBits.CLASS|AccessBits.PUBLIC_METHODS)})
+@ConfigurationHint( typeInfos = {@TypeInfo(types= {Order.class,AnnotationAttributes.class,AnnotationAttributes[].class },access = AccessBits.CLASS|AccessBits.PUBLIC_METHODS)})
 public class Hints implements NativeImageConfiguration { }
