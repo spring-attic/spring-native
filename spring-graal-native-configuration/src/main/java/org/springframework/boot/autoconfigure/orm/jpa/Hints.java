645f9be495a97c23ff30fd5e25df0c8075ef399a
Andy Clement <aclement@pivotal.io>
2020-02-19 11:38:38 -0800

    Pushing more config from reflect.json

--- spring-graal-native-configuration/src/main/java/org/springframework/boot/autoconfigure/orm/jpa/Hints.java
+++ spring-graal-native-configuration/src/main/java/org/springframework/boot/autoconfigure/orm/jpa/Hints.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.boot.autoconfigure.orm.jpa;
 
 import java.util.EventListener;
@@ -66,6 +81,7 @@ import org.springframework.boot.orm.jpa.hibernate.SpringPhysicalNamingStrategy;
 import org.springframework.graal.extension.ConfigurationHint;
 import org.springframework.graal.extension.NativeImageConfiguration;
 import org.springframework.graal.extension.TypeInfo;
+import org.springframework.orm.jpa.support.PersistenceAnnotationBeanPostProcessor;
 
 import com.zaxxer.hikari.util.ConcurrentBag.IConcurrentBagEntry;
 
@@ -108,7 +124,8 @@ proposedHints.put("Lorg/springframework/boot/autoconfigure/orm/jpa/HibernateJpaA
 				AutoFlushEventListener[].class,PersistEventListener[].class,EventType[].class,
 				ClearEventListener[].class,MergeEventListener[].class,PostCollectionRecreateEventListener[].class,
 				PostCollectionRemoveEventListener[].class,PostCollectionUpdateEventListener[].class,PostDeleteEventListener[].class,
-				
+			
+				PersistenceAnnotationBeanPostProcessor.class, 
 // Are these needed to?
 //				{
 //					"name": "org.hibernate.cache.spi.access.NaturalIdDataAccess",
