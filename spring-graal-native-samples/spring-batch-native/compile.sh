e9cd6e680de5eb9430003573a3a147a9c236da79
Andy Clement <aclement@pivotal.io>
2020-02-28 14:20:57 -0800

    wip on batch sample - for graal issue

---
+++ spring-graal-native-samples/spring-batch-native/compile.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+ARTIFACT=spring-batch-native
+MAINCLASS=org.springframework.samples.springbatchnative.SpringBatchNativeApplication
+VERSION=0.0.1-BUILD-SNAPSHOT
+FEATURE=../../../../spring-graal-native-feature/target/spring-graal-native-feature-0.6.0.BUILD-SNAPSHOT.jar
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+rm -rf target
+mkdir -p target/native-image
+
+echo "Packaging $ARTIFACT with Maven"
+../../mvnw -DskipTests package > target/native-image/output.txt
+
+JAR="$ARTIFACT-$VERSION.jar"
+rm -f $ARTIFACT
+echo "Unpacking $JAR"
+cd target/native-image
+jar -xvf ../$JAR >/dev/null 2>&1
+cp -R META-INF BOOT-INF/classes
+
+LIBPATH=`find BOOT-INF/lib | tr '\n' ':'`
+CP=BOOT-INF/classes:$LIBPATH:$FEATURE
+
+echo "Classpath: $CP"
+pwd
+
+GRAALVM_VERSION=`native-image --version`
+echo "Compiling $ARTIFACT with $GRAALVM_VERSION"
+time native-image \
+  --no-server \
+  --no-fallback \
+  -DavoidLogback=true \
+  -Ddebug=true \
+  -Dio.netty.noUnsafe=true \
+  -H:Name=$ARTIFACT \
+  -H:+ReportExceptionStackTraces \
+  --allow-incomplete-classpath \
+  -H:+TraceClassInitialization \
+  --report-unsupported-elements-at-runtime \
+  --initialize-at-build-time=com.mysql.cj.jdbc.Driver org.springframework.samples.springbatchnative.SpringBatchNativeApplication
+  -cp $CP $MAINCLASS >> output.txt
+
+
+#if [[ -f $ARTIFACT ]]
+#then
+#  printf "${GREEN}SUCCESS${NC}\n"
+#  mv ./$ARTIFACT ..
+#  exit 0
+#else
+#  printf "${RED}FAILURE${NC}: an error occurred when compiling the native-image.\n"
+#  exit 1
+#fi
+#
---
+++ spring-graal-native-samples/spring-batch-native/src/test/java/org/springframework/samples/springbatchnative/SpringBatchNativeApplicationTests.java
@@ -0,0 +1,14 @@
+package org.springframework.samples.springbatchnative;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchNativeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
