efdbf0a3888e81dd99037a652f78e52a2e73caca
Andy Clement <aclement@pivotal.io>
2020-03-04 08:45:33 -0800

    Ensure verify steps curl the actual app to check it is working

--- spring-graal-native-samples/vanilla-jpa/verify.sh
+++ spring-graal-native-samples/vanilla-jpa/verify.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 if [[ ! `cat target/native-image/test-output.txt | grep -E "Application run failed|No suitable logging system located"` ]]; then
+RESPONSE=`curl -s localhost:8080/`
+if [[ "$RESPONSE" == '{"value":"Hello"}' ]]; then
   exit 0
+else
+  exit 1
+fi
 else
   exit 1
 fi
