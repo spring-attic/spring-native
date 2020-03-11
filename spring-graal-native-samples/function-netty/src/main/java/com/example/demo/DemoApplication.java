f444adfe4d6bd48c8ecd405c045140fdfa28e4b6
Dave Syer <dsyer@pivotal.io>
2020-03-11 16:02:57 +0000

    Add function sample

---
+++ spring-graal-native-samples/function-netty/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(proxyBeanMethods = false)
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
