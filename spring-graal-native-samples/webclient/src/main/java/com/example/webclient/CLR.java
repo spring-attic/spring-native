44570428026c803de4f2c2c8b4183849dbb52608
Dave Syer <dsyer@pivotal.io>
2020-03-25 11:04:49 +0000

    Add WebClient sample

---
+++ spring-graal-native-samples/webclient/src/main/java/com/example/webclient/CLR.java
@@ -0,0 +1,21 @@
+package com.example.webclient;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
+public class CLR implements CommandLineRunner {
+
+	private WebClient client;
+
+	public CLR(WebClient.Builder builder) {
+		this.client = builder.baseUrl("http://example.com").build();
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(this.client.get().uri("/").exchange().block().bodyToMono(String.class).block());
+	}
+	
+}
