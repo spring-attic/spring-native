b57407ca459c196ba837aef5591432e48697da93
Andy Clement <aclement@pivotal.io>
2020-03-25 15:51:58 -0700

    Allow samples to temporarily be .ignore'd from build-samples.sh run

--- build-samples.sh
+++ build-samples.sh
@@ -4,12 +4,12 @@ echo "Graal: `native-image --version`" > samples-summary.csv
 echo "Date,Sample,Build Time (s),Build Mem (GB),RSS Mem (M),Image Size (M),Startup Time (s),JVM Uptime (s)" >> samples-summary.csv
 for d in $(find spring-graal-native-samples -maxdepth 2 -type d)
 do
-  if [[ -f "$d/build.sh" ]]; then
+  if [[ -f "$d/build.sh" && ! -f "$d/.ignore" ]]; then
     (cd "$d" && ./build.sh)
     if [ -f "$d/target/native-image/summary.csv" ]; then
       cat $d/target/native-image/summary.csv >> samples-summary.csv
     else
-      echo `date +%Y%m%d-%H%M`,`basename $d`,ERROR,-,,,, >> samples-summary.csv
+     echo `date +%Y%m%d-%H%M`,`basename $d`,ERROR,-,,,, >> samples-summary.csv
     fi
   fi
 done
