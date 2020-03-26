9401ef85011d76e3bd68f7c79b051efbd65007fb
Dave Syer <dsyer@pivotal.io>
2020-03-26 09:23:43 +0000

    Fix verify.sh script and add debug logging

--- spring-graal-native-samples/function-aws/build.sh
+++ spring-graal-native-samples/function-aws/build.sh
@@ -4,4 +4,8 @@ BLUE='\033[0;34m'
 NC='\033[0m'
 
 printf "=== ${BLUE}Building %s sample${NC} ===\n" "${PWD##*/}"
+
+export SPRING_CLOUD_FUNCTION_WEB_EXPORT_SOURCE_URL=http://localhost:8000/home
+export SPRING_CLOUD_FUNCTION_WEB_EXPORT_SINK_URL=http://localhost:8000/echo
+
 ./compile.sh && ${PWD%/*samples/*}/scripts/test.sh
