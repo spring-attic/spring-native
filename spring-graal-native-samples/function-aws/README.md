c7fdfeafa25ea8a4443f9987b69b93d405d165b5
Dave Syer <dsyer@pivotal.io>
2020-03-27 08:45:48 +0000

    Add packaging for AWS
    $ build.sh
    $ mvn package -P native
    
    builds a native-zip ZIP file in target.

---
+++ spring-graal-native-samples/function-aws/README.md
@@ -0,0 +1,6 @@
+```
+$ build.sh
+$ mvn package -P native
+```
+
+builds a native-zip ZIP file in target. Upload it to AWS and set the handler to "foobar".
--- spring-graal-native-samples/function-aws/src/main/java/com/example/demo/DemoApplication.java
+++ spring-graal-native-samples/function-aws/src/main/java/com/example/demo/DemoApplication.java
@@ -1,40 +1,23 @@
 package com.example.demo;
 
-import java.util.Arrays;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.function.context.FunctionRegistration;
 import org.springframework.cloud.function.context.FunctionType;
 import org.springframework.cloud.function.context.FunctionalSpringApplication;
-import org.springframework.cloud.function.web.source.ExporterProperties;
 import org.springframework.context.ApplicationContextInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.core.env.PropertySource;
 
 @SpringBootApplication(proxyBeanMethods = false)
 public class DemoApplication implements ApplicationContextInitializer<GenericApplicationContext> {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = FunctionalSpringApplication.run(DemoApplication.class, args);
-		System.err.println(
-			"Beans: " + Arrays.asList(context.getBeanDefinitionNames())
-		);
-		for (PropertySource<?> source : context.getEnvironment().getPropertySources()) {
-		System.err.println(
-			"Environment: " + source.getName() + ", " + source.getSource()
-		);
-	}
-		System.err.println(
-			"URL: " + context.getBean(ExporterProperties.class).getSource().getUrl()
-		);
+		FunctionalSpringApplication.run(DemoApplication.class, args);
 	}
 
 	@Override
 	public void initialize(GenericApplicationContext context) {
 		context.registerBean("foobar", FunctionRegistration.class,
-				() -> new FunctionRegistration<>(new Foobar()).type(
-						FunctionType.from(String.class).to(String.class)));
+				() -> new FunctionRegistration<>(new Foobar()).type(FunctionType.from(String.class).to(String.class)));
 	}
 
 }
