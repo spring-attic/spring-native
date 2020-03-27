77c3c6bad96942e5920ba961c993171ac0fc13c9
Sébastien Deleuze <sdeleuze@pivotal.io>
2020-03-27 10:17:04 +0100

    Fix deploy.sh

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -3,4 +3,4 @@
 set -e
 
 cd spring-graal-native
-./mvnw deploy -Dartifactory.username=$ARTIFACTORY_USERNAME -Dartifactory.password=$ARTIFACTORY_PASSWORD publish
+./mvnw deploy -Dartifactory.username=$ARTIFACTORY_USERNAME -Dartifactory.password=$ARTIFACTORY_PASSWORD
