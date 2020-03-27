2c4aa7fecc41046965631635e9aac767f2bf863a
Dave Syer <dsyer@pivotal.io>
2020-03-27 11:05:23 +0000

    Re-organize so that AWS sample reports correct memory sizes

--- spring-graal-native-samples/function-aws/build.sh
+++ spring-graal-native-samples/function-aws/build.sh
@@ -5,4 +5,13 @@ NC='\033[0m'
 
 printf "=== ${BLUE}Building %s sample${NC} ===\n" "${PWD##*/}"
 
-./compile.sh && ${PWD%/*samples/*}/scripts/test.sh --spring.cloud.function.web.export.source.url=http://localhost:8000/home --spring.cloud.function.web.export.sink.url=http://localhost:8000/echo
+./compile.sh || exit 1
+
+JARDIR=target/native-image
+java -cp $JARDIR/BOOT-INF/lib/*:$JARDIR/BOOT-INF/classes:$JARDIR:target/test-classes com.example.test.TestServer &
+SPID=$!
+sleep 5
+
+${PWD%/*samples/*}/scripts/test.sh --spring.cloud.function.web.export.source.url=http://localhost:8000/home --spring.cloud.function.web.export.sink.url=http://localhost:8000/echo
+
+kill $SPID
