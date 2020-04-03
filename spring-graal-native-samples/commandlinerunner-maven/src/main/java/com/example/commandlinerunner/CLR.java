7101f7d1b83f9c770f461ec4dc688d0eede7806a
Sébastien Deleuze <sdeleuze@pivotal.io>
2020-04-03 16:16:00 +0200

    Polish commandlinerunner-maven

--- spring-graal-native-samples/commandlinerunner-maven/src/main/java/com/example/commandlinerunner/CLR.java
+++ spring-graal-native-samples/commandlinerunner-maven/src/main/java/com/example/commandlinerunner/CLR.java
@@ -8,7 +8,7 @@ public class CLR implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("CLR running!");
+		System.out.println("commandlinerunner running!");
 	}
 	
 }
