95f5dcfbe97c694f171c9b9c65b8228d7d7f89f3
Sébastien Deleuze <sdeleuze@pivotal.io>
2020-04-06 15:06:47 +0200

    Make commandlinerunner-maven compatible with build-samples.sh

--- spring-graal-native-samples/commandlinerunner-maven/build.sh
+++ spring-graal-native-samples/commandlinerunner-maven/build.sh
@@ -1,1 +1,7 @@
-mvn -Pgraal package
+#!/usr/bin/env bash
+
+BLUE='\033[0;34m'
+NC='\033[0m'
+
+printf "=== ${BLUE}Building %s sample${NC} ===\n" "${PWD##*/}"
+./compile.sh && ${PWD%/*samples/*}/scripts/test.sh
