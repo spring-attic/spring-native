c7fcc71500ec4130b1e5ed8e0e4860271a07d91b
Sébastien Deleuze <sdeleuze@pivotal.io>
2020-04-09 17:15:57 +0200

    Remove wip-agent directories and related files
    They will be fixed are part of 0.7 milestone, see related
    gh-78 issue.

--- spring-graal-native-samples/webflux-netty/wip-agent/4compile.sh
+++
@@ -1,9 +0,0 @@
-native-image \
-  --no-server \
-  -Djava.awt.headless=true \
-  --no-fallback \
-  -H:+TraceClassInitialization \
-  -H:+ReportExceptionStackTraces \
-  -H:Name=wn \
-  -cp .:$CP:graal \
-  com.example.demo.DemoApplication
