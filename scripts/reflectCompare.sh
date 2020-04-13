43860275ac257da02fa0f1e1b3749dd4040efc0c
Sébastien Deleuze <sdeleuze@pivotal.io>
2020-04-13 11:48:13 +0200

    Release spring-graal-native 0.6.1.RELEASE

--- scripts/reflectCompare.sh
+++ scripts/reflectCompare.sh
@@ -1,2 +1,2 @@
 # Creates a compact diff between two reflect json files
-java -classpath `dirname $0`/../spring-graal-native-feature/target/spring-graal-native-feature-0.6.1.BUILD-SNAPSHOT.jar org.springframework.graal.support.ReflectionJsonComparator $1 $2
+java -classpath `dirname $0`/../spring-graal-native-feature/target/spring-graal-native-feature-0.6.1.RELEASE.jar org.springframework.graal.support.ReflectionJsonComparator $1 $2
