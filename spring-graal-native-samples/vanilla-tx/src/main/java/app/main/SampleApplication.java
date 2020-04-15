f35bf56ebb4a80d606139796f2b5691cdf11d83f
Sébastien Deleuze <sdeleuze@pivotal.io>
2020-04-15 19:41:34 +0200

    Add tests to validate initialization.json
    This commit also includes a refactoring of vanilla-tx to
    use Spring MVC instead of WebFlux.
    
    Closes gh-85

--- spring-graal-native-samples/vanilla-tx/src/main/java/app/main/SampleApplication.java
+++ spring-graal-native-samples/vanilla-tx/src/main/java/app/main/SampleApplication.java
@@ -1,25 +1,22 @@
 package app.main;
 
 import app.main.model.Foo;
-import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Schedulers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.servlet.function.RouterFunction;
 
-import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
-import static org.springframework.web.reactive.function.server.RouterFunctions.route;
-import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+import static org.springframework.web.servlet.function.RequestPredicates.GET;
+import static org.springframework.web.servlet.function.RouterFunctions.route;
+import static org.springframework.web.servlet.function.ServerResponse.ok;
 
 @SpringBootApplication(proxyBeanMethods = false)
 public class SampleApplication {
 
 	@Bean
 	public RouterFunction<?> userEndpoints(Finder<Foo> entities) {
-		return route(GET("/"), request -> ok()
-				.body(Mono.fromCallable(() -> entities.find(1L)).subscribeOn(Schedulers.elastic()), Foo.class));
+		return route(GET("/"), request -> ok().body(entities.find(1L)));
 	}
 
 	public static void main(String[] args) {
--- spring-graal-native-samples/vanilla-tx/src/test/java/app/main/SampleApplicationTests.java
+++ spring-graal-native-samples/vanilla-tx/src/test/java/app/main/SampleApplicationTests.java
@@ -23,11 +23,13 @@ import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWeb
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import static org.springframework.boot.test.context.SpringBootTest.*;
+
 /**
  * @author Dave Syer
  *
  */
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
 public class SampleApplicationTests {
 
