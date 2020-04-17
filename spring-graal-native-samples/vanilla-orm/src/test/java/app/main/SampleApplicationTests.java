73537ac043061911228e945c2786e481c120e2ee
Sébastien Deleuze <sdeleuze@pivotal.io>
2020-04-17 16:23:28 +0200

    Use Spring MVC instead of WebFlux in vanilla-orm

--- spring-graal-native-samples/vanilla-orm/src/test/java/app/main/SampleApplicationTests.java
+++ spring-graal-native-samples/vanilla-orm/src/test/java/app/main/SampleApplicationTests.java
@@ -16,33 +16,24 @@
 
 package app.main;
 
-import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.web.server.WebHandler;
 
 /**
  * @author Dave Syer
  *
  */
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
 public class SampleApplicationTests {
 
 	@Autowired
-	private WebHandler webHandler;
-
-	@Autowired
 	private WebTestClient client;
-
-	@Before
-	public void init() {
-		client = WebTestClient.bindToWebHandler(webHandler).build();
-	}
 
 	@Test
 	public void test() {
