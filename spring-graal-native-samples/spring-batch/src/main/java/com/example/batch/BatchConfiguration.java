b17f83df267f04f55e1f8153e4bcdfb37e1000da
Andy Clement <aclement@pivotal.io>
2020-04-27 16:43:26 -0700

    wip - batch sample first commit

---
+++ spring-graal-native-samples/spring-batch/src/main/java/com/example/batch/BatchConfiguration.java
@@ -0,0 +1,94 @@
+/**
+ * Copyright 2020-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.batch;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.ListableJobLocator;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.converter.JobParametersConverter;
+import org.springframework.batch.core.explore.JobExplorer;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.JobOperator;
+import org.springframework.batch.core.launch.support.SimpleJobOperator;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @author Michael Minella
+ */
+@EnableBatchProcessing
+@SpringBootApplication(proxyBeanMethods=false)
+public class BatchConfiguration {
+
+	@Autowired
+	private JobBuilderFactory jobBuilderFactory;
+
+	@Autowired
+	private StepBuilderFactory stepBuilderFactory;
+
+	@Bean
+	public Job job(Step step1) {
+		return this.jobBuilderFactory.get("job")
+				.start(step1)
+				.build();
+	}
+
+	@Bean
+	public Step step1() {
+		return this.stepBuilderFactory.get("step1")
+				.tasklet(new Tasklet() {
+					@Override
+					public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+						System.out.println(">> This was run in a Spring Batch app!");
+
+						return RepeatStatus.FINISHED;
+					}
+				}).build();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchConfiguration.class, args);
+	}
+	
+
+	@Bean
+	@ConditionalOnMissingBean(JobOperator.class)
+	public JobOperator jobOperator(ObjectProvider<JobParametersConverter> jobParametersConverter,
+			JobExplorer jobExplorer, JobLauncher jobLauncher, ListableJobLocator jobRegistry,
+			JobRepository jobRepository) throws Exception {
+		System.out.println("FOOBAR");
+		SimpleJobOperator factory = new SimpleJobOperator();
+		factory.setJobExplorer(jobExplorer);
+		factory.setJobLauncher(jobLauncher);
+		factory.setJobRegistry(jobRegistry);
+		factory.setJobRepository(jobRepository);
+		jobParametersConverter.ifAvailable(factory::setJobParametersConverter);
+		return factory;
+	}
+}
