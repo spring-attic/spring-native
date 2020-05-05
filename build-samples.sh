e213a0b55e4caeac52734d64549a48ef3630bb58
Sébastien Deleuze <sdeleuze@pivotal.io>
2020-05-05 15:32:07 +0200

    Return proper exit status in build[-key]-samples.sh
    Closes gh-119

--- build-samples.sh
+++ build-samples.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
+RC=0
+
 echo "Graal: `native-image --version`" > samples-summary.csv
 echo "Date,Sample,Build Time (s),Build Mem (GB),RSS Mem (M),Image Size (M),Startup Time (s),JVM Uptime (s)" >> samples-summary.csv
 for d in $(find spring-graal-native-samples -maxdepth 2 -type d)
 do
   if [[ -f "$d/build.sh" && ! -f "$d/.ignore" ]]; then
-    (cd "$d" && ./build.sh)
+    if ! (cd "$d" && ./build.sh); then
+      RC=1
+    fi
     if [ -f "$d/target/native-image/summary.csv" ]; then
       cat $d/target/native-image/summary.csv >> samples-summary.csv
     else
@@ -20,3 +24,5 @@ if ! [ -x "$(command -v tty-table)" ]; then
 else
   tail -n +2 samples-summary.csv | tty-table
 fi
+
+exit $RC
