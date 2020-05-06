2f861510e65dca7e750d79f28ff4880006ebcfb4
Andy Clement <aclement@pivotal.io>
2020-05-05 17:38:42 -0700

    security sample working via agent generated config

---
+++ spring-graal-native-samples/gs-securing-web/compile.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+ARTIFACT=gs-securing-web
+MAINCLASS=com.example.securingweb.SecuringWebApplication
+VERSION=0.0.1-SNAPSHOT
+FEATURE=../../../../spring-graal-native/target/spring-graal-native-0.7.0.BUILD-SNAPSHOT.jar
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+rm -rf target
+mkdir -p target/native-image
+
+echo "Packaging $ARTIFACT with Maven"
+mvn -DskipTests package > target/native-image/output.txt
+
+JAR="$ARTIFACT-$VERSION.jar"
+rm -f $ARTIFACT
+echo "Unpacking $JAR"
+cd target/native-image
+jar -xvf ../$JAR >/dev/null 2>&1
+cp -R META-INF BOOT-INF/classes
+
+LIBPATH=`find BOOT-INF/lib | tr '\n' ':'`
+CP=BOOT-INF/classes:$LIBPATH:$FEATURE
+
+if [ ! -f "$FEATURE" ]; then
+    printf "${RED}FAILURE${NC}: $FEATURE does not exist, please build the root project before building this sample.\n"
+    exit 1
+fi
+
+GRAALVM_VERSION=`native-image --version`
+echo "Compiling $ARTIFACT with $GRAALVM_VERSION"
+{ time native-image \
+  -Dspring.graal.mode=initialization-only \
+  --verbose \
+  --no-server \
+  --no-fallback \
+  -H:Name=$ARTIFACT \
+  -H:+ReportExceptionStackTraces \
+  -H:IncludeResourceBundles=javax.servlet.http.LocalStrings \
+  -H:IncludeResourceBundles=javax.servlet.LocalStrings \
+  --enable-all-security-services \
+  -Dspring.graal.remove-unused-autoconfig=true \
+  -cp $CP $MAINCLASS >> output.txt ; } 2>> output.txt
+
+if [[ -f $ARTIFACT ]]
+then
+  printf "${GREEN}SUCCESS${NC}\n"
+  mv ./$ARTIFACT ..
+  exit 0
+else
+  cat output.txt
+  printf "${RED}FAILURE${NC}: an error occurred when compiling the native-image.\n"
+  exit 1
+fi
