25ffc07464133c4695a8ee7827540e1b5a88f82e
Sébastien Deleuze <sdeleuze@pivotal.io>
2020-05-06 15:37:57 +0200

    Fix logger sample
    See gh-84

--- spring-graal-native-samples/logger/verify.sh
+++ spring-graal-native-samples/logger/verify.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 OUTPUT=`cat target/native-image/test-output.txt`
-if [[ ${OUTPUT} == *"INFO: info"* && ${OUTPUT} == *"SEVERE: ouch"* && ${OUTPUT} == *"java.lang.RuntimeException"* ]]
+if [[ ${OUTPUT} == *"INFO com.example.logger.LoggerApplication - info"* && ${OUTPUT} == *"ERROR com.example.logger.LoggerApplication - ouch"* && ${OUTPUT} == *"java.lang.RuntimeException"* ]]
 then
   exit 0
 else 
