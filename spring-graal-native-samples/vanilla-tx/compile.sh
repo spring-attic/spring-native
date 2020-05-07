0595758530bfed6de60103ab173f6a198388c338
Sébastien Deleuze <sdeleuze@pivotal.io>
2020-05-07 11:21:14 +0200

    Remove --initialize-at-build from vanilla-tx and Petclinic samples
    In 20.1 proxified classes do not need anymore to be initialized at build time.
    
    See gh-90

--- spring-graal-native-samples/vanilla-tx/compile.sh
+++ spring-graal-native-samples/vanilla-tx/compile.sh
@@ -39,7 +39,6 @@ echo "Compiling $ARTIFACT with $GRAALVM_VERSION"
   -H:+TraceClassInitialization \
   -H:Name=$ARTIFACT \
   -H:+ReportExceptionStackTraces \
-  --initialize-at-build-time=app.main.Finder \
   -Dspring.graal.verbose=true \
   -Dspring.graal.remove-unused-autoconfig=true \
   -cp $CP $MAINCLASS >> output.txt ; } 2>> output.txt
