9f9ea86756f738ef77cab31cff32f8fe9a3e3f66
Dave Syer <dsyer@pivotal.io>
2020-05-20 08:41:29 +0000

    Exclude Spring Data Web configuration in PetClinic
    Can be reverted when Spring Data is fixed to add
    proxyBeanMethods = false

--- spring-graalvm-native-samples/spring-petclinic-jpa/src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ spring-graalvm-native-samples/spring-petclinic-jpa/src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -18,6 +18,7 @@ package org.springframework.samples.petclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebAutoConfiguration;
 
 /**
  * PetClinic Spring Boot Application.
@@ -25,7 +26,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author Dave Syer
  *
  */
-@SpringBootApplication(proxyBeanMethods = false)
+@SpringBootApplication(proxyBeanMethods = false, exclude = SpringDataWebAutoConfiguration.class)
 public class PetClinicApplication {
 
 	public static void main(String[] args) throws Exception {
