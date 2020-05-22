8f2c2a13cff3c1bdf1cee0e3e5058a348601fac2
Sébastien Deleuze <sdeleuze@pivotal.io>
2020-05-22 09:47:41 +0200

    Use proxyBeanMethods=false when possible

--- spring-graalvm-native-samples/actuator-webmvc/src/main/java/com/example/demo/DemoApplication.java
+++ spring-graalvm-native-samples/actuator-webmvc/src/main/java/com/example/demo/DemoApplication.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(proxyBeanMethods = false)
 public class DemoApplication {
 
 	public static void main(String[] args) {
