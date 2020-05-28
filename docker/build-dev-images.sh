0b0db9ad7edba40ed45302e59e5a9481a94c7014
Sébastien Deleuze <sdeleuze@vmware.com>
2020-05-28 15:50:57 +0200

    Add support for Docker scripts on Mac
    See gh-79

--- docker/build-dev-images.sh
+++ docker/build-dev-images.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DEV_IMAGE=$(test $OSTYPE = "darwin" && echo "Dockerfile.spring-graalvm-native-dev-mac" || echo "Dockerfile.spring-graalvm-native-dev")
+echo "Using $DEV_IMAGE since OSTYPE is $OSTYPE"
 
 # Update remote images
 docker pull springci/spring-graalvm-native:20.1-dev-java8
@@ -13,25 +15,25 @@ docker build \
   --build-arg USER=$USER \
   --build-arg USER_ID=$(id -u ${USER}) \
   --build-arg USER_GID=$(id -g ${USER}) \
-  -t spring-graalvm-native-dev:20.1-dev-java8 - < $DIR/Dockerfile.spring-graalvm-native-dev
+  -t spring-graalvm-native-dev:20.1-dev-java8 - < $DIR/$DEV_IMAGE
 
 docker build \
   --build-arg BASE_IMAGE=springci/spring-graalvm-native:20.1-dev-java11 \
   --build-arg USER=$USER \
   --build-arg USER_ID=$(id -u ${USER}) \
   --build-arg USER_GID=$(id -g ${USER}) \
-  -t spring-graalvm-native-dev:20.1-dev-java11 - < $DIR/Dockerfile.spring-graalvm-native-dev
+  -t spring-graalvm-native-dev:20.1-dev-java11 - < $DIR/$DEV_IMAGE
 
 docker build \
   --build-arg BASE_IMAGE=springci/spring-graalvm-native:master-java8 \
   --build-arg USER=$USER \
   --build-arg USER_ID=$(id -u ${USER}) \
   --build-arg USER_GID=$(id -g ${USER}) \
-  -t spring-graalvm-native-dev:master-java8 - < $DIR/Dockerfile.spring-graalvm-native-dev
+  -t spring-graalvm-native-dev:master-java8 - < $DIR/$DEV_IMAGE
 
 docker build \
   --build-arg BASE_IMAGE=springci/spring-graalvm-native:master-java11 \
   --build-arg USER=$USER \
   --build-arg USER_ID=$(id -u ${USER}) \
   --build-arg USER_GID=$(id -g ${USER}) \
-  -t spring-graalvm-native-dev:master-java11 - < $DIR/Dockerfile.spring-graalvm-native-dev
+  -t spring-graalvm-native-dev:master-java11 - < $DIR/$DEV_IMAGE
