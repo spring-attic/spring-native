9e50d4685c653ca3cd02144e8dcd4b8a4700d5d5
Sébastien Deleuze <sdeleuze@vmware.com>
2020-06-02 12:35:00 +0200

    Add --workdir support to run-dev-container.sh
    See gh-79

--- run-dev-container.sh
+++ run-dev-container.sh
@@ -3,6 +3,7 @@
 JAVA_VERSION=8
 GRAALVM_VERSION=20.1-dev
 PULL=false
+HOST_WORK_DIR="$( pwd )"
 
 while test $# -gt 0; do
   case "$1" in
@@ -15,6 +16,7 @@ while test $# -gt 0; do
       echo "-h, --help                show brief help"
       echo "-j, --java=VERSION        specify Java version to use, can be 8 or 11, 8 by default"
       echo "-g, --graalvm=VERSION     specify GraalVM version to use, can be 20.1-dev or master, 20.1-dev by default"
+      echo "-w, --workdir=/foo        specify the working directory, should be an absolute path, current one by default"
       echo "-p, --pull                force container image refresh"
       exit 0
       ;;
@@ -46,7 +48,22 @@ while test $# -gt 0; do
       export GRAALVM_VERSION=`echo $1 | sed -e 's/^[^=]*=//g'`
       shift
       ;;
+    -w)
+      shift
+      if test $# -gt 0; then
+        export HOST_WORK_DIR=$1
+      else
+        echo "no working directory specified"
+        exit 1
+      fi
+      shift
+      ;;
+    --workdir)
+      export HOST_WORK_DIR=`echo $1 | sed -e 's/^[^=]*=//g'`
+      shift
+      ;;
     -p)
+
       export PULL=true
       shift
       ;;
@@ -61,9 +78,8 @@ while test $# -gt 0; do
 done
 
 DOCKER_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/docker" >/dev/null 2>&1 && pwd )"
-CURRENT_DIR="$( pwd )"
 CONTAINER_HOME=/home/$USER
-WORK_DIR=$CONTAINER_HOME/spring-graalvm-native
+CONTAINER_WORK_DIR=$CONTAINER_HOME/spring-graalvm-native
 DEV_IMAGE=$(test $OSTYPE = "darwin" && echo "Dockerfile.spring-graalvm-native-dev-mac" || echo "Dockerfile.spring-graalvm-native-dev")
 
 if [ "$PULL" = true ] ; then
@@ -78,4 +94,4 @@ docker image ls | grep spring-graalvm-native-dev | grep ${GRAALVM_VERSION}-java$
   --build-arg USER_GID=$(id -g ${USER}) \
   -t spring-graalvm-native-dev:${GRAALVM_VERSION}-java${JAVA_VERSION} - < $DOCKER_DIR/$DEV_IMAGE
 
-docker run --hostname docker -v $CURRENT_DIR:$WORK_DIR -v $HOME/.m2:$CONTAINER_HOME/.m2 -it -w $WORK_DIR -u $(id -u ${USER}):$(id -g ${USER}) spring-graalvm-native-dev:${GRAALVM_VERSION}-java${JAVA_VERSION}
+docker run --hostname docker -v $HOST_WORK_DIR:$CONTAINER_WORK_DIR -v $HOME/.m2:$CONTAINER_HOME/.m2 -it -w $CONTAINER_WORK_DIR -u $(id -u ${USER}):$(id -g ${USER}) spring-graalvm-native-dev:${GRAALVM_VERSION}-java${JAVA_VERSION}
