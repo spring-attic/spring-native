92f25cdfcf86a310921b0582a3cc5dd5c7590e3d
Sébastien Deleuze <sdeleuze@vmware.com>
2020-06-02 12:23:35 +0200

    Introduce run-dev-container.sh
    See gh-79

--- docker/run-master-java11.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd )"
-CONTAINER_HOME=/home/$USER
-WORK_DIR=$CONTAINER_HOME/spring-graalvm-native
-docker run --hostname docker -v $CURRENT_DIR:$WORK_DIR -v $HOME/.m2:$CONTAINER_HOME/.m2 -it -w $WORK_DIR -u $(id -u ${USER}):$(id -g ${USER}) spring-graalvm-native-dev:master-java11
