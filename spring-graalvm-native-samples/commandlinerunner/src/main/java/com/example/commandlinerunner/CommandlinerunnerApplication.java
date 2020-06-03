b859386e0e462aa80cb3aaf2815e3b4d883bacad
Sébastien Deleuze <sdeleuze@vmware.com>
2020-06-03 12:57:44 +0200

    Polishing

--- spring-graalvm-native-samples/commandlinerunner/src/main/java/com/example/commandlinerunner/CommandlinerunnerApplication.java
+++ spring-graalvm-native-samples/commandlinerunner/src/main/java/com/example/commandlinerunner/CommandlinerunnerApplication.java
@@ -3,7 +3,7 @@ package com.example.commandlinerunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-	@SpringBootApplication(proxyBeanMethods=false)
+@SpringBootApplication(proxyBeanMethods=false)
 public class CommandlinerunnerApplication {
 
 	public static void main(String[] args) throws InterruptedException {
