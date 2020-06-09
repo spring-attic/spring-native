709c5aebcc0eed2cb3b5e83478da27bf101d0697
Sébastien Deleuze <sdeleuze@vmware.com>
2020-06-09 21:37:05 +0200

    Revert "some more hacking to read custom repository implementations"
    This reverts commit fef70cf99f3f692db8682251576e1fd10a1aa115.

--- spring-graalvm-native-feature/src/main/java/org/springframework/graalvm/extension/NativeImageContext.java
+++ spring-graalvm-native-feature/src/main/java/org/springframework/graalvm/extension/NativeImageContext.java
@@ -40,8 +40,4 @@ public interface NativeImageContext {
 	void addReflectiveAccessHierarchy(Type type, Flag... flags);
 
 	boolean hasReflectionConfigFor(String key);
-
-	default boolean hasReflectionConfigFor(Type type) {
-		return hasReflectionConfigFor(type.getDottedName());
-	}
 }
