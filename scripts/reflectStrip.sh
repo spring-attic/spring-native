d418c05530952fb630045ca35eeaa363c79d4d03
Sébastien Deleuze <sdeleuze@vmware.com>
2020-06-10 12:26:52 +0200

    Release Spring GraalVM Native 0.7.0

--- scripts/reflectStrip.sh
+++ scripts/reflectStrip.sh
@@ -1,2 +1,2 @@
 # Creates a compact diff between two reflect json files
-java -classpath `dirname $0`/../spring-graalvm-native-feature/target/spring-graalvm-native-feature-0.7.0-SNAPSHOT.jar org.springframework.graalvm.support.ReflectionJsonStrip $1 $2
+java -classpath `dirname $0`/../spring-graalvm-native-feature/target/spring-graalvm-native-feature-0.7.0.jar org.springframework.graalvm.support.ReflectionJsonStrip $1 $2
