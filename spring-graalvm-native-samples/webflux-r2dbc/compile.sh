35eff959580aed40c740bfdcb105e60835820a3f
Sébastien Deleuze <sdeleuze@vmware.com>
2020-06-10 22:46:14 +0200

    Change graal profile to native in R2DBC sample

--- spring-graalvm-native-samples/webflux-r2dbc/compile.sh
+++ spring-graalvm-native-samples/webflux-r2dbc/compile.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 rm -rf target/
 mkdir -p target/native-image 2>/dev/null
-mvn -ntp -Pgraal package > target/native-image/output.txt
+mvn -ntp -Pnative package > target/native-image/output.txt
