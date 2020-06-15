b47378d59e65039328841f9a17df0a22c79a42ec
Christoph Strobl <cstrobl@vmware.com>
2020-06-15 11:17:02 +0200

    #159 - Split Hints for common SpringData infrastructure and store specifics.
    Use AbstractRepositoryConfigurationSourceSupport as trigger for common Spring Data infrastructure like RepositoryFactoryBeanSupport. This allows hints for dedicated modules to be reduced to store specifics.

--- spring-graalvm-native-configuration/src/main/java/org/springframework/boot/autoconfigure/data/r2dbc/R2dbcRepositoriesHints.java
+++ spring-graalvm-native-configuration/src/main/java/org/springframework/boot/autoconfigure/data/r2dbc/R2dbcRepositoriesHints.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *      https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,15 +15,11 @@
  */
 package org.springframework.boot.autoconfigure.data.r2dbc;
 
-import org.springframework.data.mapping.context.MappingContext;
 import org.springframework.data.r2dbc.dialect.DialectResolver.R2dbcDialectProvider;
 import org.springframework.data.r2dbc.repository.config.R2dbcRepositoryConfigurationExtension;
 import org.springframework.data.r2dbc.repository.support.R2dbcRepositoryFactoryBean;
 import org.springframework.data.r2dbc.repository.support.SimpleR2dbcRepository;
 import org.springframework.data.repository.core.RepositoryMetadata;
-import org.springframework.data.repository.core.support.PropertiesBasedNamedQueries;
-import org.springframework.data.repository.core.support.RepositoryFactoryBeanSupport;
-import org.springframework.data.repository.core.support.RepositoryFragmentsFactoryBean;
 import org.springframework.graalvm.extension.NativeImageConfiguration;
 import org.springframework.graalvm.extension.NativeImageHint;
 import org.springframework.graalvm.extension.TypeInfo;
@@ -31,8 +27,8 @@ import org.springframework.graalvm.type.AccessBits;
 
 @NativeImageHint(trigger = R2dbcRepositoriesAutoConfiguration.class, typeInfos = {
 		@TypeInfo(types = {
-				R2dbcRepositoryFactoryBean.class, RepositoryFactoryBeanSupport.class, R2dbcRepositoryConfigurationExtension.class,
-				MappingContext.class, PropertiesBasedNamedQueries.class, RepositoryFragmentsFactoryBean.class,
+				R2dbcRepositoryFactoryBean.class,
+				R2dbcRepositoryConfigurationExtension.class,
 				R2dbcRepositoriesAutoConfigureRegistrar.class,
 				SimpleR2dbcRepository.class,
 				RepositoryMetadata.class,
