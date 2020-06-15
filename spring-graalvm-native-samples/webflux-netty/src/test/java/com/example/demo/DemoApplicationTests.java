39d40677aac2d60be9cb57fee840942f8a361e88
Sébastien Deleuze <sdeleuze@vmware.com>
2020-06-15 10:18:22 +0200

    Uncomment webflux-netty test

--- spring-graalvm-native-samples/webflux-netty/src/test/java/com/example/demo/DemoApplicationTests.java
+++ spring-graalvm-native-samples/webflux-netty/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,16 +1,14 @@
-//package com.example.demo;
-//
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//public class DemoApplicationTests {
-//
-//	@Test
-//	public void contextLoads() {
-//	}
-//
-//}
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
