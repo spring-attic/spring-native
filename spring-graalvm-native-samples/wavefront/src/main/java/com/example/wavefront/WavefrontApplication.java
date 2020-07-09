4acf6a8a83cdafa7a83786e1afde45d1c2b075d3
Sébastien Deleuze <sdeleuze@vmware.com>
2020-07-09 08:12:15 +0200

    Add Wavefront support
    For now no real metrics reported because GC, thread and heap size
    reporting tools seems not be available in native images. The next
    step is Sleuth support.
    
    See gh-199

---
+++ spring-graalvm-native-samples/wavefront/src/main/java/com/example/wavefront/WavefrontApplication.java
@@ -0,0 +1,13 @@
+package com.example.wavefront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(proxyBeanMethods = false)
+public class WavefrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WavefrontApplication.class, args);
+	}
+
+}
