c380dd3ecc0f8802dbe4c55f0723b81b30a2e02a
Andy Clement <aclement@pivotal.io>
2020-07-17 09:28:50 -0700

    working on hybrid setup

--- spring-graalvm-native-samples/webflux-netty/hybrid.sh
+++ spring-graalvm-native-samples/webflux-netty/hybrid.sh
@@ -3,10 +3,9 @@
 BLUE='\033[0;34m'
 NC='\033[0m'
 
+EXEC=`basename $PWD`-hybrid
+
 printf "=== ${BLUE}Building %s sample${NC} ===\n" "${PWD##*/}"
-mvn clean
-rm -rf target/
-mkdir -p target/native-image 2>/dev/null
-mkdir -p target/classes/META-INF/native-image 2>/dev/null
-mvn -ntp -Phybrid-native package > target/native-image/output.txt
-${PWD%/*samples/*}/scripts/test.sh
+mvn -Phybrid-native clean package > output.txt 2>&1 && cp target/$EXEC . && ${PWD%/*samples/*}/scripts/test.sh $EXEC . 
+rm $EXEC
+mv output.txt target/.
--- spring-graalvm-native-samples/webflux-netty/src/test/java/com/example/demo/test/AppContextStartupTest.java
+++ spring-graalvm-native-samples/webflux-netty/src/test/java/com/example/demo/test/AppContextStartupTest.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-public class DemoApplicationTests {
+public class AppContextStartupTest {
 
 	@Test
 	public void contextLoads() {
