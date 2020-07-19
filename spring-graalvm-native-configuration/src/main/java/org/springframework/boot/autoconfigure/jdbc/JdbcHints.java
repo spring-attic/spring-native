fdc30f5b8fd2a21f72e5fe615ab33c9d1d45c36a
Sébastien Deleuze <sdeleuze@vmware.com>
2020-07-19 17:09:49 +0200

    Optimize Jdbc, Tx and Validation hints

--- spring-graalvm-native-configuration/src/main/java/org/springframework/boot/autoconfigure/jdbc/JdbcHints.java
+++ spring-graalvm-native-configuration/src/main/java/org/springframework/boot/autoconfigure/jdbc/JdbcHints.java
@@ -17,6 +17,49 @@ package org.springframework.boot.autoconfigure.jdbc;
 
 import java.sql.Statement;
 
+import com.mysql.cj.conf.url.FailoverConnectionUrl;
+import com.mysql.cj.conf.url.FailoverDnsSrvConnectionUrl;
+import com.mysql.cj.conf.url.LoadBalanceConnectionUrl;
+import com.mysql.cj.conf.url.LoadBalanceDnsSrvConnectionUrl;
+import com.mysql.cj.conf.url.ReplicationConnectionUrl;
+import com.mysql.cj.conf.url.ReplicationDnsSrvConnectionUrl;
+import com.mysql.cj.conf.url.SingleConnectionUrl;
+import com.mysql.cj.conf.url.XDevApiConnectionUrl;
+import com.mysql.cj.conf.url.XDevApiDnsSrvConnectionUrl;
+import com.mysql.cj.exceptions.AssertionFailedException;
+import com.mysql.cj.exceptions.CJCommunicationsException;
+import com.mysql.cj.exceptions.CJConnectionFeatureNotAvailableException;
+import com.mysql.cj.exceptions.CJException;
+import com.mysql.cj.exceptions.CJOperationNotSupportedException;
+import com.mysql.cj.exceptions.CJPacketTooBigException;
+import com.mysql.cj.exceptions.CJTimeoutException;
+import com.mysql.cj.exceptions.ClosedOnExpiredPasswordException;
+import com.mysql.cj.exceptions.ConnectionIsClosedException;
+import com.mysql.cj.exceptions.DataConversionException;
+import com.mysql.cj.exceptions.DataReadException;
+import com.mysql.cj.exceptions.DataTruncationException;
+import com.mysql.cj.exceptions.DeadlockTimeoutRollbackMarker;
+import com.mysql.cj.exceptions.FeatureNotAvailableException;
+import com.mysql.cj.exceptions.InvalidConnectionAttributeException;
+import com.mysql.cj.exceptions.NumberOutOfRange;
+import com.mysql.cj.exceptions.OperationCancelledException;
+import com.mysql.cj.exceptions.PasswordExpiredException;
+import com.mysql.cj.exceptions.PropertyNotModifiableException;
+import com.mysql.cj.exceptions.RSAException;
+import com.mysql.cj.exceptions.SSLParamsException;
+import com.mysql.cj.exceptions.StatementIsClosedException;
+import com.mysql.cj.exceptions.StreamingNotifiable;
+import com.mysql.cj.exceptions.UnableToConnectException;
+import com.mysql.cj.exceptions.UnsupportedConnectionStringException;
+import com.mysql.cj.exceptions.WrongArgumentException;
+import com.mysql.cj.jdbc.Driver;
+import com.mysql.cj.jdbc.ha.NdbLoadBalanceExceptionChecker;
+import com.mysql.cj.jdbc.ha.StandardLoadBalanceExceptionChecker;
+import com.mysql.cj.log.StandardLogger;
+import com.mysql.cj.protocol.AsyncSocketFactory;
+import com.mysql.cj.protocol.NamedPipeSocketFactory;
+import com.mysql.cj.protocol.SocksProxySocketFactory;
+import com.mysql.cj.protocol.StandardSocketFactory;
 import org.h2.mvstore.db.MVTableEngine;
 import org.h2.store.fs.FilePathAsync;
 import org.h2.store.fs.FilePathDisk;
@@ -39,70 +82,69 @@ import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import com.zaxxer.hikari.util.ConcurrentBag.IConcurrentBagEntry;
 
-@NativeImageHint(trigger=DataSourceInitializationConfiguration.Registrar.class,
-	typeInfos= {@TypeInfo(types=DataSourceInitializerPostProcessor.class,access=AccessBits.FULL_REFLECTION)})
+@NativeImageHint(trigger=DataSourceInitializationConfiguration.Registrar.class, typeInfos= {
+		@TypeInfo(types=DataSourceInitializerPostProcessor.class, access=AccessBits.FULL_REFLECTION)})
+
 @NativeImageHint(trigger=EmbeddedDataSourceConfiguration.class, typeInfos= {
-		@TypeInfo(types=EmbeddedDatabase.class,typeNames="org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseFactory$EmbeddedDataSourceProxy",
-				access=AccessBits.CLASS|AccessBits.DECLARED_CONSTRUCTORS|AccessBits.DECLARED_METHODS),
+		@TypeInfo(types=EmbeddedDatabase.class, typeNames="org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseFactory$EmbeddedDataSourceProxy",
+				access=AccessBits.LOAD_AND_CONSTRUCT|AccessBits.DECLARED_METHODS),
 		@TypeInfo(
-				access=AccessBits.CLASS|AccessBits.DECLARED_CONSTRUCTORS,
-				typeNames= {
-						"org.h2.store.fs.FilePathMemLZF","org.h2.store.fs.FilePathNioMemLZF"},
-				types= {
-						FilePathDisk.class, FilePathMem.class, FilePathNioMem.class, FilePathSplit.class,FilePathNio.class,FilePathNioMapped.class,FilePathAsync.class,FilePathZip.class,FilePathRetryOnInterrupt.class}),
-})
-@NativeImageHint(trigger=Hikari.class,typeInfos= {
-		@TypeInfo(types= {HikariDataSource.class,HikariConfig.class,MVTableEngine.class,Statement.class,Statement[].class,
-				// TODO what is the right place for this one? (and its array variant)
-				IConcurrentBagEntry[].class,IConcurrentBagEntry.class})
-	})
+				typeNames= {"org.h2.store.fs.FilePathMemLZF","org.h2.store.fs.FilePathNioMemLZF"},
+				types= {FilePathDisk.class, FilePathMem.class, FilePathNioMem.class, FilePathSplit.class,FilePathNio.class,FilePathNioMapped.class,FilePathAsync.class,FilePathZip.class,FilePathRetryOnInterrupt.class},
+				access=AccessBits.LOAD_AND_CONSTRUCT)})
+
+@NativeImageHint(trigger=Hikari.class, typeInfos= {
+		@TypeInfo(types= {HikariDataSource.class,MVTableEngine.class,Statement.class,Statement[].class, IConcurrentBagEntry[].class,IConcurrentBagEntry.class},
+				access=AccessBits.LOAD_AND_CONSTRUCT),
+	@TypeInfo(types = HikariConfig.class, access = AccessBits.FULL_REFLECTION)})
+
 // MySQL JDBC driver
 @NativeImageHint(trigger=DataSourceAutoConfiguration.class, typeInfos= {
-		@TypeInfo(
-				access=AccessBits.CLASS|AccessBits.DECLARED_CONSTRUCTORS,
-				types= {com.mysql.cj.conf.url.FailoverConnectionUrl.class,
-						com.mysql.cj.conf.url.FailoverDnsSrvConnectionUrl.class,
-						com.mysql.cj.conf.url.LoadBalanceConnectionUrl.class,
-						com.mysql.cj.conf.url.LoadBalanceDnsSrvConnectionUrl.class,
-						com.mysql.cj.conf.url.ReplicationConnectionUrl.class,
-						com.mysql.cj.conf.url.ReplicationDnsSrvConnectionUrl.class,
-						com.mysql.cj.conf.url.SingleConnectionUrl.class,
-						com.mysql.cj.conf.url.XDevApiConnectionUrl.class,
-						com.mysql.cj.conf.url.XDevApiDnsSrvConnectionUrl.class,
-						com.mysql.cj.exceptions.AssertionFailedException.class,
-						com.mysql.cj.exceptions.CJCommunicationsException.class,
-						com.mysql.cj.exceptions.CJConnectionFeatureNotAvailableException.class,
-						com.mysql.cj.exceptions.CJException.class,
-						com.mysql.cj.exceptions.CJOperationNotSupportedException.class,
-						com.mysql.cj.exceptions.CJPacketTooBigException.class,
-						com.mysql.cj.exceptions.CJTimeoutException.class,
-						com.mysql.cj.exceptions.ClosedOnExpiredPasswordException.class,
-						com.mysql.cj.exceptions.ConnectionIsClosedException.class,
-						com.mysql.cj.exceptions.DataConversionException.class,
-						com.mysql.cj.exceptions.DataReadException.class,
-						com.mysql.cj.exceptions.DataTruncationException.class,
-						com.mysql.cj.exceptions.DeadlockTimeoutRollbackMarker.class,
-						com.mysql.cj.exceptions.FeatureNotAvailableException.class,
-						com.mysql.cj.exceptions.InvalidConnectionAttributeException.class,
-						com.mysql.cj.exceptions.NumberOutOfRange.class,
-						com.mysql.cj.exceptions.OperationCancelledException.class,
-						com.mysql.cj.exceptions.PasswordExpiredException.class,
-						com.mysql.cj.exceptions.PropertyNotModifiableException.class,
-						com.mysql.cj.exceptions.RSAException.class,
-						com.mysql.cj.exceptions.SSLParamsException.class,
-						com.mysql.cj.exceptions.StatementIsClosedException.class,
-						com.mysql.cj.exceptions.StreamingNotifiable.class,
-						com.mysql.cj.exceptions.UnableToConnectException.class,
-						com.mysql.cj.exceptions.UnsupportedConnectionStringException.class,
-						com.mysql.cj.exceptions.WrongArgumentException.class,
-						com.mysql.cj.jdbc.Driver.class,
-						com.mysql.cj.jdbc.ha.NdbLoadBalanceExceptionChecker.class,
-						com.mysql.cj.jdbc.ha.StandardLoadBalanceExceptionChecker.class,
-						com.mysql.cj.log.StandardLogger.class,
-						com.mysql.cj.protocol.AsyncSocketFactory.class,
-						com.mysql.cj.protocol.NamedPipeSocketFactory.class,
-						com.mysql.cj.protocol.SocksProxySocketFactory.class,
-						com.mysql.cj.protocol.StandardSocketFactory.class}),
+		@TypeInfo(types= {
+				FailoverConnectionUrl.class,
+				FailoverDnsSrvConnectionUrl.class,
+				LoadBalanceConnectionUrl.class,
+				LoadBalanceDnsSrvConnectionUrl.class,
+				ReplicationConnectionUrl.class,
+				ReplicationDnsSrvConnectionUrl.class,
+				SingleConnectionUrl.class,
+				XDevApiConnectionUrl.class,
+				XDevApiDnsSrvConnectionUrl.class,
+				AssertionFailedException.class,
+				CJCommunicationsException.class,
+				CJConnectionFeatureNotAvailableException.class,
+				CJException.class,
+				CJOperationNotSupportedException.class,
+				CJPacketTooBigException.class,
+				CJTimeoutException.class,
+				ClosedOnExpiredPasswordException.class,
+				ConnectionIsClosedException.class,
+				DataConversionException.class,
+				DataReadException.class,
+				DataTruncationException.class,
+				DeadlockTimeoutRollbackMarker.class,
+				FeatureNotAvailableException.class,
+				InvalidConnectionAttributeException.class,
+				NumberOutOfRange.class,
+				OperationCancelledException.class,
+				PasswordExpiredException.class,
+				PropertyNotModifiableException.class,
+				RSAException.class,
+				SSLParamsException.class,
+				StatementIsClosedException.class,
+				StreamingNotifiable.class,
+				UnableToConnectException.class,
+				UnsupportedConnectionStringException.class,
+				WrongArgumentException.class,
+				Driver.class,
+				NdbLoadBalanceExceptionChecker.class,
+				StandardLoadBalanceExceptionChecker.class,
+				StandardLogger.class,
+				AsyncSocketFactory.class,
+				NamedPipeSocketFactory.class,
+				SocksProxySocketFactory.class,
+				StandardSocketFactory.class },
+				access=AccessBits.LOAD_AND_CONSTRUCT),
 }, resourcesInfos = {
 		@ResourcesInfo(isBundle = true, patterns = {
 			"com.mysql.cj.LocalizedErrorMessages"
