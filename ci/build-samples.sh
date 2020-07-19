055313c6363eafe42b26366fc4bb7f68ced3ec1b
Sébastien Deleuze <sdeleuze@vmware.com>
2020-07-19 15:18:49 +0200

    Run MySQL on the CI

--- ci/build-samples.sh
+++ ci/build-samples.sh
@@ -3,6 +3,7 @@
 mkdir /tmp/data
 /usr/bin/mongod --fork --dbpath /tmp/data --logpath /tmp/data/mongod.log
 redis-server --daemonize yes
+/tmp/mysql.sh
 cd spring-graalvm-native
 ./build.sh
 ./build-samples.sh
