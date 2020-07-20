9623181bd47f1b5f02de99b83954173f977ef038
Sébastien Deleuze <sdeleuze@vmware.com>
2020-07-20 09:34:20 +0200

    Fix MySQL on the CI

--- ci/build-samples.sh
+++ ci/build-samples.sh
@@ -3,6 +3,9 @@
 mkdir /tmp/data
 /usr/bin/mongod --fork --dbpath /tmp/data --logpath /tmp/data/mongod.log
 redis-server --daemonize yes
+chown -R mysql:mysql /var/lib/mysql
+chown -R mysql:mysql /var/log/mysql
+chown -R mysql:mysql /var/run/mysqld
 /tmp/mysql.sh
 cd spring-graalvm-native
 ./build.sh
