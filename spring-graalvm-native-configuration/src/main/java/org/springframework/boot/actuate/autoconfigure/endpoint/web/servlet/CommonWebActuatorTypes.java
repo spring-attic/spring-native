052be4db66275b4b18a96ea1b9408d16188af29a
Andy Clement <aclement@pivotal.io>
2020-07-24 15:37:51 -0700

    more hint tweaks

--- spring-graalvm-native-configuration/src/main/java/org/springframework/boot/actuate/autoconfigure/endpoint/web/servlet/CommonWebActuatorTypes.java
+++ spring-graalvm-native-configuration/src/main/java/org/springframework/boot/actuate/autoconfigure/endpoint/web/servlet/CommonWebActuatorTypes.java
@@ -15,8 +15,66 @@
  */
 package org.springframework.boot.actuate.autoconfigure.endpoint.web.servlet;
 
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.CorsEndpointProperties;
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.ServletEndpointManagementContextConfiguration.WebMvcServletEndpointManagementContextConfiguration;
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.reactive.WebFluxEndpointManagementContextConfiguration;
+import org.springframework.boot.actuate.autoconfigure.web.ManagementContextConfiguration;
+import org.springframework.boot.actuate.autoconfigure.web.ManagementContextFactory;
+import org.springframework.boot.actuate.autoconfigure.web.ManagementContextType;
+import org.springframework.boot.actuate.autoconfigure.web.reactive.ReactiveManagementChildContextConfiguration;
+import org.springframework.boot.actuate.autoconfigure.web.server.ManagementPortType;
+import org.springframework.boot.actuate.endpoint.web.EndpointMediaTypes;
+import org.springframework.boot.actuate.endpoint.web.PathMappedEndpoints;
+import org.springframework.boot.actuate.endpoint.web.PathMapper;
+import org.springframework.boot.actuate.endpoint.web.ServletEndpointRegistrar;
+import org.springframework.boot.actuate.endpoint.web.WebEndpointsSupplier;
+import org.springframework.boot.actuate.endpoint.web.annotation.ControllerEndpointDiscoverer;
+import org.springframework.boot.actuate.endpoint.web.annotation.ControllerEndpointsSupplier;
+import org.springframework.boot.actuate.endpoint.web.annotation.EndpointWebExtension;
+import org.springframework.boot.actuate.endpoint.web.annotation.WebEndpoint;
+import org.springframework.boot.actuate.endpoint.web.annotation.WebEndpointDiscoverer;
+import org.springframework.boot.actuate.endpoint.web.servlet.AbstractWebMvcEndpointHandlerMapping;
+import org.springframework.boot.actuate.endpoint.web.servlet.WebMvcEndpointHandlerMapping;
+import org.springframework.boot.actuate.logging.LogFileWebEndpoint;
+import org.springframework.boot.actuate.management.HeapDumpWebEndpoint;
+import org.springframework.boot.actuate.trace.http.HttpTraceEndpoint;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
 import org.springframework.graalvm.extension.TypeInfo;
 
+/**
+ * Example... this could host @TypeInfo annotations that any other @NativeImageHint could pull in
+ * via an importInfos reference to this type.
+ * 
+ * @author Andy Clement
+ */
+@TypeInfo(types = {
+		HttpTraceEndpoint.class,
+		LogFileWebEndpoint.class,
+		ServletEndpointRegistrar.class,
+		org.springframework.boot.actuate.autoconfigure.web.jersey.JerseySameManagementContextConfiguration.class,
+		org.springframework.boot.actuate.autoconfigure.web.jersey.JerseyChildManagementContextConfiguration.class,
+		HttpTraceRepository.class,
+		ManagementContextType.class,
+		HeapDumpWebEndpoint.class,
+		EndpointMediaTypes.class,
+		PathMappedEndpoints.class,
+		WebEndpointsSupplier.class,
+		CorsEndpointProperties.class,
+		EndpointWebExtension.class,
+		WebEndpoint.class,
+		WebEndpointDiscoverer.class,
+		// web package
+		ManagementContextConfiguration.class,
+		ManagementContextFactory.class,
+		ManagementContextType.class,
+		PathMapper.class,
+		ManagementPortType.class,
+}, typeNames = {
+		"org.springframework.boot.actuate.autoconfigure.web.server.EnableManagementContext",
+		"org.springframework.boot.actuate.autoconfigure.web.server.ManagementContextAutoConfiguration$SameManagementContextConfiguration",
+		"org.springframework.boot.actuate.autoconfigure.endpoint.web.jersey.JerseyWebEndpointManagementContextConfiguration",
+		"org.springframework.boot.actuate.autoconfigure.endpoint.web.MappingWebEndpointPathMapper",
+		"org.springframework.boot.actuate.endpoint.web.annotation.WebEndpointFilter",
+	})
 public class CommonWebActuatorTypes {
-
 }
