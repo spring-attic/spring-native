5c664020502ac606df00588bba2bf5cec8fcbe5f
Filip Hanik <fhanik@pivotal.io>
2020-07-27 12:18:36 -0700

    Dynamic Maven Plugin that Optimizes tomcat-embed-core library
    - Generate the XReflectionIntrospectionUtils code
    - Replace the IntrospectionUtils.set/getProperty method to call the non reflection code
    - Repackage the tomcat-embed-core jar file and remove non needed classes
    
    Working springmvc-example

---
+++ spring-graalvm-native-substitutions/src/main/java/org/apache/tomcat/util/Target_IntrospectionUtils.java
@@ -0,0 +1,19 @@
+package org.apache.tomcat.util;
+
+import com.oracle.svm.core.annotate.Substitute;
+import com.oracle.svm.core.annotate.TargetClass;
+import org.springframework.graalvm.substitutions.OnlyIfPresent;
+
+@TargetClass(className = "org.apache.tomcat.util.IntrospectionUtils", onlyWith = { OnlyIfPresent.class })
+public final class Target_IntrospectionUtils {
+
+    @Substitute
+    public static Object getProperty(Object o, String name) {
+        return XReflectionIntrospectionUtils.getPropertyInternal(o, name);
+    }
+
+    @Substitute
+    public static boolean setProperty(Object o, String name, String value, boolean invokeSetProperty) {
+        return XReflectionIntrospectionUtils.setPropertyInternal(o, name, value, invokeSetProperty);
+    }
+}
