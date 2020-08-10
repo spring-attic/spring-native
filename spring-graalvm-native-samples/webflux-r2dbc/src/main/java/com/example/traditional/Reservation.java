c37b74290f4294e1e73d945350afbe0ffa47598c
Sébastien Deleuze <sdeleuze@vmware.com>
2020-08-10 08:18:30 +0200

    Fix and polish webflux-r2dbc

---
+++ spring-graalvm-native-samples/webflux-r2dbc/src/main/java/com/example/traditional/Reservation.java
@@ -0,0 +1,52 @@
+package com.example.traditional;
+
+import org.springframework.data.annotation.Id;
+
+public class Reservation {
+
+	@Id
+	private Integer id;
+	private String name;
+
+	public String toString() {
+		return "Reservation(id="+id+",name="+name+")";
+	}
+
+	@Override
+	public int hashCode() {
+		return id * 37 + name.hashCode();
+	}
+
+	public boolean equals(Object o) {
+		if (o instanceof Reservation) {
+			Reservation that = (Reservation) o;
+			return this.id == that.id && this.name.equals(that.name);
+		}
+		return false;
+	}
+
+	public Reservation(Integer id, String name) {
+		this.id = id;
+		this.name = name;
+
+	}
+
+	public Reservation() {
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
--- spring-graalvm-native-samples/webflux-r2dbc/src/main/java/com/example/traditional/TraditionalApplication.java
+++ spring-graalvm-native-samples/webflux-r2dbc/src/main/java/com/example/traditional/TraditionalApplication.java
@@ -4,9 +4,7 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.r2dbc.core.DatabaseClient;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.r2dbc.core.DatabaseClient;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
@@ -17,79 +15,23 @@ import static org.springframework.web.reactive.function.server.ServerResponse.ok
 public class TraditionalApplication {
 
 	@Bean
-	RouterFunction<ServerResponse> routes(ReservationRepository rr) {
-		return route().GET("/reservations", r -> ok().body(rr.findAll(), Reservation.class)).build();
+	RouterFunction<ServerResponse> routes(ReservationRepository reservationRepository) {
+		return route().GET("/reservations", r -> ok().body(reservationRepository.findAll(), Reservation.class)).build();
 	}
 
 	@Bean
 	ApplicationRunner runner(DatabaseClient dbc, ReservationRepository reservationRepository) {
 		return args -> {
 
-			dbc.execute("create table reservation\n" + "(\n" + "    id   serial primary key,\n"
-					+ "    name varchar(255) not null\n" + ")").fetch().rowsUpdated().subscribe();
-
-			reservationRepository.save(new Reservation(null, "Andy")).subscribe();
-			reservationRepository.save(new Reservation(null, "Sebastien")).subscribe();
-
-			reservationRepository.findAll().subscribe(System.out::println);
-			;
+			dbc.sql("create table reservation\n" + "(\n" + "    id   serial primary key,\n"
+					+ "    name varchar(255) not null\n" + ")").fetch().rowsUpdated()
+					.then(reservationRepository.save(new Reservation(null, "Andy")))
+					.then(reservationRepository.save(new Reservation(null, "Sebastien")))
+					.then(reservationRepository.findAll().doOnNext(System.out::println).then()).subscribe();
 		};
 	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TraditionalApplication.class, args);
-	}
-}
-
-interface ReservationRepository extends ReactiveCrudRepository<Reservation, Integer> {
-
-}
-
-class Reservation {
-
-	@Id
-	private Integer id;
-	private String name;
-
-	public String toString() {
-		return "Reservation(id="+id+",name="+name+")";
-	}
-
-	@Override
-	public int hashCode() {
-		return id * 37 + name.hashCode();
-	}
-
-	public boolean equals(Object o) {
-		if (o instanceof Reservation) {
-			Reservation that = (Reservation) o;
-			return this.id == that.id && this.name.equals(that.name);
-		}
-		return false;
-	}
-
-	public Reservation(Integer id, String name) {
-		this.id = id;
-		this.name = name;
-
-	}
-
-	public Reservation() {
-	}
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
 	}
 }
