6358e0d197352d9884ee2f73c17d4d5013e40543
Sébastien Deleuze <sdeleuze@vmware.com>
2020-08-10 12:35:12 +0200

    Reduce logging reflection after Spring Boot improvements
    Require gh-224 to be fixed to be effective in functional mode.
    
    Closes gh-220

---
+++ spring-graalvm-native-configuration/src/main/java/org/springframework/boot/logging/LogbackHints.java
@@ -0,0 +1,34 @@
+package org.springframework.boot.logging;
+
+import ch.qos.logback.classic.pattern.DateConverter;
+import ch.qos.logback.classic.pattern.LevelConverter;
+import ch.qos.logback.classic.pattern.LineSeparatorConverter;
+import ch.qos.logback.classic.pattern.LoggerConverter;
+import ch.qos.logback.classic.pattern.MDCConverter;
+import ch.qos.logback.classic.pattern.MessageConverter;
+import ch.qos.logback.classic.pattern.ThreadConverter;
+
+import org.springframework.boot.logging.logback.ColorConverter;
+import org.springframework.boot.logging.logback.ExtendedWhitespaceThrowableProxyConverter;
+import org.springframework.boot.logging.logback.WhitespaceThrowableProxyConverter;
+import org.springframework.graalvm.extension.NativeImageConfiguration;
+import org.springframework.graalvm.extension.NativeImageHint;
+import org.springframework.graalvm.extension.TypeInfo;
+import org.springframework.graalvm.support.Mode;
+
+// TODO Send a PR to Logback to remove reflection usage in ch.qos.logback.classic.PatternLayout
+@NativeImageHint(typeInfos =
+		@TypeInfo(types= {
+				DateConverter.class,
+				LevelConverter.class,
+				LoggerConverter.class,
+				MessageConverter.class,
+				LineSeparatorConverter.class,
+				ThreadConverter.class,
+				MDCConverter.class,
+				ColorConverter.class,
+				WhitespaceThrowableProxyConverter.class,
+				ExtendedWhitespaceThrowableProxyConverter.class
+}), modes = {Mode.FUNCTIONAL, Mode.DEFAULT})
+public class LogbackHints implements NativeImageConfiguration {
+}
