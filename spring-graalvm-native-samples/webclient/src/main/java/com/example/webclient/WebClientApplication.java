37e4e6e420b3ac29ca4ac6b57bc2e74ef7e31bc1
Sébastien Deleuze <sdeleuze@vmware.com>
2020-08-12 15:50:31 +0200

    Use Maven plugin and Paketo buildpack in webclient sample
    See gh-165

--- spring-graalvm-native-samples/webclient/src/main/java/com/example/webclient/WebClientApplication.java
+++ spring-graalvm-native-samples/webclient/src/main/java/com/example/webclient/WebClientApplication.java
@@ -8,9 +8,7 @@ public class WebClientApplication {
 
 	public static void main(String[] args) throws InterruptedException {
 		SpringApplication.run(WebClientApplication.class, args);
-		if (System.getProperty("org.graalvm.nativeimage.imagecode") != null) {
-			Thread.currentThread().join(); // To be able to measure memory consumption
-		}
+		Thread.currentThread().join(); // To be able to measure memory consumption
 	}
 	
 }
