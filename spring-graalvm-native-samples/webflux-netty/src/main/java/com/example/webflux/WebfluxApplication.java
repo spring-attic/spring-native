312a78bde8640b5a89b6617396cbad0198751a07
Sébastien Deleuze <sdeleuze@vmware.com>
2020-08-12 15:10:31 +0200

    Use Maven plugin and Paketo buildpack in webflux-netty sample
    See gh-165

--- spring-graalvm-native-samples/webflux-netty/src/main/java/com/example/webflux/WebfluxApplication.java
+++ spring-graalvm-native-samples/webflux-netty/src/main/java/com/example/webflux/WebfluxApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.webflux;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication(proxyBeanMethods = false)
-public class DemoApplication {
+public class WebfluxApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(WebfluxApplication.class, args);
 	}
 
 	@RestController
