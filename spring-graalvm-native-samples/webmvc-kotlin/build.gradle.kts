f5caff72502122fe602ddca05712846fbd647c55
Sébastien Deleuze <sdeleuze@vmware.com>
2020-08-14 08:55:51 +0200

    Upgrade to Spring Boot 2.4.0-M2
    Closes gh-138

--- spring-graalvm-native-samples/webmvc-kotlin/build.gradle.kts
+++ spring-graalvm-native-samples/webmvc-kotlin/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 
 plugins {
-    id("org.springframework.boot") version "2.4.0-SNAPSHOT"
+    id("org.springframework.boot") version "2.4.0-M2"
     id("io.spring.dependency-management") version "1.0.9.RELEASE"
     kotlin("jvm") version "1.3.72"
     kotlin("plugin.spring") version "1.3.72"
