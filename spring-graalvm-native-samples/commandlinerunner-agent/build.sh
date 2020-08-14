fbd9f1c46921d1e48abe72c20786582637274b43
Andy Clement <aclement@pivotal.io>
2020-08-13 21:59:52 -0700

    Fix standalone CLR agent project

--- spring-graalvm-native-samples/commandlinerunner-agent/build.sh
+++ spring-graalvm-native-samples/commandlinerunner-agent/build.sh
@@ -4,7 +4,5 @@ BLUE='\033[0;34m'
 NC='\033[0m'
 
 printf "=== ${BLUE}Building %s sample${NC} ===\n" "${PWD##*/}"
-rm -rf target/
-mkdir -p target/native-image 2>/dev/null
-mvn -ntp -Pnative package > target/native-image/output.txt
+./compile.sh
 ${PWD%/*samples/*}/scripts/test.sh
