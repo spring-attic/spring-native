1569289120978faf9a27ef4000e9fcf8d93c720b
Sébastien Deleuze <sdeleuze@vmware.com>
2020-08-14 08:42:10 +0200

    Remove Batch samples for now
    They are in https://github.com/spring-projects-experimental/spring-graalvm-native/tree/batch-samples,
    see related gh-203 issue.

--- spring-graalvm-native-samples/batch-io/verify.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-file1="src/test/resources/output.csv"
-file2=$1
-
-if cmp -s "$file1" "$file2"; then
-    exit 0
-else
-    exit 1
-fi
--- spring-graalvm-native-samples/batch/verify.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-if [[ `cat target/native-image/test-output.txt | grep ">> This was run in a Spring Batch app!"` ]]; then
-  exit 0
-else
-  exit 1
-fi
