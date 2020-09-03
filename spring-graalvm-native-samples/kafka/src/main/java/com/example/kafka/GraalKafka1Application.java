3afb2927769fd0b8e523dc2b9cbc35019f98a082
Gary Russell <grussell@vmware.com>
2020-09-03 16:16:53 -0400

    Kafka Sample

---
+++ spring-graalvm-native-samples/kafka/src/main/java/com/example/kafka/GraalKafka1Application.java
@@ -0,0 +1,39 @@
+package com.example.kafka;
+
+import org.apache.kafka.clients.admin.NewTopic;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaTemplate;
+
+@SpringBootApplication(proxyBeanMethods = false)
+public class GraalKafka1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraalKafka1Application.class, args).close();
+	}
+
+	@KafkaListener(id = "graal", topics = "graal")
+	public void listen(String in) {
+		System.out.println("++++++Received:" + in);
+	}
+
+	@Bean
+	public NewTopic topic() {
+		return TopicBuilder.name("graal").partitions(1).replicas(1).build();
+	}
+
+	@Bean
+	public ApplicationRunner runner(KafkaTemplate<String, String> template) {
+		return args -> {
+			template.send("graal", "foo");
+			System.out.println("++++++Sent:foo");
+			Thread.sleep(5000);
+		};
+	}
+
+}
---
+++ spring-graalvm-native-samples/kafka/src/test/java/com/example/kafka/GraalKafka1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraalKafka1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
