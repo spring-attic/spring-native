65bf4b00d0ca8b6bb6a85e8533641a846569ad5b
Sébastien Deleuze <sdeleuze@vmware.com>
2020-09-07 09:17:35 +0200

    Use debug variants of LabsJDK
    To help tracking oracle/graal#2748.

--- docker/build-graalvm-ce-images.sh
+++ docker/build-graalvm-ce-images.sh
@@ -5,21 +5,21 @@ echo $DIR
 
 
 docker build \
-  --build-arg JVMCI_URL=https://github.com/graalvm/graal-jvmci-8/releases/download/jvmci-20.2-b03/openjdk-8u262+10-jvmci-20.2-b03-linux-amd64.tar.gz \
+  --build-arg JVMCI_URL=https://github.com/graalvm/graal-jvmci-8/releases/download/jvmci-20.2-b03/openjdk-8u262+10-jvmci-20.2-b03-fastdebug-linux-amd64.tar.gz \
   --build-arg GRAALVM_BRANCH=release/graal-vm/20.2 \
   -t springci/graalvm-ce:20.2-dev-java8 - < $DIR/Dockerfile.graalvm-ce
 
 docker build \
-  --build-arg JVMCI_URL=https://github.com/graalvm/labs-openjdk-11/releases/download/jvmci-20.2-b03/labsjdk-ce-11.0.8+10-jvmci-20.2-b03-linux-amd64.tar.gz \
+  --build-arg JVMCI_URL=https://github.com/graalvm/labs-openjdk-11/releases/download/jvmci-20.2-b03/labsjdk-ce-11.0.8+10-jvmci-20.2-b03-debug-linux-amd64.tar.gz \
   --build-arg GRAALVM_BRANCH=release/graal-vm/20.2 \
   -t springci/graalvm-ce:20.2-dev-java11 - < $DIR/Dockerfile.graalvm-ce
 
 docker build \
-  --build-arg JVMCI_URL=https://github.com/graalvm/graal-jvmci-8/releases/download/jvmci-20.2-b03/openjdk-8u262+10-jvmci-20.2-b03-linux-amd64.tar.gz \
+  --build-arg JVMCI_URL=https://github.com/graalvm/graal-jvmci-8/releases/download/jvmci-20.2-b03/openjdk-8u262+10-jvmci-20.2-b03-fastdebug-linux-amd64.tar.gz \
   --build-arg GRAALVM_BRANCH=master \
   -t springci/graalvm-ce:master-java8 - < $DIR/Dockerfile.graalvm-ce
 
 docker build \
-  --build-arg JVMCI_URL=https://github.com/graalvm/labs-openjdk-11/releases/download/jvmci-20.2-b03/labsjdk-ce-11.0.8+10-jvmci-20.2-b03-linux-amd64.tar.gz \
+  --build-arg JVMCI_URL=https://github.com/graalvm/labs-openjdk-11/releases/download/jvmci-20.2-b03/labsjdk-ce-11.0.8+10-jvmci-20.2-b03-debug-linux-amd64.tar.gz \
   --build-arg GRAALVM_BRANCH=master \
   -t springci/graalvm-ce:master-java11 - < $DIR/Dockerfile.graalvm-ce
