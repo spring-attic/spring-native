c57233c808d86077a6937b101968841f18d9aa58
Dave Syer <dsyer@vmware.com>
2020-09-09 11:18:13 +0100

    Fix cloud sample to register HTTP Url prefix

--- spring-graalvm-native-samples/cloud/src/main/java/com/example/webflux/WebfluxApplication.java
+++ spring-graalvm-native-samples/cloud/src/main/java/com/example/webflux/WebfluxApplication.java
@@ -1,24 +1,24 @@
 package com.example.webflux;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication(proxyBeanMethods = false)
+@RestController
 public class WebfluxApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebfluxApplication.class, args);
 	}
 
-	@RestController
-	class Foo {
-
-		@GetMapping("/")
-		public String greet() {
-			return "hi!";
-		}
+	@GetMapping("/")
+	public String greet() {
+		return "hi!";
 	}
 
 }
