6ea66977060667ffa4f33b7df2fff9b2e35c9468
Dave Syer <dsyer@vmware.com>
2020-09-09 11:18:13 +0100

    Fix cloud sample to register HTTP Url prefix

--- spring-graalvm-native-samples/cloud/compile.sh
+++ spring-graalvm-native-samples/cloud/compile.sh
@@ -28,6 +28,7 @@ GRAALVM_VERSION=`native-image --version`
 echo "Compiling $ARTIFACT with $GRAALVM_VERSION"
 { time native-image \
   -H:Name=$ARTIFACT \
+  -H:EnableURLProtocols=http \
   -Dspring.spel.ignore=true \
   -Dspring.native.remove-yaml-support=true \
   -cp $CP $MAINCLASS >> output.txt ; } 2>> output.txt
