a4658c6245fc979e0b0c6e6a5a5f53152905a2ce
Dave Syer <dsyer@vmware.com>
2020-09-09 11:37:11 +0100

    Ensure config client can deserialize properties

---
+++ spring-graalvm-native-configuration/src/main/java/org/springframework/cloud/config/client/ConfigClientHints.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.cloud.config.client;
+
+import org.springframework.cloud.config.environment.Environment;
+import org.springframework.cloud.config.environment.PropertySource;
+import org.springframework.graalvm.extension.NativeImageConfiguration;
+import org.springframework.graalvm.extension.NativeImageHint;
+import org.springframework.graalvm.extension.TypeInfo;
+import org.springframework.graalvm.type.AccessBits;
+
+@NativeImageHint(trigger = ConfigClientAutoConfiguration.class, typeInfos = { @TypeInfo(types = {
+        Environment.class, PropertySource.class }, access = AccessBits.ALL) })
+public class ConfigClientHints implements NativeImageConfiguration {
+
+}
