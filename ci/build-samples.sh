69803f52f51e215bc8f524a525b074ee0b0e9d42
Christoph Strobl <cstrobl@vmware.com>
2020-09-10 07:21:57 +0200

    Add Spring Data Elasticsearch hints and sample
    Provide native image hints that add required proxy
    and reflection for Spring Data Elasticsearch Repositories.
    
    Provide a Map based Substitution for the org.apache.http.impl.client.BasicAuthCache
    used by the Elasticsearch REST Client.
    
    Move Reactive Spring Data hints to dedicated class to be used by TypeInfo imports.
    
    Update docker configuration to include and start Elasticsearch server.

--- ci/build-samples.sh
+++ ci/build-samples.sh
@@ -4,6 +4,7 @@ RC=0
 mkdir /tmp/data
 /usr/bin/mongod --fork --dbpath /tmp/data --logpath /tmp/data/mongod.log
 redis-server --daemonize yes
+elasticsearch start
 chown -R mysql:mysql /var/lib/mysql
 chown -R mysql:mysql /var/log/mysql
 chown -R mysql:mysql /var/run/mysqld
