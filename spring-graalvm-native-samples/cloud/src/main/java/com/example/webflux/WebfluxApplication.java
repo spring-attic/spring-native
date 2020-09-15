0e06a62b2b5519989882e95b9287c4a54f4f5569
Dave Syer <dsyer@vmware.com>
2020-09-15 17:17:33 +0100

    Organize imports

--- spring-graalvm-native-samples/cloud/src/main/java/com/example/webflux/WebfluxApplication.java
+++ spring-graalvm-native-samples/cloud/src/main/java/com/example/webflux/WebfluxApplication.java
@@ -1,10 +1,7 @@
 package com.example.webflux;
 
-import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
