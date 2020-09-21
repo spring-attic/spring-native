13c46833beb5ea31fb1db1474353cded4a95451e
Dave Syer <dsyer@vmware.com>
2020-09-21 08:46:32 +0100

    Add test containers test to function-aws sample

---
+++ spring-graalvm-native-samples/function-aws/src/test/java/com/example/demo/ContainerTests.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2019-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.demo;
+
+import java.util.concurrent.TimeUnit;
+
+import org.awaitility.Awaitility;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.output.ToStringConsumer;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Dave Syer
+ *
+ */
+// @Disabled
+public class ContainerTests {
+
+	@Test
+	void test() throws Exception {
+		ToStringConsumer consumer = new ToStringConsumer();
+		try (@SuppressWarnings("resource")
+		GenericContainer<?> container = new GenericContainer<>("lambci/lambda:provided").withLogConsumer(consumer)
+				.withFileSystemBind("src/test/resources", "/var/task").withEnv("DOCKER_LAMBDA_STAY_OPEN", "1")
+				.withExposedPorts(9001)) {
+			container.start();
+			int port = container.getFirstMappedPort();
+			String host = container.getHost();
+			System.err.println(host + ":" + port);
+			DemoApplication.main(new String[] { "--AWS_LAMBDA_RUNTIME_API=" + host + ":" + port, "--_HANDLER=foobar",
+					"--logging.level.org.springframework=DEBUG" });
+			Awaitility.waitAtMost(30, TimeUnit.SECONDS).until(() -> {
+				ResponseEntity<String> result = new RestTemplate().postForEntity(
+						"http://" + host + ":" + port + "/2015-03-31/functions/foobar/invocations", "foo",
+						String.class);
+				return result.getBody().contains("hi foo!") && result.getHeaders().containsKey("X-Amzn-Requestid");
+			});
+		}
+		String output = consumer.toUtf8String();
+		assertThat(output).contains("Lambda API listening on port 9001");
+		assertThat(output).contains("START RequestId:");
+		assertThat(output).contains("END RequestId:");
+	}
+
+}
--- spring-graalvm-native-samples/function-aws/src/test/java/com/example/demo/DemoApplicationTests.java
+++ spring-graalvm-native-samples/function-aws/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,12 +1,10 @@
 package com.example.demo;
 
-import static org.assertj.core.api.Assertions.*;
-
 import com.example.test.TestServer;
-
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
@@ -15,41 +13,43 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.util.SocketUtils;
 import org.springframework.web.reactive.function.client.WebClient;
 
-@FunctionalSpringBootTest({"spring.cloud.function.web.export.source.url=http://localhost:${export.port}/home",
-    "spring.cloud.function.web.export.sink.url=http://localhost:${export.port}/echo",
-    "logging.level.reactor=OFF",
-    "logging.level.io.netty=OFF"})
+import static org.assertj.core.api.Assertions.assertThat;
+
+@FunctionalSpringBootTest({ "spring.cloud.function.web.export.source.url=http://localhost:${export.port}/home",
+		"spring.cloud.function.web.export.sink.url=http://localhost:${export.port}/echo", "logging.level.reactor=OFF",
+		"logging.level.io.netty=OFF" })
 public class DemoApplicationTests {
 
-    static ConfigurableApplicationContext context;
+	static ConfigurableApplicationContext context;
 
-    @Value("${export.port}")
-    private int port;
+	@Value("${export.port}")
+	private int port;
 
-    @Autowired
-    private WebClient.Builder builder;
+	@Autowired
+	private WebClient.Builder builder;
 
 	@Test
 	public void contextLoads() throws Exception {
-        WebClient client = builder.baseUrl("http://localhost:" + port).build();
-        client.post().uri("/add").bodyValue("Fred").exchange().block();
-        Thread.sleep(100L);
-        String response = client.get().uri("/take").exchange().block().bodyToMono(String.class).block();
-        assertThat(response).isEqualTo("hi Fred!");
-    }
+		WebClient client = builder.baseUrl("http://localhost:" + port).build();
+		client.post().uri("/add").bodyValue("Fred").exchange().block();
+		Thread.sleep(100L);
+		String response = client.get().uri("/take").exchange().block().bodyToMono(String.class).block();
+		assertThat(response).isEqualTo("\"hi Fred!\"");
+	}
 
-    @AfterAll
-    static void after() {
-        if (context != null) {
-            context.close();
-        }
-    }
+	@AfterAll
+	static void after() {
+		if (context != null) {
+			context.close();
+		}
+	}
 
-    @BeforeAll
-    static void before() {
-        int port = SocketUtils.findAvailableTcpPort();
-        System.setProperty("export.port", "" + port);
-        context = SpringApplication.run(TestServer.class, "--server.port="+port, "--spring.cloud.function.web.export.enabled=false", "--spring.main.web-application-type=reactive");
-    }
+	@BeforeAll
+	static void before() {
+		int port = SocketUtils.findAvailableTcpPort();
+		System.setProperty("export.port", "" + port);
+		context = SpringApplication.run(TestServer.class, "--server.port=" + port,
+				"--spring.cloud.function.web.export.enabled=false", "--spring.main.web-application-type=reactive");
+	}
 
 }
