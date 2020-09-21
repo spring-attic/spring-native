4b20a02400beb0ce1f202aa759d8f55d0c51209f
Dave Syer <dsyer@vmware.com>
2020-09-21 10:28:18 +0100

    Back to String->String

--- spring-graalvm-native-samples/function-aws/verify.sh
+++ spring-graalvm-native-samples/function-aws/verify.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
-curl -s localhost:8000/add -d world -H "Content-Type: text/plain"
-echo
-echo Waiting...
-sleep 1
-RESPONSE=`curl -s localhost:8000/take`
-echo Got response: $RESPONSE
-if [[ "$RESPONSE" == 'hi world!' ]]; then
+RESPONSE=`curl -v -d world http://localhost:9001/2015-03-31/functions/foobar/invocations`
+if [[ "$RESPONSE" == '"hi world!"' ]]; then
   exit 0
 else
   exit 1
