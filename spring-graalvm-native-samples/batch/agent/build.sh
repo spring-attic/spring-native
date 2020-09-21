30078640d6b90fc5bbdcdf295e180bee34a29be3
Michael Minella <mminella@pivotal.io>
2020-09-21 14:11:17 -0500

    Added PWDs

--- spring-graalvm-native-samples/batch/agent/build.sh
+++ spring-graalvm-native-samples/batch/agent/build.sh
@@ -3,14 +3,23 @@
 # Build the project and unpack the fat jar ready to run
 ./1prepare.sh
 
+echo "1 is done"
+pwd
+
 # Set the CP for the jars/code in the unpacked application, leaving us in the BOOT-INF/classes folder
 . ./2setcp.sh
+echo "2 is done"
+pwd
 
 # Run the application with the agent to populate the configuration files
 ../../../3runWithAgent.sh
+echo "3 is done"
+pwd
 
 # Run native image to compile the application
 ../../../4compile.sh
+echo "4 is done"
+pwd
 
 # Test the application
 
