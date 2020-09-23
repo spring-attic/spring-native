c0667d79dbc6ad9dc5fece8c08b4806804bb05b9
Dave Syer <dsyer@vmware.com>
2020-09-23 13:26:14 +0100

    Fix shell integration test

--- spring-graalvm-native-samples/function-aws/build.sh
+++ spring-graalvm-native-samples/function-aws/build.sh
@@ -8,10 +8,10 @@ printf "=== ${BLUE}Building %s sample${NC} ===\n" "${PWD##*/}"
 ./compile.sh || exit 1
 
 JARDIR=target/native-image
-docker run --cidfile=.cid -p 9001:9001 -e DOCKER_LAMBDA_STAY_OPEN=1 -v `pwd`/src/test/resources:/var/task lambci/lambda:provided
-SPID=$(cat .cid)
+rm .cid
+docker run --cidfile=.cid -p 9001:9001 -e DOCKER_LAMBDA_STAY_OPEN=1 -v `pwd`/src/test/resources:/var/task lambci/lambda:provided &
 sleep 5
 
 AWS_LAMBDA_RUNTIME_API=localhost:9001 _HANDLER=foobar ${PWD%/*samples/*}/scripts/test.sh 
 
-docker kill $SPID
+docker kill $(cat .cid)
