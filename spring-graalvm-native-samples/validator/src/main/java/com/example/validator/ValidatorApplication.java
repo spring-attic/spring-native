30332553840ecebc0a9bc9554cd12b626c6448e5
Dave Syer <dsyer@vmware.com>
2020-09-25 14:01:54 +0100

    Add a simple sample using @Validated

---
+++ spring-graalvm-native-samples/validator/src/main/java/com/example/validator/ValidatorApplication.java
@@ -0,0 +1,75 @@
+package com.example.validator;
+
+import javax.validation.Validator;
+import javax.validation.constraints.Pattern;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.validation.annotation.Validated;
+
+@SpringBootApplication(proxyBeanMethods = false)
+@EnableConfigurationProperties(Foo.class)
+public class ValidatorApplication {
+
+	public ValidatorApplication(Validator validator, Foo foo) {
+		System.err.println("Valid: " + validator.validate(foo));
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		SpringApplication.run(ValidatorApplication.class, args);
+		Thread.currentThread().join(); // To be able to measure memory consumption
+	}
+
+	@Bean
+	public static BeanDefinitionRegistryPostProcessor ripper() {
+		return new BeanDefinitionRegistryPostProcessor() {
+
+			@Override
+			public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+			}
+
+			@Override
+			public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
+				if (registry.containsBeanDefinition("methodValidationPostProcessor")) {
+					registry.removeBeanDefinition("methodValidationPostProcessor");
+				}
+			}
+
+		};
+	}
+
+}
+
+@ConfigurationProperties("app")
+@Validated
+class Foo {
+	@Pattern(regexp = "[A-Z][a-z]+", message = "Invalid lastname")
+	private String value;
+
+	public Foo() {
+	}
+
+	public Foo(String value) {
+		this.value = value;
+	}
+
+	public String getValue() {
+		return this.value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	@Override
+	public String toString() {
+		return "Foo [value=" + this.value + "]";
+	}
+}
---
+++ spring-graalvm-native-samples/validator/src/test/java/com/example/validator/ValidatorApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.validator;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ValidatorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
