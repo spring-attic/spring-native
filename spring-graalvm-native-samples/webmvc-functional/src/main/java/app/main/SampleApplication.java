bfcb24666c7f5c61b933aa54cecaae8d1ce667de
Dave Syer <dsyer@vmware.com>
2020-09-28 13:14:12 +0100

    Remove unused code

--- spring-graalvm-native-samples/webmvc-functional/src/main/java/app/main/SampleApplication.java
+++ spring-graalvm-native-samples/webmvc-functional/src/main/java/app/main/SampleApplication.java
@@ -1,26 +1,21 @@
 package app.main;
 
+import static org.springframework.web.servlet.function.RouterFunctions.route;
+import static org.springframework.web.servlet.function.ServerResponse.ok;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.context.ConfigurationPropertiesAutoConfiguration;
 import org.springframework.boot.autoconfigure.context.PropertyPlaceholderAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.servlet.ServletWebServerFactoryAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.core.Ordered;
-import org.springframework.core.env.Environment;
-import org.springframework.init.func.InfrastructureInitializer;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
-
-import static org.springframework.web.servlet.function.RouterFunctions.route;
-import static org.springframework.web.servlet.function.ServerResponse.ok;
 
 @SpringBootConfiguration
 @ImportAutoConfiguration({ PropertyPlaceholderAutoConfiguration.class, ConfigurationPropertiesAutoConfiguration.class,
@@ -36,19 +31,6 @@ public class SampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleApplication.class,args);
-		/*
-		SpringApplication app = new SpringApplicationBuilder(SampleApplication.class)
-				.initializers(new InfrastructureInitializer(Ordered.HIGHEST_PRECEDENCE).binding(ServerProperties.class, SampleApplication::bind))
-				.build();
-		app.run(args);
-		*/
-	}
-
-	static ServerProperties bind(ServerProperties bean, Environment environment) {
-		bean.getServlet().setRegisterDefaultServlet(false);
-		bean.setPort(environment.getProperty("server.port", Integer.class,
-				environment.getProperty("SERVER_PORT", Integer.class, 8080)));
-		return bean;
 	}
 
 }
