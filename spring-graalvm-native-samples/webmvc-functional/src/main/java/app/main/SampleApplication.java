02caf0b4be9d0e0390abe377994bb02b525f36cf
Dave Syer <dsyer@vmware.com>
2020-09-28 13:45:26 +0100

    Update webmvc-functional to work with @SpringBootApplication

--- spring-graalvm-native-samples/webmvc-functional/src/main/java/app/main/SampleApplication.java
+++ spring-graalvm-native-samples/webmvc-functional/src/main/java/app/main/SampleApplication.java
@@ -4,24 +4,12 @@ import static org.springframework.web.servlet.function.RouterFunctions.route;
 import static org.springframework.web.servlet.function.ServerResponse.ok;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
-import org.springframework.boot.autoconfigure.context.ConfigurationPropertiesAutoConfiguration;
-import org.springframework.boot.autoconfigure.context.PropertyPlaceholderAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.servlet.ServletWebServerFactoryAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 
-@SpringBootConfiguration
-@ImportAutoConfiguration({ PropertyPlaceholderAutoConfiguration.class, ConfigurationPropertiesAutoConfiguration.class,
-		ServletWebServerFactoryAutoConfiguration.class, WebMvcAutoConfiguration.class,
-		DispatcherServletAutoConfiguration.class, ErrorMvcAutoConfiguration.class })
-@ComponentScan
+@SpringBootApplication(proxyBeanMethods = false)
 public class SampleApplication {
 
 	@Bean
