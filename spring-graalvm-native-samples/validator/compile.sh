45f869e3c97bb5ce011b5e2a1ae0242efeea9fc7
Dave Syer <dsyer@vmware.com>
2020-10-01 17:05:16 +0100

    Dump json in validator sample

--- spring-graalvm-native-samples/validator/compile.sh
+++ spring-graalvm-native-samples/validator/compile.sh
@@ -31,6 +31,7 @@ echo "Compiling $ARTIFACT with $GRAALVM_VERSION"
   -H:Name=$ARTIFACT \
   -Dspring.spel.ignore=true \
   -Dspring.native.remove-yaml-support=true \
+  -Dspring.native.dump-config=dump.json \
   -cp $CP $MAINCLASS >> output.txt ; } 2>> output.txt
 
 if [[ -f $ARTIFACT ]]
