11a6e99bec90568d3188d365158ce91bca406cd7
Eleftheria Stein <eleftheria.kousathana@gmail.com>
2020-10-21 09:53:58 +0200

    Update Spring Security sample

---
+++ spring-graalvm-native-samples/security/src/main/java/com/example/securingweb/MainController.java
@@ -0,0 +1,27 @@
+package com.example.securingweb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String index() {
+        return "home";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
