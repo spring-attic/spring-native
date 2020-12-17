2ec1c040785a70322e2f4a55a5b65cf437a3e68d
Brian Clozel <bclozel@vmware.com>
2020-12-17 19:16:50 +0100

    Introduce TypeName for the TypeSystem
    Prior to this commit, the `TypeSystem` would expose many variants of
    methods for resolving `Types`s:
    
    * the className (or dotted name), `java.lang.String[]`
    * the slashName, `java/lang/String[]`
    * the typeSignature (or Lname), `[Ljava/lang/String;`
    
    This commit moves those concepts to a single `TypeName` class and a new
    `resolve(TypeName)` method on the `TypeSystem`. Other variants are now
    deprecated.

---
+++ spring-graalvm-native-feature/src/test/java/org/springframework/nativex/type/TypeNameTests.java
@@ -0,0 +1,69 @@
+package org.springframework.nativex.type;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests for {@link TypeName}
+ *
+ * @author Brian Clozel
+ */
+class TypeNameTests {
+
+	@ParameterizedTest
+	@MethodSource("provideTypeNames")
+	void shouldParseClassName(String className, String slashName, String typeSignature) {
+		TypeName typeName = TypeName.fromClassName(className);
+		assertThat(typeName.toSlashName()).isEqualTo(slashName);
+		assertThat(typeName.toTypeSignature()).isEqualTo(typeSignature);
+	}
+
+	@ParameterizedTest
+	@MethodSource("provideTypeNames")
+	void shouldParseSlashName(String className, String slashName, String typeSignature) {
+		TypeName typeName = TypeName.fromSlashName(slashName);
+		assertThat(typeName.toClassName()).isEqualTo(className);
+		assertThat(typeName.toTypeSignature()).isEqualTo(typeSignature);
+	}
+
+	@ParameterizedTest
+	@MethodSource("provideTypeNames")
+	void shouldParseTypeSignature(String className, String slashName, String typeSignature) {
+		TypeName typeName = TypeName.fromTypeSignature(typeSignature);
+		assertThat(typeName.toClassName()).isEqualTo(className);
+		assertThat(typeName.toSlashName()).isEqualTo(slashName);
+	}
+
+	@Test
+	void shouldGenerateShortName() {
+		TypeName typeName = TypeName.fromClassName("java.lang.String");
+		assertThat(typeName.toShortName()).isEqualTo("jl.String");
+	}
+
+	@Test
+	void shouldExtractSimpleName() {
+		TypeName typeName = TypeName.fromClassName("java.lang.String");
+		assertThat(typeName.toSimpleName()).isEqualTo("String");
+	}
+
+	@Test
+	void shouldExtractPackage() {
+		TypeName typeName = TypeName.fromClassName("java.lang.String");
+		assertThat(typeName.getPackageName()).isEqualTo("java.lang");
+	}
+
+	private static Stream<Arguments> provideTypeNames() {
+		return Stream.of(
+				Arguments.of("java.lang.String", "java/lang/String", "Ljava/lang/String;"),
+				Arguments.of("java.lang.String[]", "java/lang/String[]", "[Ljava/lang/String;"),
+				Arguments.of("java.lang.String[][]", "java/lang/String[][]", "[[Ljava/lang/String;")
+		);
+	}
+
+}
