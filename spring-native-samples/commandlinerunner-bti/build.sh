faf17f8d3d87e8d8d435925491bcb8968cfb0bb9
Andy Clement <aclement@pivotal.io>
2021-01-07 12:10:43 -0800

    use mvn in build.sh

--- spring-native-samples/commandlinerunner-bti/build.sh
+++ spring-native-samples/commandlinerunner-bti/build.sh
@@ -4,4 +4,7 @@ BLUE='\033[0;34m'
 NC='\033[0m'
 
 printf "=== ${BLUE}Building %s sample${NC} ===\n" "${PWD##*/}"
-./compile.sh && ${PWD%/*samples/*}/scripts/test.sh
+mvn -Pnative clean package 2>&1 > output.txt
+mkdir -p target/native-image
+mv output.txt target/native-image/.
+${PWD%/*samples/*}/scripts/test.sh
