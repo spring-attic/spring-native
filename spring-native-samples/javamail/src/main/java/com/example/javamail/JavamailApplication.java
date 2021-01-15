12b84b14341f9b36b9b902d514ecfc1837fad038
Geoffrey Rekier <grekier@gmail.com>
2021-01-15 23:47:14 +0100

    Support for spring-boot-starter-mail

---
+++ spring-native-samples/javamail/src/main/java/com/example/javamail/JavamailApplication.java
@@ -0,0 +1,13 @@
+package com.example.javamail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavamailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavamailApplication.class, args);
+	}
+
+}
