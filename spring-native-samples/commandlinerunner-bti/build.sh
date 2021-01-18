f2ce59e97af53d134ac3479cc631f4e2d93b96ae
Andy Clement <aclement@pivotal.io>
2021-01-18 15:27:36 -0800

    rename native to bti-native

--- spring-native-samples/commandlinerunner-bti/build.sh
+++ spring-native-samples/commandlinerunner-bti/build.sh
@@ -4,7 +4,7 @@ BLUE='\033[0;34m'
 NC='\033[0m'
 
 printf "=== ${BLUE}Building %s sample${NC} ===\n" "${PWD##*/}"
-mvn -Pnative clean package 2>&1 > output.txt
+mvn -Pbti-native clean package 2>&1 > output.txt
 mkdir -p target/native-image
 mv output.txt target/native-image/.
 ${PWD%/*samples/*}/scripts/test.sh
