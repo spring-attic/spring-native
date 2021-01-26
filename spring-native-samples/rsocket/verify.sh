5c757a893d1fd0f4eefa96b00080839238def4db
Sébastien Deleuze <sdeleuze@vmware.com>
2021-01-26 10:07:39 +0100

    Use native version of rsc
    Kudos to @making

--- spring-native-samples/rsocket/verify.sh
+++ spring-native-samples/rsocket/verify.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-if [[ `java -jar /opt/rsc.jar --debug --request --data "{\"origin\":\"Client\",\"interaction\":\"Request\"}" --route request-response tcp://localhost:7000 | grep "Frame => Stream ID: 1 Type: NEXT_COMPLETE Flags: 0b1100000 Length: 81"` ]]; then
-  if [[ `java -jar /opt/rsc.jar --debug --request --data "{\"origin\":\"Client\",\"interaction\":\"Request\"}" --route mono-request-response tcp://localhost:7000 | grep "Frame => Stream ID: 1 Type: NEXT_COMPLETE Flags: 0b1100000 Length: 81"` ]]; then
+if [[ `rsc --debug --request --data "{\"origin\":\"Client\",\"interaction\":\"Request\"}" --route request-response tcp://localhost:7000 | grep "Frame => Stream ID: 1 Type: NEXT_COMPLETE Flags: 0b1100000 Length: 81"` ]]; then
+  if [[ `rsc --debug --request --data "{\"origin\":\"Client\",\"interaction\":\"Request\"}" --route mono-request-response tcp://localhost:7000 | grep "Frame => Stream ID: 1 Type: NEXT_COMPLETE Flags: 0b1100000 Length: 81"` ]]; then
     exit 0
   fi
 else
