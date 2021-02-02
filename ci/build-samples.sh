33c518bf137e014493ce31b294fdc18a8c58da4f
Sébastien Deleuze <sdeleuze@vmware.com>
2021-02-02 11:03:00 +0100

    CI script cleanup
    See gh-226

--- ci/build-samples.sh
+++ ci/build-samples.sh
@@ -1,15 +1,6 @@
 #!/usr/bin/env sh
 
 RC=0
-mkdir /tmp/data
-/usr/bin/mongod --fork --dbpath /tmp/data --logpath /tmp/data/mongod.log
-redis-server --daemonize yes
-sudo -u elasticsearch /usr/share/elasticsearch/bin/elasticsearch -d
-neo4j start
-chown -R mysql:mysql /var/lib/mysql
-chown -R mysql:mysql /var/log/mysql
-chown -R mysql:mysql /var/run/mysqld
-/tmp/mysql.sh
 native-image --version
 /bin/start-docker.sh
 cd spring-native
@@ -19,5 +10,4 @@ fi
 if ! (./build-samples.sh); then
     RC=1
 fi
-mysqladmin shutdown
 exit $RC
