b4801bf7767db07446d106021ca60eb3dba08822
Sébastien Deleuze <sdeleuze@vmware.com>
2021-02-02 11:04:51 +0100

    Log into DockerHub prior to running CI builds
    See gh-226

--- ci/build-key-samples.sh
+++ ci/build-key-samples.sh
@@ -2,6 +2,9 @@
 
 native-image --version
 /bin/start-docker.sh
+if [[ -n $DOCKER_HUB_USERNAME ]]; then
+	docker login -u $DOCKER_HUB_USERNAME -p $DOCKER_HUB_PASSWORD
+fi
 cd spring-native
 ./build.sh
 ./build-key-samples.sh
--- ci/build-samples.sh
+++ ci/build-samples.sh
@@ -3,6 +3,9 @@
 RC=0
 native-image --version
 /bin/start-docker.sh
+if [[ -n $DOCKER_HUB_USERNAME ]]; then
+	docker login -u $DOCKER_HUB_USERNAME -p $DOCKER_HUB_PASSWORD
+fi
 cd spring-native
 if ! (./build.sh); then
     RC=1
