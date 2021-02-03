19446efad9e0eae575bcc50cc46a8e2a4b1907aa
Brian Clozel <bclozel@vmware.com>
2021-02-03 14:13:43 +0100

    Configure native build plugin for testing phase
    Prior to this commit, the build-time infrastructure plugin would be run
    on the "process-classes" phase, so right after source compilation and
    before running the test suite.
    This was too early as we'd generate sources and compile them with the
    assumptions made on the compile+runtime classpath at that point.
    
    Running tests with this set of sources would fail because test
    dependencies would be missing from the picture when bootstrap sources
    were generated.
    
    This commit revisits the execution phases for the native build plugin:
    * one during the "process-test-classes" phase with the test classpath,
    before running the test suite
    * another one during the "prepare-package" phase with the
    compile+runtime classpath before packaging the application.
    
    Fixes gh-456

--- spring-native-maven-plugin/src/main/java/org/springframework/nativex/maven/AbstractBootstrapMojo.java
+++ spring-native-maven-plugin/src/main/java/org/springframework/nativex/maven/AbstractBootstrapMojo.java
@@ -1,15 +1,5 @@
 package org.springframework.nativex.maven;
 
-import static org.twdata.maven.mojoexecutor.MojoExecutor.artifactId;
-import static org.twdata.maven.mojoexecutor.MojoExecutor.configuration;
-import static org.twdata.maven.mojoexecutor.MojoExecutor.element;
-import static org.twdata.maven.mojoexecutor.MojoExecutor.executeMojo;
-import static org.twdata.maven.mojoexecutor.MojoExecutor.executionEnvironment;
-import static org.twdata.maven.mojoexecutor.MojoExecutor.goal;
-import static org.twdata.maven.mojoexecutor.MojoExecutor.groupId;
-import static org.twdata.maven.mojoexecutor.MojoExecutor.plugin;
-import static org.twdata.maven.mojoexecutor.MojoExecutor.version;
-
 import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -33,35 +23,78 @@ import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.project.MavenProject;
 import org.codehaus.plexus.util.xml.Xpp3Dom;
 import org.sonatype.plexus.build.incremental.BuildContext;
+
 import org.springframework.nativex.buildtools.BootstrapCodeGenerator;
+
+import static org.twdata.maven.mojoexecutor.MojoExecutor.artifactId;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.configuration;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.element;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.executeMojo;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.executionEnvironment;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.goal;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.groupId;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.plugin;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.version;
 
 /**
  * @author Brian Clozel
  */
-@Mojo(name = "bootstrap", defaultPhase = LifecyclePhase.PREPARE_PACKAGE, requiresProject = true, threadSafe = true,
-		requiresDependencyResolution = ResolutionScope.COMPILE_PLUS_RUNTIME,
-		requiresDependencyCollection = ResolutionScope.COMPILE_PLUS_RUNTIME)
-public class GenerateBootstrapMojo extends AbstractMojo {
+abstract class AbstractBootstrapMojo extends AbstractMojo {
 
-	private static Log logger = LogFactory.getLog(GenerateBootstrapMojo.class);
+	private static final String DEFAULT_COMPILER_PLUGIN_VERSION = "3.8.1";
+
+	protected static Log logger = LogFactory.getLog(AbstractBootstrapMojo.class);
 
 	@Parameter(defaultValue = "${project}", readonly = true, required = true)
-	private MavenProject project;
+	protected MavenProject project;
 
 	@Parameter(defaultValue = "${session}", readonly = true, required = true)
-	private MavenSession session;
+	protected MavenSession session;
 
 	@Component
-	private BuildContext buildContext;
+	protected BuildContext buildContext;
 
 	@Component
-	private BuildPluginManager pluginManager;
+	protected BuildPluginManager pluginManager;
+
+	@Parameter(defaultValue = "${project.build.directory}")
+	protected File buildDir;
 
 	/**
 	 * The location of the generated bootstrap sources.
 	 */
 	@Parameter(defaultValue = "${project.build.directory}/spring-bootstrap/")
-	private File outputDirectory;
+	protected File outputDirectory;
+
+
+	protected void compileGeneratedSources(Path generatedRootFolder) throws MojoExecutionException {
+		String compilerVersion = this.project.getProperties().getProperty("maven-compiler-plugin.version", DEFAULT_COMPILER_PLUGIN_VERSION);
+		Path sourcePath = generatedRootFolder.resolve(Paths.get("src", "main", "java"));
+		Xpp3Dom compilerConfig = configuration(element("compileSourceRoots", element("compileSourceRoot", sourcePath.toString())));
+		executeMojo(
+				plugin(groupId("org.apache.maven.plugins"), artifactId("maven-compiler-plugin"), version(compilerVersion)),
+				goal("compile"), compilerConfig, executionEnvironment(this.project, this.session, this.pluginManager));
+	}
+
+	protected void compileGeneratedTestSources(Path generatedRootFolder) throws MojoExecutionException {
+		String compilerVersion = this.project.getProperties().getProperty("maven-compiler-plugin.version", DEFAULT_COMPILER_PLUGIN_VERSION);
+		Path sourcePath = generatedRootFolder.resolve(Paths.get("src", "test", "java"));
+		Xpp3Dom compilerConfig = configuration(element("compileSourceRoots", element("compileSourceRoot", sourcePath.toString())));
+		executeMojo(
+				plugin(groupId("org.apache.maven.plugins"), artifactId("maven-compiler-plugin"), version(compilerVersion)),
+				goal("testCompile"), compilerConfig, executionEnvironment(this.project, this.session, this.pluginManager));
+	}
+
+	protected void processGeneratedResources(Path sourcePath, Path destinationPath) throws MojoExecutionException {
+		String resourcesVersion = this.project.getProperties().getProperty("maven-resources-plugin.version", "3.2.0");
+		Xpp3Dom resourceConfig = configuration(element("resources", element("resource", element("directory", sourcePath.toString()))),
+				element("outputDirectory", destinationPath.toString()));
+		executeMojo(
+				plugin(groupId("org.apache.maven.plugins"), artifactId("maven-resources-plugin"), version(resourcesVersion)),
+				goal("copy-resources"), resourceConfig, executionEnvironment(this.project, this.session, this.pluginManager));
+	}
+
+	  /*
 
 	@Override
 	public void execute() throws MojoExecutionException, MojoFailureException {
@@ -97,5 +130,6 @@ public class GenerateBootstrapMojo extends AbstractMojo {
 				goal("copy-resources"), resourceConfig, executionEnvironment(this.project, this.session, this.pluginManager));
 		this.buildContext.refresh(this.outputDirectory);
 	}
+	   */
 
 }
---
+++ spring-native-maven-plugin/src/main/java/org/springframework/nativex/maven/BootstrapTestsMojo.java
@@ -0,0 +1,59 @@
+package org.springframework.nativex.maven;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.maven.model.Resource;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+
+import org.springframework.nativex.buildtools.BootstrapCodeGenerator;
+
+/**
+ * @author Brian Clozel
+ */
+@Mojo(name = "bootstrapTests", defaultPhase = LifecyclePhase.PROCESS_TEST_CLASSES, requiresProject = true, threadSafe = true,
+		requiresDependencyResolution = ResolutionScope.TEST,
+		requiresDependencyCollection = ResolutionScope.TEST)
+public class BootstrapTestsMojo extends AbstractBootstrapMojo {
+
+	/**
+	 * The location of the generated bootstrap test sources.
+	 */
+	@Parameter(defaultValue = "${project.build.directory}/spring-bootstrap-test/")
+	private File outputDirectory;
+
+	@Override
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		Set<Path> resourceFolders = new HashSet<>();
+		for (Resource r: project.getTestResources()) {
+			// TODO respect includes/excludes
+			resourceFolders.add(new File(r.getDirectory()).toPath());
+		}
+		try {
+			BootstrapCodeGenerator generator = new BootstrapCodeGenerator();
+			generator.generate(Paths.get(this.outputDirectory.toURI()), this.project.getTestClasspathElements(), resourceFolders);
+		}
+		catch (Throwable exc) {
+			logger.error(exc);
+			logger.error(Arrays.toString(exc.getStackTrace()));
+			throw new MojoExecutionException("Could not generate test source files", exc);
+		}
+
+		compileGeneratedTestSources(this.outputDirectory.toPath());
+
+		Path resourcePath = this.outputDirectory.toPath().resolve(Paths.get("src", "test", "resources"));
+		processGeneratedResources(resourcePath, Paths.get(project.getBuild().getOutputDirectory()));
+
+		this.buildContext.refresh(this.buildDir);
+	}
+
+}
