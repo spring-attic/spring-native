e6f589669eaaee8e9f5ef172bb05dd823bf943ce
Brian Clozel <bclozel@vmware.com>
2021-02-08 11:42:58 +0100

    Fix SpringFactoriesLoader substitution
    Prior to this commit, the SpringFactoriesLoader substitution would
    override the `loadFactoryNames` in a way that would only consider one
    of:
    
    * "regular" factories with a default constructor that we can instantiate
    directly
    * factories with no default constructor requiring a specific dependeny
    injection mechanism at runtime
    
    In some cases, the application contributes factories of both types and
    we need to consider them all.
    
    See gh-466

--- spring-native-configuration/src/main/java/org/springframework/core/io/support/Target_SpringFactoriesLoader.java
+++ spring-native-configuration/src/main/java/org/springframework/core/io/support/Target_SpringFactoriesLoader.java
@@ -1,19 +1,17 @@
 package org.springframework.core.io.support;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.function.Supplier;
-import java.util.stream.Collectors;
-
-import org.apache.commons.logging.Log;
-import org.springframework.lang.Nullable;
-import org.springframework.nativex.substitutions.WithBuildtools;
-import org.springframework.util.Assert;
 
 import com.oracle.svm.core.annotate.Alias;
 import com.oracle.svm.core.annotate.Substitute;
 import com.oracle.svm.core.annotate.TargetClass;
+import org.apache.commons.logging.Log;
+
+import org.springframework.lang.Nullable;
+import org.springframework.nativex.substitutions.WithBuildtools;
+import org.springframework.util.Assert;
 
 @TargetClass(className="org.springframework.core.io.support.SpringFactoriesLoader", onlyWith = { WithBuildtools.class, OnlyIfPresent.class })
 final class Target_SpringFactoriesLoader {
@@ -25,36 +23,31 @@ final class Target_SpringFactoriesLoader {
 	@Substitute
 	public static <T> List<T> loadFactories(Class<T> factoryType, @Nullable ClassLoader classLoader) {
 		Assert.notNull(factoryType, "'factoryType' must not be null");
-		List<Supplier<Object>> result = (List<Supplier<Object>>) Target_StaticSpringFactories.factories.get(factoryType);
+		List<Supplier<Object>> result = Target_StaticSpringFactories.factories.get(factoryType);
 		if (result == null) {
 			return new ArrayList<>(0);
 		}
-		List<T> facs = new ArrayList<>();
+		List<T> factories = new ArrayList<>(result.size());
 		for (Supplier<Object> supplier: result) {
-			facs.add((T) supplier.get());
+			factories.add((T) supplier.get());
 		}
-		return facs;
-//		return (List<T>) result.stream().map(Supplier::get).collect(Collectors.toList());
+		return factories;
 	}
 
 	@Substitute
 	public static List<String> loadFactoryNames(Class<?> factoryType, @Nullable ClassLoader classLoader) {
+		List<String> result = new ArrayList<>();
 		List<String> names = Target_StaticSpringFactories.names.get(factoryType);
 		if (names != null) {
-			return names;
+			result.addAll(names);
 		}
-		else {
-			List<Supplier<Object>> stored = Target_StaticSpringFactories.factories.get(factoryType);
-			if (stored == null) {
-				return Collections.emptyList();
-			}
-			List<String> result = new ArrayList<>();
+		List<Supplier<Object>> stored = Target_StaticSpringFactories.factories.get(factoryType);
+		if (stored != null) {
 			for (Supplier<Object> supplier: stored) {
 				result.add(supplier.get().getClass().getName());
 			}
-			return result;
-//			return stored.stream().map(Supplier::get).map(factory -> factory.getClass().getName()).collect(Collectors.toList());
 		}
+		return result;
 	}
 
 }
--- spring-native-substitutions/src/main/java/org/springframework/core/io/support/Target_SpringFactoriesLoader.java
+++ spring-native-substitutions/src/main/java/org/springframework/core/io/support/Target_SpringFactoriesLoader.java
@@ -1,20 +1,18 @@
 package org.springframework.core.io.support;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.function.Supplier;
-import java.util.stream.Collectors;
-
-import org.apache.commons.logging.Log;
-import org.springframework.lang.Nullable;
-import org.springframework.nativex.substitutions.OnlyIfPresent;
-import org.springframework.nativex.substitutions.WithBuildtools;
-import org.springframework.util.Assert;
 
 import com.oracle.svm.core.annotate.Alias;
 import com.oracle.svm.core.annotate.Substitute;
 import com.oracle.svm.core.annotate.TargetClass;
+import org.apache.commons.logging.Log;
+
+import org.springframework.lang.Nullable;
+import org.springframework.nativex.substitutions.OnlyIfPresent;
+import org.springframework.nativex.substitutions.WithBuildtools;
+import org.springframework.util.Assert;
 
 @TargetClass(className="org.springframework.core.io.support.SpringFactoriesLoader", onlyWith = { WithBuildtools.class, OnlyIfPresent.class })
 final class Target_SpringFactoriesLoader {
@@ -22,40 +20,35 @@ final class Target_SpringFactoriesLoader {
 	@Alias
 	private static Log logger;
 
+	@SuppressWarnings("unchecked")
 	@Substitute
 	public static <T> List<T> loadFactories(Class<T> factoryType, @Nullable ClassLoader classLoader) {
 		Assert.notNull(factoryType, "'factoryType' must not be null");
-		List<Supplier<Object>> result = (List<Supplier<Object>>) Target_StaticSpringFactories.factories.get(factoryType);
+		List<Supplier<Object>> result = Target_StaticSpringFactories.factories.get(factoryType);
 		if (result == null) {
-			return Collections.emptyList();
+			return new ArrayList<>(0);
 		}
-		List<Object> list = new ArrayList<>();
-		for (Supplier<Object> objectSupplier : result) {
-			Object o = objectSupplier.get();
-			list.add(o);
+		List<T> factories = new ArrayList<>(result.size());
+		for (Supplier<Object> supplier: result) {
+			factories.add((T) supplier.get());
 		}
-		return (List<T>) list;
+		return factories;
 	}
 
 	@Substitute
 	public static List<String> loadFactoryNames(Class<?> factoryType, @Nullable ClassLoader classLoader) {
+		List<String> result = new ArrayList<>();
 		List<String> names = Target_StaticSpringFactories.names.get(factoryType);
 		if (names != null) {
-			return names;
+			result.addAll(names);
 		}
-		else {
-			List<Supplier<Object>> stored = Target_StaticSpringFactories.factories.get(factoryType);
-			if (stored == null) {
-				return Collections.emptyList();
+		List<Supplier<Object>> stored = Target_StaticSpringFactories.factories.get(factoryType);
+		if (stored != null) {
+			for (Supplier<Object> supplier: stored) {
+				result.add(supplier.get().getClass().getName());
 			}
-			List<String> list = new ArrayList<>();
-			for (Supplier<Object> objectSupplier : stored) {
-				Object factory = objectSupplier.get();
-				String name = factory.getClass().getName();
-				list.add(name);
-			}
-			return list;
 		}
+		return result;
 	}
 
 }
