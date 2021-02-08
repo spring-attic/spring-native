504f3c91e1b46a45dd0efb10a2cb732d68f9bfc0
Sébastien Deleuze <sdeleuze@vmware.com>
2021-02-08 10:35:52 +0100

    Use hints in webflux-thymeleaf sample

--- spring-native-samples/webflux-thymeleaf/src/main/java/hello/Application.java
+++ spring-native-samples/webflux-thymeleaf/src/main/java/hello/Application.java
@@ -2,7 +2,11 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.nativex.extension.NativeHint;
+import org.springframework.nativex.extension.TypeInfo;
+import org.springframework.nativex.type.AccessBits;
 
+@NativeHint(typeInfos = @TypeInfo(types = Greeting.class, access = AccessBits.LOAD_AND_CONSTRUCT | AccessBits.PUBLIC_METHODS))
 @SpringBootApplication
 public class Application {
 
