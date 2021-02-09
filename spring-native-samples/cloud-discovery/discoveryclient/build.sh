926a6f7ff40c3046e4ab79d03bb7db3a10adfd2a
Sébastien Deleuze <sdeleuze@vmware.com>
2021-02-09 11:01:14 +0100

    Polishing

--- spring-native-samples/cloud-discovery/discoveryclient/build.sh
+++ spring-native-samples/cloud-discovery/discoveryclient/build.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 
-BLUE='\033[0;34m'
-NC='\033[0m'
-
 # Build the server
 cd ../discoveryserver
 mvn clean package
 cd ../discoveryclient
 
-printf "=== ${BLUE}Building %s sample${NC} ===\n" "${PWD##*/}"
 ${PWD%/*samples/*}/scripts/compileWithMaven.sh || exit 1
 
 java -jar ../discoveryserver/target/discoveryserver-0.0.1-SNAPSHOT.jar &
