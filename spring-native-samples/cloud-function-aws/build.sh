8063eb52d6369dec76df8506e88cdedef6bbd1a3
Sébastien Deleuze <sdeleuze@vmware.com>
2021-02-09 13:44:25 +0100

    Using new bti structure in cloud-function-aws
    This sample is now using docker-compose like other ones.
    
    See gh-466

--- spring-native-samples/cloud-function-aws/build.sh
+++ spring-native-samples/cloud-function-aws/build.sh
@@ -1,18 +1,7 @@
 #!/usr/bin/env bash
+set -e
 
-BLUE='\033[0;34m'
-NC='\033[0m'
-
-printf "=== ${BLUE}Building %s sample${NC} ===\n" "${PWD##*/}"
-
-./compile.sh || exit 1
-
-JARDIR=target/native-image
-rm -f target/.cid
-echo -n "" > target/docker.log
-docker run --cidfile=target/.cid -p 9001:9001 -e DOCKER_LAMBDA_STAY_OPEN=1 -v `pwd`/src/test/resources:/var/task lambci/lambda:provided >target/docker.log 2>&1  &
-tail -f target/docker.log | sed '/Lambda API listening on port 9001/ q'
-
-AWS_LAMBDA_RUNTIME_API=localhost:9001 _HANDLER=foobar ${PWD%/*samples/*}/scripts/test.sh 
-
-docker kill $(cat target/.cid)
+docker-compose up -d lambda
+${PWD%/*samples/*}/scripts/compileWithMaven.sh
+AWS_LAMBDA_RUNTIME_API=localhost:9001 _HANDLER=foobar ${PWD%/*samples/*}/scripts/test.sh
+docker-compose down
