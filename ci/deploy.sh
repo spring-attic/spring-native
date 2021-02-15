8964c08b2628343a27f69fcd6af0405aba3caaa3
Sébastien Deleuze <sdeleuze@vmware.com>
2021-02-15 08:16:30 +0100

    Deploy Javadoc

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -3,4 +3,4 @@
 set -e
 
 cd spring-native
-./mvnw -ntp deploy -P artifactory,docs -Dartifactory.username=$ARTIFACTORY_USERNAME -Dartifactory.password=$ARTIFACTORY_PASSWORD
+./mvnw -ntp javadoc:javadoc deploy -P artifactory,docs -Dartifactory.username=$ARTIFACTORY_USERNAME -Dartifactory.password=$ARTIFACTORY_PASSWORD
