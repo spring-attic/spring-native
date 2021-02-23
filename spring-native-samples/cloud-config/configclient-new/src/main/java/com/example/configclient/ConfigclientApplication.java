657042cbc52b96962b779e8b01f129c625769da5
spencergibb <spencer@gibb.us>
2021-02-23 18:11:48 -0500

    Removes logback and example reflection

--- spring-native-samples/cloud-config/configclient-new/src/main/java/com/example/configclient/ConfigclientApplication.java
+++ spring-native-samples/cloud-config/configclient-new/src/main/java/com/example/configclient/ConfigclientApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
@@ -17,16 +18,9 @@ public class ConfigclientApplication {
 		} catch (Exception e) {
 		}
 	}
-}
 
-@Configuration
-class CLR implements CommandLineRunner {
-
-	@Value("${my.prop}") String injected;
-
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println(":" + injected);
+	@Bean
+	CommandLineRunner commandLineRunner(@Value("${my.prop}") String injected) {
+		return args -> System.out.println(":" + injected);
 	}
-
 }
