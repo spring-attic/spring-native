fde3a2ac79586ad966fed65a8b91ebbee8d4f26b
Sébastien Deleuze <sdeleuze@vmware.com>
2021-02-24 18:05:24 +0100

    Improve petclinic-jdbc verify.sh

--- spring-native-samples/petclinic-jdbc/verify.sh
+++ spring-native-samples/petclinic-jdbc/verify.sh
@@ -1,7 +1,23 @@
 #!/usr/bin/env bash
 RESPONSE=`curl -s localhost:8080`
-if [[ $RESPONSE == *"Welcome"* ]]; then
- exit 0
-else
+if [[ $RESPONSE != *"Welcome"* ]]; then
  exit 1
 fi
+
+RESPONSE=`curl -s http://localhost:8080/owners/7`
+if [[ $RESPONSE != *"Jeff Black"* ]]; then
+ exit 2
+fi
+
+RESPONSE=`curl -s http://localhost:8080/vets.html`
+if [[ $RESPONSE != *"James Carter"* ]]; then
+ exit 3
+fi
+
+RESPONSE=`curl -Ls "http://localhost:8080/owners?lastName=Frank"`
+if [[ $RESPONSE != *"George Franklin"* ]]; then
+ exit 4
+fi
+
+exit 0
+
