f22d1212f491efa830994e9fe051810e3394181c
spencergibb <spencer@gibb.us>
2021-02-25 16:14:13 -0500

    Adds EncryptionBootstrapConfiguration to BootstrapHints

--- spring-native-configuration/src/main/java/org/springframework/cloud/bootstrap/BootstrapHints.java
+++ spring-native-configuration/src/main/java/org/springframework/cloud/bootstrap/BootstrapHints.java
@@ -3,6 +3,7 @@ package org.springframework.cloud.bootstrap;
 import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
 import org.springframework.cloud.bootstrap.config.PropertySourceBootstrapConfiguration;
 import org.springframework.cloud.bootstrap.config.PropertySourceLocator;
+import org.springframework.cloud.bootstrap.encrypt.EncryptionBootstrapConfiguration;
 import org.springframework.nativex.hint.AccessBits;
 import org.springframework.nativex.hint.NativeHint;
 import org.springframework.nativex.hint.TypeHint;
@@ -11,6 +12,6 @@ import org.springframework.nativex.type.NativeConfiguration;
 @NativeHint(trigger = RefreshAutoConfiguration.class, types = {
 		@TypeHint(types = {BootstrapImportSelectorConfiguration.class}, access = AccessBits.ALL)}, follow = true)
 @NativeHint(trigger = BootstrapImportSelector.class, types =
-		{@TypeHint(types = {PropertySourceBootstrapConfiguration.class, PropertySourceLocator.class})}, follow = true)
+		{@TypeHint(types = {PropertySourceBootstrapConfiguration.class, PropertySourceLocator.class, EncryptionBootstrapConfiguration.class})}, follow = true)
 public class BootstrapHints implements NativeConfiguration {
 }
