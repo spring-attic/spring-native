b09953ad714583a262bb218766a268f951ce6085
spencergibb <spencer@gibb.us>
2021-02-25 11:06:55 -0500

    Removes native-image json files from configclient-new

--- samples/cloud-config/configclient-new/src/main/java/com/example/configclient/ConfigclientApplication.java
+++ samples/cloud-config/configclient-new/src/main/java/com/example/configclient/ConfigclientApplication.java
@@ -4,29 +4,19 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ConfigclientApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigclientApplication.class, args);
-		try {
-			Thread.sleep(20000);
-		} catch (Exception e) {
-		}
-	}
-}
-
-@Configuration
-class CLR implements CommandLineRunner {
-
-	@Value("${my.prop}") String injected;
-
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println(":" + injected);
+		try { Thread.sleep(20000); } catch (Exception e) {}
 	}
 
+	@Bean
+	CommandLineRunner commandLineRunner(@Value("${my.prop}") String injected) {
+		return args -> System.out.println(":" + injected);
+	}
 }
