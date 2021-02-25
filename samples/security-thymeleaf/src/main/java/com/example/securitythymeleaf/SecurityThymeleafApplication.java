0a22b1ad8eedb72e88996c00ed627e6589a1af62
Eleftheria Stein <eleftheria.kousathana@gmail.com>
2021-02-25 11:51:22 +0100

    Support Thymeleaf extras for Spring Security 5
    Closes gh-516

---
+++ samples/security-thymeleaf/src/main/java/com/example/securitythymeleaf/SecurityThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.securitythymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityThymeleafApplication {
+
+	public static void main(String[] args) throws Throwable {
+		SpringApplication.run(SecurityThymeleafApplication.class, args);
+	}
+
+}
