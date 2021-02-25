5764034156c0437479555f0b25a0ec258c992ab1
spencergibb <spencer@gibb.us>
2021-02-25 16:06:13 -0500

    Adds PropertySourceLocator to BootstrapHints

--- spring-native-configuration/src/main/java/org/springframework/cloud/bootstrap/BootstrapHints.java
+++ spring-native-configuration/src/main/java/org/springframework/cloud/bootstrap/BootstrapHints.java
@@ -2,13 +2,15 @@ package org.springframework.cloud.bootstrap;
 
 import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
 import org.springframework.cloud.bootstrap.config.PropertySourceBootstrapConfiguration;
+import org.springframework.cloud.bootstrap.config.PropertySourceLocator;
 import org.springframework.nativex.hint.AccessBits;
 import org.springframework.nativex.hint.NativeHint;
 import org.springframework.nativex.hint.TypeHint;
 import org.springframework.nativex.type.NativeConfiguration;
 
-@NativeHint(trigger= RefreshAutoConfiguration.class, types= {
-		@TypeHint(types={BootstrapImportSelectorConfiguration.class}, access=AccessBits.ALL)},follow=true)
-@NativeHint(trigger = BootstrapImportSelector.class, types = {@TypeHint(types = {PropertySourceBootstrapConfiguration.class})}, follow = true)
+@NativeHint(trigger = RefreshAutoConfiguration.class, types = {
+		@TypeHint(types = {BootstrapImportSelectorConfiguration.class}, access = AccessBits.ALL)}, follow = true)
+@NativeHint(trigger = BootstrapImportSelector.class, types =
+		{@TypeHint(types = {PropertySourceBootstrapConfiguration.class, PropertySourceLocator.class})}, follow = true)
 public class BootstrapHints implements NativeConfiguration {
 }
