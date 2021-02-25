b46d7b019f5ff57cf7ea199024ca9a4fe6a5a331
spencergibb <spencer@gibb.us>
2021-02-25 16:35:13 -0500

    Adds DecryptEnvironmentPostProcessor to BootstrapHints

--- spring-native-configuration/src/main/java/org/springframework/cloud/bootstrap/BootstrapHints.java
+++ spring-native-configuration/src/main/java/org/springframework/cloud/bootstrap/BootstrapHints.java
@@ -3,6 +3,7 @@ package org.springframework.cloud.bootstrap;
 import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
 import org.springframework.cloud.bootstrap.config.PropertySourceBootstrapConfiguration;
 import org.springframework.cloud.bootstrap.config.PropertySourceLocator;
+import org.springframework.cloud.bootstrap.encrypt.DecryptEnvironmentPostProcessor;
 import org.springframework.cloud.bootstrap.encrypt.EncryptionBootstrapConfiguration;
 import org.springframework.cloud.bootstrap.encrypt.EnvironmentDecryptApplicationInitializer;
 import org.springframework.nativex.hint.AccessBits;
@@ -11,7 +12,7 @@ import org.springframework.nativex.hint.TypeHint;
 import org.springframework.nativex.type.NativeConfiguration;
 
 @NativeHint(trigger = RefreshAutoConfiguration.class, types = {
-		@TypeHint(types = {BootstrapImportSelectorConfiguration.class, EnvironmentDecryptApplicationInitializer.class}, access = AccessBits.ALL)}, follow = true)
+		@TypeHint(types = {BootstrapImportSelectorConfiguration.class, EnvironmentDecryptApplicationInitializer.class, DecryptEnvironmentPostProcessor.class}, access = AccessBits.ALL)}, follow = true)
 @NativeHint(trigger = BootstrapImportSelector.class, types =
 		{@TypeHint(types = {PropertySourceBootstrapConfiguration.class, PropertySourceLocator.class, EncryptionBootstrapConfiguration.class})}, follow = true)
 public class BootstrapHints implements NativeConfiguration {
