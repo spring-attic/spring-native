eb716c42f66cdd9780e53345a648a7570c3f1ba5
Eleftheria Stein <eleftheria.kousathana@gmail.com>
2021-02-25 11:51:22 +0100

    Support Thymeleaf extras for Spring Security 5
    Closes gh-516

---
+++ samples/security-thymeleaf/src/main/java/com/example/securitythymeleaf/WebSecurityConfig.java
@@ -0,0 +1,40 @@
+package com.example.securitythymeleaf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests(authorize -> authorize
+				.antMatchers("/").permitAll()
+				.anyRequest().authenticated()
+			)
+			.formLogin(withDefaults());
+	}
+
+	@Bean
+	@Override
+	public UserDetailsService userDetailsService() {
+		UserDetails user =
+			 User.withDefaultPasswordEncoder()
+				.username("user")
+				.password("password")
+				.roles("USER")
+				.build();
+
+		return new InMemoryUserDetailsManager(user);
+	}
+}
