75814f69d604ab3c5c163ae53928e79c6fb5c394
spencergibb <spencer@gibb.us>
2021-02-25 14:09:09 -0500

    Adds support for BootstrapImportSelector to BootstrapHints

--- spring-native-configuration/src/main/java/org/springframework/cloud/bootstrap/BootstrapHints.java
+++ spring-native-configuration/src/main/java/org/springframework/cloud/bootstrap/BootstrapHints.java
@@ -1,10 +1,14 @@
 package org.springframework.cloud.bootstrap;
 
+import org.springframework.cloud.autoconfigure.RefreshAutoConfiguration;
+import org.springframework.cloud.bootstrap.config.PropertySourceBootstrapConfiguration;
 import org.springframework.nativex.hint.AccessBits;
 import org.springframework.nativex.hint.NativeHint;
 import org.springframework.nativex.hint.TypeHint;
 import org.springframework.nativex.type.NativeConfiguration;
 
-@NativeHint(trigger = BootstrapApplicationListener.class, types = {@TypeHint(types = {BootstrapImportSelectorConfiguration.class}, access = AccessBits.ALL)})
+@NativeHint(trigger= RefreshAutoConfiguration.class, types= {
+		@TypeHint(types={BootstrapImportSelectorConfiguration.class}, access=AccessBits.ALL)},follow=true)
+@NativeHint(trigger = BootstrapImportSelector.class, types = {@TypeHint(types = {PropertySourceBootstrapConfiguration.class})}, follow = true)
 public class BootstrapHints implements NativeConfiguration {
 }
