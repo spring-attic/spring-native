2015af66483dbb6194ad4f901af4dfb22b9f3302
Andy Clement <aclement@pivotal.io>
2021-03-01 14:29:40 -0800

    Javadoc on the options for future reference

--- spring-aot/src/main/java/org/springframework/nativex/AotOptions.java
+++ spring-aot/src/main/java/org/springframework/nativex/AotOptions.java
@@ -1,24 +1,62 @@
 package org.springframework.nativex;
 
+import java.beans.beancontext.BeanContext;
+
 import org.springframework.nativex.support.Mode;
 
 /**
  * Options for Spring Native.
  *
  * @author Sebastien Deleuze
+ * @author Andy Clement
  */
 public class AotOptions {
 
 	private String mode = Mode.NATIVE.toString();
 
+	/**
+	 * Determine if extra debug should come out related to verification of reflection requests.
+	 * Spring Native will compute configuration but a final stage verification runs before it is
+	 * written out to try and ensure what is requested will not lead to problems when the
+	 * native-image is built. This flag enables debugging of that final stage verification.
+	 */
 	private boolean debugVerify;
 
+	/**
+	 * Determine whether configuration should be computed for any configuration explicitly excluded
+	 * via an explicit <tt>spring.autoconfigure.exclude</tt> in
+	 * <tt>application*.properties</tt> or <tt>application*.yml</tt>.
+	 */
 	private boolean ignoreHintsOnExcludedConfig = true;
 
+	/**
+	 * Determine whether it is safe to discard native-image configuration related to auto-configurations
+	 * at build time, where it can be determined those auto-configurations will not be active at runtime
+	 * (for example if they use a ConditionalOnClass annotation and the class referenced is not on the
+	 * classpath). It is safe to discard this configuration as long as the AOT processing is also making
+	 * the same decision about what it discards when creating spring factories code that represents
+	 * <tt>spring.factories</tt> content.
+	 */
 	private boolean removeUnusedConfig = true;
 
+	/**
+	 * Determine if the system should fast fail if a hint is missing for an import selector.
+	 * Spring Native tries to follow configuration chains (configuration that imports other configuration)
+	 * but it struggles when it hits an import selector, since that uses code to determine the next
+	 * link in the chain. If a hint is missing to provide this next link in the chain, the system fails
+	 * immediately, by default, rather than limping along and potentially failing in a very crypic way
+	 * later. 
+	 */
 	private boolean failOnMissingSelectorHint = true;
 
+	/**
+	 * Determine whether to check Spring components are suitable for inclusion in a native-image.
+	 * For a concrete example, verification currently checks whether bean factory methods are being
+	 * invoked directly in configuration classes. If they are then that must be enforced by using
+	 * CGLIB proxies. CGLIB proxies are not supported in native-image and so a verification error
+	 * will come out with a message indicating the code should switch to method parameter
+	 * injection (allowing Spring to control bean method invocation).
+	 */
 	private boolean verify = true;
 
 	private boolean removeYamlSupport;
@@ -29,10 +67,50 @@ public class AotOptions {
 
 	private boolean removeSpelSupport;
 
+	/**
+	 * When performing build time properties checks (via build-time-properties-checks option) this 
+	 * determines how to treat the <tt>match-if-missing</tt> constraint that can be specified in
+	 * conditional on property annotations. This option means nothing unless activating build time
+	 * property checking. Without this ability to override the behavior, any condition on a
+	 * property that specified match-if-missing is true could not benefit from build time property checking if the 
+	 * property is not set because match if missing says the condition should pass even if it isn't there.
+	 * 
+	 * Both this and the build time property checking option are experimental as we explore what it
+	 * means to do build time property checking.
+	 */
 	private boolean buildTimePropertiesMatchIfMissing = true;
 
+	/**
+	 * Controls which properties can be evaluated at build time. Checking properties at build time
+	 * can cause configurations using property connected conditions like @ConditionalOnProperty to
+	 * be discarded early, producing a more optimal image and runtime behaviour.
+	 * 
+	 * This should be set to an initial control value that specifies the default, then a series of values that
+	 * tweak that default for certain properties.
+	 * 
+	 * The two default settings are: 
+	 * <ul>
+	 * <li> <tt>default-include-all</tt> indicates by default, unless tweaked otherwise, <b>all</b> properties are safe
+	 * to evaluate at build time.</li>
+	 * <li> <tt>default-exclude-all</tt> indicates by default, unless tweaked otherwise, <b>no</b> properties are safe
+	 * to evaluate at build time.</li>
+	 * </ul>
+	 * 
+	 * Following the default behavior, the tweaks look like a series of property names (or just prefixes) with
+	 * an optional ! (not) operator on the front.
+	 * 
+	 * Example:
+	 * <pre><code>
+	 * buildTimePropertiesChecks = [default-include-all,!spring.dont.include.these.,!or.these]
+	 * </code></pre>
+	 * Here the default is evaluate everything but if the property in question starts with <tt>spring.dont.include.these.</tt>
+	 * or <tt>or.these.</tt> then those will not be evaluated at build time.
+	 * 
+	 * 
+	 * Both this and the build time properties match if missing option are experimental as we explore what it
+	 * means to do build time property checking.
+	 */
 	private String[] buildTimePropertiesChecks;
-
 
 	public String getMode() {
 		return mode;
