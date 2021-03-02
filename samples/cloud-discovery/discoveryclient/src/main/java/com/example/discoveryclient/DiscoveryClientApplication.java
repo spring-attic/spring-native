981b703f08eb0fb6b531acba69fdd0fec4125e84
spencergibb <spencer@gibb.us>
2021-03-02 13:19:44 -0500

    DiscoveryClient sample application polish

--- samples/cloud-discovery/discoveryclient/src/main/java/com/example/discoveryclient/DiscoveryClientApplication.java
+++ samples/cloud-discovery/discoveryclient/src/main/java/com/example/discoveryclient/DiscoveryClientApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,25 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableDiscoveryClient
 public class DiscoveryClientApplication {
 
+	@Bean
+	public CommandLineRunner clientCommandLineRunner() {
+		return args -> 	System.out.println("Foo");
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoveryClientApplication.class, args);
 		try { Thread.sleep(20000); } catch (Exception e) {}
 	}
-}
 
-@RestController
-class Foo {
-	@GetMapping("/")
-	public String bar() { return "bar";}
-	
-}
+	@RestController
+	static class Foo {
+		@GetMapping("/")
+		public String bar() { return "bar";}
 
-@Configuration
-class CLR implements CommandLineRunner {
-
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("Foo");
 	}
-
 }
