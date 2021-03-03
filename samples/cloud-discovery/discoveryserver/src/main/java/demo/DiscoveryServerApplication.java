bfb929dbf685aad3b13c4b01ed7ca29456dc6e42
spencergibb <spencer@gibb.us>
2021-03-03 10:23:25 -0500

    Fixes SpringApplication classname

--- samples/cloud-discovery/discoveryserver/src/main/java/demo/DiscoveryServerApplication.java
+++ samples/cloud-discovery/discoveryserver/src/main/java/demo/DiscoveryServerApplication.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class DiscoveryServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
+		SpringApplication.run(DiscoveryServerApplication.class, args);
 	}
 
 }
