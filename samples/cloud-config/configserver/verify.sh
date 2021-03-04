88aa921e9fe4fc52947c8b5a892dee1c70a2f1ee
spencergibb <spencer@gibb.us>
2021-03-04 16:28:44 -0500

    Adds support for origin in ConfigServerHints
    Also updates verify.sh to test a nested property value rather than just the top level "label" property since that provided a false positive.
    
    Fixes gh-556

--- samples/cloud-config/configserver/verify.sh
+++ samples/cloud-config/configserver/verify.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-RESPONSE=`curl localhost:9000/config-client/dev/master | jq -r .label`
-if [[  "$RESPONSE" == "master" ]]; then
+RESPONSE=`curl -H "Accept: application/vnd.spring-cloud.config-server.v2+json" localhost:9000/config-client/dev/master | jq -r '.propertySources[0].source."my.prop".value'`
+if [[  "$RESPONSE" == "from application-dev.yml" ]]; then
   exit 0
 else
   exit 1
--- spring-native-configuration/src/main/java/org/springframework/cloud/config/server/ConfigServerHints.java
+++ spring-native-configuration/src/main/java/org/springframework/cloud/config/server/ConfigServerHints.java
@@ -4,6 +4,9 @@ import org.eclipse.jgit.api.MergeCommand;
 import org.eclipse.jgit.internal.JGitText;
 import org.eclipse.jgit.lib.CoreConfig;
 
+import org.springframework.boot.origin.OriginLookup;
+import org.springframework.boot.origin.TextResourceOrigin;
+import org.springframework.cloud.config.environment.PropertyValueDescriptor;
 import org.springframework.cloud.config.server.config.ConfigServerAutoConfiguration;
 import org.springframework.nativex.hint.AccessBits;
 import org.springframework.nativex.hint.NativeHint;
@@ -13,7 +16,8 @@ import org.springframework.nativex.type.NativeConfiguration;
 @NativeHint(trigger = ConfigServerAutoConfiguration.class, options = { "--enable-all-security-services" }, types = {
 		@TypeHint(types = {MergeCommand.FastForwardMode.Merge.class, JGitText.class, CoreConfig.class,
 				CoreConfig.AutoCRLF.class, CoreConfig.CheckStat.class, CoreConfig.EOL.class,
-				CoreConfig.HideDotFiles.class, CoreConfig.SymLinks.class}, access = AccessBits.ALL)
+				CoreConfig.HideDotFiles.class, CoreConfig.SymLinks.class, PropertyValueDescriptor.class,
+				TextResourceOrigin.class, OriginLookup.class}, access = AccessBits.ALL)
 })
 public class ConfigServerHints implements NativeConfiguration {
 }
