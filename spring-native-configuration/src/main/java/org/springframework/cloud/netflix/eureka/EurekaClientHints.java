e6d9391ff7613ec0b9b5eca75cca9cdecd89919a
spencergibb <spencer@gibb.us>
2021-03-04 17:16:09 -0500

    Adds EurekaClientHints
    Starts removing com.netflix entries from reflect-config.json

--- spring-native-configuration/src/main/java/org/springframework/cloud/netflix/eureka/EurekaClientHints.java
+++ spring-native-configuration/src/main/java/org/springframework/cloud/netflix/eureka/EurekaClientHints.java
@@ -1,5 +1,13 @@
 package org.springframework.cloud.netflix.eureka;
 
+import com.netflix.appinfo.ApplicationInfoManager;
+import com.netflix.appinfo.EurekaInstanceConfig;
+import com.netflix.discovery.DiscoveryClient;
+import com.netflix.discovery.converters.jackson.builder.ApplicationsJacksonBuilder;
+import com.netflix.discovery.shared.Application;
+import com.netflix.discovery.shared.Applications;
+
+import org.springframework.cloud.netflix.eureka.http.EurekaApplications;
 import org.springframework.cloud.netflix.eureka.loadbalancer.EurekaLoadBalancerClientConfiguration;
 import org.springframework.cloud.netflix.eureka.loadbalancer.LoadBalancerEurekaAutoConfiguration;
 import org.springframework.nativex.hint.AccessBits;
@@ -11,6 +19,13 @@ import org.springframework.nativex.type.NativeConfiguration;
 		@TypeHint(types = {
 				EurekaLoadBalancerClientConfiguration.class
 		}, access = AccessBits.ALL)
+})
+@NativeHint(trigger = EurekaClientAutoConfiguration.class, types = {
+		@TypeHint(types = {
+				ApplicationInfoManager.class, EurekaInstanceConfig.class, DiscoveryClient.class,
+				EurekaApplications.class, Applications.class, Application.class, ApplicationsJacksonBuilder.class
+
+		}, typeNames = "com.netflix.discovery.DiscoveryClient$EurekaTransport", access = AccessBits.ALL)
 })
 public class EurekaClientHints implements NativeConfiguration {
 }
