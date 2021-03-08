9a3b39e524f1e45430aa79813bb1d9f9797805b6
Jens Schauder <jschauder@vmware.com>
2021-03-08 11:05:05 +0100

    `build.sh` runs the build for all profiles.
    The new `buid-single-profile.sh` runs the build with only the currently set DB.

--- samples/data-jpa/build.sh
+++ samples/data-jpa/build.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
+set -e
+
+export DB=h2
+${PWD%/*samples/*}/scripts/compileWithMaven.sh && ${PWD%/*samples/*}/scripts/test.sh
+export DB=hsqldb
 ${PWD%/*samples/*}/scripts/compileWithMaven.sh && ${PWD%/*samples/*}/scripts/test.sh
