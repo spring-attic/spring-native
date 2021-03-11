3d96ca6dc3250aedeb97af8a18f4e20585b90fdf
Sébastien Deleuze <sdeleuze@vmware.com>
2021-03-11 08:43:37 +0100

    Set consistently the copyright in Java sources
    Closes gh-585

--- spring-native-configuration/src/main/java/org/springframework/boot/autoconfigure/data/jdbc/JdbcRepositoriesHints.java
+++ spring-native-configuration/src/main/java/org/springframework/boot/autoconfigure/data/jdbc/JdbcRepositoriesHints.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 the original author or authors.
+ * Copyright 2019-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.springframework.boot.autoconfigure.data.jdbc;
 
 import org.springframework.data.jdbc.core.convert.RelationResolver;
