efa1d68311950d669e88f82700825be11c7b9c34
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2021-03-15 13:41:34 +0100

    Added sleuth specific entries

--- samples/wavefront-sleuth/verify.sh
+++ samples/wavefront-sleuth/verify.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-RESPONSE=`curl -s localhost:8080/`
-if [[ "$RESPONSE" == 'Hello from tomcat' ]]; then
+RESPONSE=`curl -s localhost:8080/ -H "X-B3-TraceId: 1bf6ce8439eb15b8" -H "X-B3-SpanId: 1bf6ce8439eb15b8"`
+if [[ "$RESPONSE" == 'Hello from tomcat [1bf6ce8439eb15b8]' ]]; then
   exit 0
 else
   exit 1
