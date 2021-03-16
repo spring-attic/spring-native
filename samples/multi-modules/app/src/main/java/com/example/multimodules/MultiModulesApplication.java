98a9c2e68c476b1fd8140e0ce9e863acefe0494b
Brian Clozel <bclozel@vmware.com>
2021-03-16 15:53:39 +0100

    WIP multi-modules project
    Fixes gh-

---
+++ samples/multi-modules/app/src/main/java/com/example/multimodules/MultiModulesApplication.java
@@ -0,0 +1,13 @@
+package com.example.multimodules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiModulesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiModulesApplication.class, args);
+	}
+
+}
---
+++ samples/multi-modules/app/src/test/java/com/example/multimodules/MultiModulesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.multimodules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiModulesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
