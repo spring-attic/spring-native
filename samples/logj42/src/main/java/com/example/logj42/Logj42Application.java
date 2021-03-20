210f0f7ca202e91cd0cc14190840c9d4d2fc6054
Josh Long <josh@joshlong.com>
2021-03-19 23:47:26 -0700

    log4j2

---
+++ samples/logj42/src/main/java/com/example/logj42/Logj42Application.java
@@ -0,0 +1,25 @@
+package com.example.logj42;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+
+@Log4j2
+@SpringBootApplication
+public class Logj42Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Logj42Application.class, args);
+	}
+
+	@Bean
+	ApplicationRunner runner() {
+		return event -> {
+			log.info("hello, world!");
+		};
+	}
+}
---
+++ samples/logj42/src/test/java/com/example/logj42/Logj42ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.logj42;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Logj42ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
