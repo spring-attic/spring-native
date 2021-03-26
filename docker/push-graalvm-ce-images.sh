1c272427a60e1ce9b672d910b14f17602b20da31
Stephane Nicoll <snicoll@vmware.com>
2021-03-26 15:55:24 +0100

    Build GraalVM images automatically
    This commit watches the Graal github repository and trigger the build of
    the springci/graalvm-ce images once a day if necessary.
    
    Images are tagged with the milestone so that each branch can have
    isolated configuration. Rather than putting the GraalVM version in the
    image name, images are categorized as "stable" (the current maintenance
    release) or "dev" (the current development branch).

--- docker/push-graalvm-ce-images.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-docker push springci/graalvm-ce:21.0-dev-java8
-docker push springci/graalvm-ce:21.0-dev-java11
-docker push springci/graalvm-ce:master-java8
-docker push springci/graalvm-ce:master-java11
