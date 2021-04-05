d793cb83285f56e7b1342250cd74ca5c89a8d2e1
Andy Clement <aclement@vmware.com>
2021-04-05 16:00:38 -0700

    Actuator prometheus endpoint reachable

--- samples/actuator-webmvc/src/main/java/com/example/demo/DemoApplication.java
+++ samples/actuator-webmvc/src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +12,12 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+}
+
+@RestController
+class C {
+	@GetMapping("/")
+	public String h() {
+		return "hello";
+	}
 }
--- samples/actuator-webmvc/verify.sh
+++ samples/actuator-webmvc/verify.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 RESPONSE=`curl -s localhost:8080/actuator/health`
-if [[ "$RESPONSE" == *"UP"* ]]; then
-  exit 0
-else 
-  echo "failed to get UP health response"
+if [[ "$RESPONSE" != *"UP"* ]]; then
+  echo "Failed to get UP response: $RESPONSE"
   exit 1
 fi
+RESPONSE=`curl -s localhost:8080/actuator/prometheus | grep ^jvm_classes_loaded_classes | wc -l`
+if [[ "$RESPONSE" != *"1"* ]]; then
+  echo "Failed to get data from prometheus endpoint: $RESPONSE"
+  exit 1
+fi
+exit 0
