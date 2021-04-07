85e3e2c4e92a76855e429d421d933cda0398bdbb
Jens Schauder <jschauder@vmware.com>
2021-04-07 12:29:30 +0200

    Fixed missing reflection configuration for EnumType.
    Closes #692

--- samples/data-jpa/src/main/java/app/main/model/Foo.java
+++ samples/data-jpa/src/main/java/app/main/model/Foo.java
@@ -1,6 +1,8 @@
 package app.main.model;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -9,29 +11,32 @@ import javax.persistence.OneToOne;
 @Entity
 public class Foo {
 
-    @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
-    private long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
 	private String value;
+
+	@Enumerated
+	private SomeEnum enumerated;
+
 	@OneToOne(cascade = CascadeType.ALL)
 	private Flurb flurb;
+
 	public Foo() {
 	}
+
 	public Foo(String value) {
 		this.value = value;
 	}
+
 	public String getValue() {
 		return this.value;
 	}
+
 	public void setValue(String value) {
 		this.value = value;
 	}
-    @Override
-    public String toString() {
-        return String.format(
-                "Foo[id=%d, value='%s']",
-                id, value);
-    }
 
 	public Flurb getFlurb() {
 		return flurb;
@@ -40,4 +45,16 @@ public class Foo {
 	public void setFlurb(Flurb flurb) {
 		this.flurb = flurb;
 	}
+
+	enum SomeEnum {
+		ONE, TWO
+	}
+
+	@Override
+	public String toString() {
+		return String.format(
+				"Foo[id=%d, value='%s']",
+				id, value);
+	}
+
 }
