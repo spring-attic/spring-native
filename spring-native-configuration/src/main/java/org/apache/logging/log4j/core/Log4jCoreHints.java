146e3bf4fc2da5918bfcc3e06482d3fabb58cb53
Sébastien Deleuze <sdeleuze@vmware.com>
2021-04-27 15:10:53 +0200

    Mention that Log4j2 hints are partial.
    They are just designed to avoid crashing during compilation.
    
    Reopens gh-115
    See gh-398

--- spring-native-configuration/src/main/java/org/apache/logging/log4j/core/Log4jCoreHints.java
+++ spring-native-configuration/src/main/java/org/apache/logging/log4j/core/Log4jCoreHints.java
@@ -7,12 +7,16 @@ import org.springframework.nativex.type.NativeConfiguration;
 
 
 /**
+ * Minimal Log4j configuration to avoid crashing during compilation.
+ * TODO See https://github.com/spring-projects-experimental/spring-native/issues/115
+ *
  * @author Sebastien Deleuze
  */
 @NativeHint(
 	trigger = org.apache.logging.log4j.core.Logger.class, options = "-Dlog4j2.disable.jmx=true",
 		initialization = @InitializationHint(typeNames = {
-				"com.oracle.truffle.js.scriptengine.GraalJSEngineFactory"
+				"com.oracle.truffle.js.scriptengine.GraalJSEngineFactory",
+				"com.oracle.truffle.js.scriptengine.GraalJSScriptEngine"
 		}, initTime = InitializationTime.BUILD)
 )
 public class Log4jCoreHints implements NativeConfiguration {
