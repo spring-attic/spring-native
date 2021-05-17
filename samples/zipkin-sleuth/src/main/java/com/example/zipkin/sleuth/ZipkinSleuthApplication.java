19425e58a6413611185aa112c4155b5d2e0497b8
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2021-05-17 18:03:26 +0200

    Added Sleuth Zipkin support; fixes gh-780

---
+++ samples/zipkin-sleuth/src/main/java/com/example/zipkin/sleuth/ZipkinSleuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.zipkin.sleuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipkinSleuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinSleuthApplication.class, args);
+	}
+
+}
---
+++ samples/zipkin-sleuth/src/test/java/com/example/zipkin/sleuth/test/ZipkinSleuthApplicationTest.java
@@ -0,0 +1,14 @@
+package com.example.zipkin.sleuth.test;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ZipkinSleuthApplicationTest {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
