c57302ddd011d3ef440a8b90dafce27571dc4d2e
Stephane Nicoll <snicoll@vmware.com>
2021-06-22 09:17:04 +0200

    Simplify wrapper by using standard reflection

--- spring-aot/src/main/java/org/springframework/context/annotation/BuildTimeBeanDefinitionsRegistrar.java
+++ spring-aot/src/main/java/org/springframework/context/annotation/BuildTimeBeanDefinitionsRegistrar.java
@@ -1,19 +1,7 @@
 package org.springframework.context.annotation;
 
-import java.io.IOException;
-
-import org.springframework.beans.factory.BeanDefinitionStoreException;
-import org.springframework.beans.factory.annotation.AnnotatedBeanDefinition;
-import org.springframework.beans.factory.config.BeanDefinitionHolder;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
-import org.springframework.beans.factory.support.BeanDefinitionReaderUtils;
-import org.springframework.beans.factory.support.BeanNameGenerator;
 import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.core.type.classreading.MetadataReader;
-import org.springframework.core.type.classreading.MetadataReaderFactory;
-import org.springframework.core.type.classreading.SimpleMetadataReaderFactory;
-import org.springframework.core.type.classreading.TypeDescriptor;
-import org.springframework.core.type.classreading.TypeSystem;
 
 /**
  * Parse the {@link Configuration @Configuration classes} and provide the bean definitions
@@ -26,30 +14,16 @@ public class BuildTimeBeanDefinitionsRegistrar {
 
 	private final GenericApplicationContext context;
 
-	private final TypeSystem typeSystem;
-
-	private final MetadataReaderFactory metadataReaderFactory;
-
-	private final BeanNameGenerator beanNameGenerator = AnnotationBeanNameGenerator.INSTANCE;
-
-	public BuildTimeBeanDefinitionsRegistrar(GenericApplicationContext context, TypeSystem typeSystem) {
+	public BuildTimeBeanDefinitionsRegistrar(GenericApplicationContext context) {
 		this.context = context;
-		this.typeSystem = typeSystem;
-		this.metadataReaderFactory = new SimpleMetadataReaderFactory();
 	}
 
-	/**
-	 * Register root component classes that are meant to bootstrap the context.
-	 * @param componentClasses the root component classes
-	 */
-	public void register(String... componentClasses) {
-		for (String componentClass : componentClasses) {
-			TypeDescriptor componentTypeDescriptor = this.typeSystem.resolve(componentClass);
-			if (componentTypeDescriptor != null) {
-				register(componentTypeDescriptor);
-			}
+	public void register(Class<?>... componentClasses) {
+		for (Class<?> componentClass : componentClasses) {
+			this.context.registerBean(componentClass);
 		}
 	}
+
 
 	/**
 	 * Process bean definitions without creating any instance and return the
@@ -68,23 +42,6 @@ public class BuildTimeBeanDefinitionsRegistrar {
 		configurationClassPostProcessor.setEnvironment(this.context.getEnvironment());
 		configurationClassPostProcessor.setResourceLoader(this.context);
 		configurationClassPostProcessor.postProcessBeanFactory(this.context.getBeanFactory());
-	}
-
-	private void register(TypeDescriptor typeDescriptor) {
-		MetadataReader metadataReader = getMetadataReader(typeDescriptor);
-		AnnotatedBeanDefinition beanDefinition = new AnnotatedMetadataGenericBeanDefinition(metadataReader);
-		String beanName = this.beanNameGenerator.generateBeanName(beanDefinition, this.context);
-		BeanDefinitionHolder definitionHolder = new BeanDefinitionHolder(beanDefinition, beanName);
-		BeanDefinitionReaderUtils.registerBeanDefinition(definitionHolder, this.context);
-	}
-
-	private MetadataReader getMetadataReader(TypeDescriptor typeDescriptor) {
-		try {
-			return this.metadataReaderFactory.getMetadataReader(typeDescriptor.getTypeName());
-		}
-		catch (IOException ex) {
-			throw new BeanDefinitionStoreException(String.format("Failure while reading metadata for %s.", typeDescriptor), ex);
-		}
 	}
 
 }
--- spring-aot/src/test/java/org/springframework/context/annotation/BuildTimeBeanDefinitionsRegistrarTests.java
+++ spring-aot/src/test/java/org/springframework/context/annotation/BuildTimeBeanDefinitionsRegistrarTests.java
@@ -5,12 +5,13 @@ import java.util.function.Supplier;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.context.annotation.samples.compose.ImportConfiguration;
 import org.springframework.context.annotation.samples.condition.ConditionalConfigurationOne;
 import org.springframework.context.annotation.samples.scan.ScanConfiguration;
+import org.springframework.context.annotation.samples.simple.ConfigurationOne;
+import org.springframework.context.annotation.samples.simple.ConfigurationTwo;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.core.io.DefaultResourceLoader;
-import org.springframework.core.type.classreading.TypeSystem;
 import org.springframework.mock.env.MockEnvironment;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -22,55 +23,49 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 class BuildTimeBeanDefinitionsRegistrarTests {
 
-	private static final String CONFIGURATION_ONE = "org.springframework.context.annotation.samples.simple.ConfigurationOne";
-
-	private static final String CONFIGURATION_TWO = "org.springframework.context.annotation.samples.simple.ConfigurationTwo";
-
-	private final TypeSystem typeSystem = TypeSystem.getTypeSystem(new DefaultResourceLoader());
-
 	@Test
 	void processBeanDefinitionsWithRegisteredConfigurationClasses() {
 		BuildTimeBeanDefinitionsRegistrar beanFactoryProvider = forApplicationContext(GenericApplicationContext::new);
-		beanFactoryProvider.register(CONFIGURATION_ONE, CONFIGURATION_TWO);
+		beanFactoryProvider.register(ConfigurationOne.class, ConfigurationTwo.class);
 		ConfigurableListableBeanFactory beanFactory = beanFactoryProvider.processBeanDefinitions();
-		assertThat(beanFactory.getBeanDefinitionNames()).containsOnly("configurationOne",
-				"configurationTwo", "beanOne", "beanTwo");
+		assertThat(beanFactory.getBeanDefinitionNames()).containsOnly(ConfigurationOne.class.getName(),
+				ConfigurationTwo.class.getName(), "beanOne", "beanTwo");
 	}
 
 	@Test
 	void processBeanDefinitionsWithRegisteredConfigurationClassWithImport() {
 		BuildTimeBeanDefinitionsRegistrar context = forApplicationContext(GenericApplicationContext::new);
-		context.register("org.springframework.context.annotation.samples.compose.ImportConfiguration");
+		context.register(ImportConfiguration.class);
 		ConfigurableListableBeanFactory beanFactory = context.processBeanDefinitions();
-		assertThat(beanFactory.getBeanDefinitionNames()).containsOnly("importConfiguration",
-				CONFIGURATION_ONE, CONFIGURATION_TWO, "beanOne", "beanTwo");
+		assertThat(beanFactory.getBeanDefinitionNames()).containsOnly(ImportConfiguration.class.getName(),
+				ConfigurationOne.class.getName(), ConfigurationTwo.class.getName(), "beanOne", "beanTwo");
 	}
 
 	@Test
 	void processBeanDefinitionsWithClasspathScanning() {
 		BuildTimeBeanDefinitionsRegistrar context = forApplicationContext(GenericApplicationContext::new);
-		context.register(ScanConfiguration.class.getName());
+		context.register(ScanConfiguration.class);
 		ConfigurableListableBeanFactory beanFactory = context.processBeanDefinitions();
-		assertThat(beanFactory.getBeanDefinitionNames()).containsOnly("scanConfiguration",
+		assertThat(beanFactory.getBeanDefinitionNames()).containsOnly(ScanConfiguration.class.getName(),
 				"configurationOne", "configurationTwo", "simpleComponent", "beanOne", "beanTwo");
 	}
 
 	@Test
 	void processBeanDefinitionsWithConditionsOnConfigurationClassNotMatching() {
 		BuildTimeBeanDefinitionsRegistrar context = forApplicationContext(GenericApplicationContext::new);
-		context.register(ConditionalConfigurationOne.class.getName());
+		context.register(ConditionalConfigurationOne.class);
 		ConfigurableListableBeanFactory beanFactory = context.processBeanDefinitions();
-		assertThat(beanFactory.getBeanDefinitionNames()).containsOnly("conditionalConfigurationOne");
+		assertThat(beanFactory.getBeanDefinitionNames()).containsOnly(ConditionalConfigurationOne.class.getName());
 	}
 
 	@Test
 	void processBeanDefinitionsWithConditionsOnConfigurationClassMatching() {
 		BuildTimeBeanDefinitionsRegistrar context = forApplicationContext(GenericApplicationContext::new,
 				new MockEnvironment().withProperty("test.one.enabled", "true"));
-		context.register(ConditionalConfigurationOne.class.getName());
+		context.register(ConditionalConfigurationOne.class);
 		ConfigurableListableBeanFactory beanFactory = context.processBeanDefinitions();
-		assertThat(beanFactory.getBeanDefinitionNames()).containsOnly("conditionalConfigurationOne",
-				CONFIGURATION_ONE, "beanOne", CONFIGURATION_TWO, "beanTwo");
+		assertThat(beanFactory.getBeanDefinitionNames()).containsOnly(ConditionalConfigurationOne.class.getName(),
+				ConfigurationOne.class.getName(), "beanOne", ConfigurationTwo.class.getName(), "beanTwo");
 	}
 
 	private BuildTimeBeanDefinitionsRegistrar forApplicationContext(
@@ -82,7 +77,7 @@ class BuildTimeBeanDefinitionsRegistrarTests {
 			Supplier<? extends GenericApplicationContext> contextFactory, ConfigurableEnvironment environment) {
 		GenericApplicationContext context = contextFactory.get();
 		context.setEnvironment(environment);
-		return new BuildTimeBeanDefinitionsRegistrar(context, this.typeSystem);
+		return new BuildTimeBeanDefinitionsRegistrar(context);
 	}
 
 }
