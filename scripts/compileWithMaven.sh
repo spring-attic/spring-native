82862a9018cd27ec139201c0d8a94346c6402f36
Andy Clement <aclement@pivotal.io>
2021-07-05 12:15:20 -0700

    Ensure agent samples run tests since tests create config

--- scripts/compileWithMaven.sh
+++ scripts/compileWithMaven.sh
@@ -12,7 +12,7 @@ mkdir -p target/native
 
 echo "Packaging ${PWD##*/} with Maven"
 # Only run Petclinic tests to speedup the full build while still testing a complex testing scenario
-if [[ ${PWD##*/} == petclinic* ]]
+if [[ ${PWD##*/} == petclinic* || ${PWD##*/} == *agent* ]]
 then
   echo "Performing native testing for ${PWD##*/}"
   mvn -ntp -Pnative package $* &> target/native/output.txt
