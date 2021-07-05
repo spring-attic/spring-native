b90575558239d753011eef6ecd8a33b9850a2360
Andy Clement <aclement@pivotal.io>
2021-07-05 11:21:24 -0700

    Run the build.sh content that will actually pass verify.sh

--- samples/actuator-webmvc/build.sh
+++ samples/actuator-webmvc/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-${PWD%/*samples/*}/scripts/compileWithMaven.sh -Dspring.native.build-time-properties-checks=default-include-all -Dspring.native.factories.no-actuator-metrics=true && ${PWD%/*samples/*}/scripts/test.sh
-#${PWD%/*samples/*}/scripts/compileWithMaven.sh -Dspring.native.build-time-properties-checks=default-include-all && ${PWD%/*samples/*}/scripts/test.sh
+#${PWD%/*samples/*}/scripts/compileWithMaven.sh -Dspring.native.build-time-properties-checks=default-include-all -Dspring.native.factories.no-actuator-metrics=true && ${PWD%/*samples/*}/scripts/test.sh
+${PWD%/*samples/*}/scripts/compileWithMaven.sh -Dspring.native.build-time-properties-checks=default-include-all && ${PWD%/*samples/*}/scripts/test.sh
