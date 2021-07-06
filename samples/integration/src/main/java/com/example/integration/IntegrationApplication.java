bdb85a032d8b727479c1f7b87e0686c80aa958d2
Artem Bilan <abilan@vmware.com>
2021-07-06 11:46:27 -0400

    Add more Spring Integration native hints
    * The `IntegrationFlow` interface can be proxied at runtime
    * The resource pattern for JDBC schemas should really be a regex pattern
    * The HTTP and WebFlux request mappings need a reflection access to
    method of their handlers to map
    * Process interfaces with a `@MessagingGateway` and register their
    proxy hints
    * Improve `IntegrationApplication` in the sample to cover the mentioned
    above cases about an `IntegrationFlow` proxy and `@MessagingGateway`
    * Fix `ControlBusGateway` to emit really a Control Bus command
    * Use `WebClient.Builder` bean from the ctx for application logic
    * Add reflection info for `AbstractEndpoint` to satisfy Control Bus requirements
    * Add some common types to satisfy common SpEL expressions
    * Add explicit `@TypeHint` for `Message` impls -
    * Add more Spring Integration types specific to `MessageStore` serialization functionality
    * Add programmatic scan for `IntegrationNode` impls instead of huge number of
    explicit types in `@TypeHint`
    * Add `JdbcChannelMessageStore` logic into the sample to cover its serialization functionality
    * Add `@SerializationHint` for messaging types
    * Add programmatic scan for `IntegrationNode` impls instead of huge number of
    explicit types in `@TypeHint`
    * Add `JdbcChannelMessageStore` logic into the sample to cover its serialization functionality
    * Add `RedisChannelMessageStore` logic into the sample to cover JSON (un)marshaling feature
    * Rely on Spring Integration version from parent in the sample
    
    Closes gh-894

--- samples/integration/src/main/java/com/example/integration/IntegrationApplication.java
+++ samples/integration/src/main/java/com/example/integration/IntegrationApplication.java
@@ -4,11 +4,16 @@ import java.time.Duration;
 import java.util.Calendar;
 import java.util.Date;
 
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.channel.interceptor.WireTap;
@@ -17,12 +22,19 @@ import org.springframework.integration.config.EnableMessageHistory;
 import org.springframework.integration.config.GlobalChannelInterceptor;
 import org.springframework.integration.config.IntegrationConverter;
 import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlowDefinition;
 import org.springframework.integration.dsl.IntegrationFlows;
-import org.springframework.integration.endpoint.SourcePollingChannelAdapter;
 import org.springframework.integration.handler.LoggingHandler;
 import org.springframework.integration.handler.advice.RequestHandlerRetryAdvice;
 import org.springframework.integration.http.config.EnableIntegrationGraphController;
+import org.springframework.integration.jdbc.store.JdbcChannelMessageStore;
+import org.springframework.integration.jdbc.store.channel.H2ChannelMessageStoreQueryProvider;
+import org.springframework.integration.redis.store.RedisChannelMessageStore;
+import org.springframework.integration.scheduling.PollerMetadata;
+import org.springframework.integration.support.json.JacksonJsonUtils;
+import org.springframework.integration.webflux.dsl.WebFlux;
 import org.springframework.messaging.MessageHandler;
+import org.springframework.scheduling.support.PeriodicTrigger;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import io.micrometer.core.instrument.MeterRegistry;
@@ -35,15 +47,32 @@ import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
 public class IntegrationApplication {
 
 	public static void main(String[] args) throws InterruptedException {
-		ConfigurableApplicationContext applicationContext = SpringApplication.run(IntegrationApplication.class, args);
-		applicationContext.getBean("dateSourceEndpoint", SourcePollingChannelAdapter.class).start();
+		ApplicationContext context = SpringApplication.run(IntegrationApplication.class, args);
+
+		WebClient webClient =
+				context.getBean(WebClient.Builder.class)
+						.baseUrl("http://localhost:8080")
+						.build();
 
 		Thread.sleep(1000);
 
+		System.out.println("Starting 'dateSourceEndpoint'...");
+
+		webClient
+				.get()
+				.uri("control-bus/dateSourceEndpoint")
+				.retrieve()
+				.toBodilessEntity()
+				.block(Duration.ofSeconds(10));
+
+		Thread.sleep(1000);
+
+		System.out.println("Obtaining integration graph...");
+
 		String integrationGraph =
-				WebClient.create()
+				webClient
 						.get()
-						.uri("http://localhost:8080/integration-graph")
+						.uri("integration-graph")
 						.accept(MediaType.APPLICATION_JSON)
 						.retrieve()
 						.bodyToMono(String.class)
@@ -58,14 +87,38 @@ public class IntegrationApplication {
 	}
 
 	@Bean
-	IntegrationFlow printFormattedSecondsFlow() {
+	JdbcChannelMessageStore jdbcChannelMessageStore(DataSource dataSource) {
+		JdbcChannelMessageStore jdbcChannelMessageStore = new JdbcChannelMessageStore(dataSource);
+		jdbcChannelMessageStore.setChannelMessageStoreQueryProvider(new H2ChannelMessageStoreQueryProvider());
+		return jdbcChannelMessageStore;
+	}
+
+	@Bean
+	RedisChannelMessageStore redisChannelMessageStore(RedisConnectionFactory connectionFactory) {
+		RedisChannelMessageStore redisChannelMessageStore = new RedisChannelMessageStore(connectionFactory);
+		redisChannelMessageStore.setValueSerializer(
+				new GenericJackson2JsonRedisSerializer(JacksonJsonUtils.messagingAwareMapper()));
+		return redisChannelMessageStore;
+	}
+
+	@Bean(PollerMetadata.DEFAULT_POLLER)
+	PollerMetadata defaultPoller() {
+		PollerMetadata pollerMetadata = new PollerMetadata();
+		pollerMetadata.setTrigger(new PeriodicTrigger(10));
+		return pollerMetadata;
+	}
+
+	@Bean
+	IntegrationFlow printFormattedSecondsFlow(JdbcChannelMessageStore jdbcChannelMessageStore,
+			RedisChannelMessageStore redisChannelMessageStore) {
 		return IntegrationFlows
 				.fromSupplier(Date::new,
 						e -> e.id("dateSourceEndpoint")
 								.poller(p -> p.fixedDelay(1000, 1000)))
-				.channel("dateChannel")
+				.channel(c -> c.queue("dateChannel", jdbcChannelMessageStore, "dateChannelGroup"))
 				.gateway(subflow -> subflow.convert(Integer.class,
 						e -> e.advice(new RequestHandlerRetryAdvice())))
+				.channel(c -> c.queue(redisChannelMessageStore, "secondsChannelGroup"))
 				.handle(m -> System.out.println("Current seconds: " + m.getPayload()))
 				.get();
 	}
@@ -97,6 +150,21 @@ public class IntegrationApplication {
 			}
 
 		};
+	}
+
+	@Bean
+	public IntegrationFlow controlBus() {
+		return IntegrationFlowDefinition::controlBus;
+	}
+
+	@Bean
+	public IntegrationFlow controlBusControllerFlow(ControlBusGateway controlBusGateway) {
+		return IntegrationFlows
+				.from(WebFlux.inboundChannelAdapter("/control-bus/{endpointId}")
+						.payloadExpression("#pathVariables.endpointId")
+						.requestMapping(mapping -> mapping.methods(HttpMethod.GET)))
+				.handle(controlBusGateway, "startEndpoint")
+				.get();
 	}
 
 }
