067b2ec4e81f9deb38f3e7bc028d8f1ce8cad839
Sébastien Deleuze <sdeleuze@vmware.com>
2021-07-06 11:25:25 +0200

    Increase timeout for Wavefront samples

--- samples/wavefront-sleuth/verify.sh
+++ samples/wavefront-sleuth/verify.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+sleep 5
 RESPONSE=`curl -s localhost:8080/ -H "X-B3-TraceId: 1bf6ce8439eb15b8" -H "X-B3-SpanId: 1bf6ce8439eb15b8"`
 if [[ "$RESPONSE" == 'Hello from tomcat [1bf6ce8439eb15b8]' ]]; then
   exit 0
