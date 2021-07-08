023f172ee5fcc444c3d7503bbdf8b058a3e54167
Sébastien Deleuze <sdeleuze@vmware.com>
2021-07-08 09:23:42 +0200

    Move ExceptionHandler and BeanDefinitionCustomizers to spring-native
    To make them available at runtime

--- spring-native/src/main/java/org/springframework/context/bootstrap/infrastructure/BeanDefinitionCustomizers.java
+++ spring-native/src/main/java/org/springframework/context/bootstrap/infrastructure/BeanDefinitionCustomizers.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.springframework.context.bootstrap.infrastructure;
 
 import org.springframework.beans.factory.config.BeanDefinitionCustomizer;
