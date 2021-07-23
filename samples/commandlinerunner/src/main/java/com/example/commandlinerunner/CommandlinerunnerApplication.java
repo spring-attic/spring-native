f04dfd92c4168e305c23c1cc115f9dc6f5a71a46
Sébastien Deleuze <sdeleuze@vmware.com>
2021-07-23 15:09:20 +0200

    Polishing

--- samples/commandlinerunner/src/main/java/com/example/commandlinerunner/CommandlinerunnerApplication.java
+++ samples/commandlinerunner/src/main/java/com/example/commandlinerunner/CommandlinerunnerApplication.java
@@ -1,14 +1,12 @@
 package com.example.commandlinerunner;
 
-import java.lang.reflect.InvocationTargetException;
-
 import org.springframework.aot.SpringAotApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CommandlinerunnerApplication {
 
-	public static void main(String[] args) throws InterruptedException, ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+	public static void main(String[] args) throws InterruptedException {
 		SpringAotApplication.run(CommandlinerunnerApplication.class, args);
 		Thread.currentThread().join(); // To be able to measure memory consumption
 	}
