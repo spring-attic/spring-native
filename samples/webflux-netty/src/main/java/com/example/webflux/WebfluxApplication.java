7f8e66405b42e07fa55f9d01ca95400b405d8d2d
Sébastien Deleuze <sdeleuze@vmware.com>
2021-07-23 15:29:13 +0200

    Migrate webflux-netty sample
    See related gh-925 issue.

--- samples/webflux-netty/src/main/java/com/example/webflux/WebfluxApplication.java
+++ samples/webflux-netty/src/main/java/com/example/webflux/WebfluxApplication.java
@@ -1,6 +1,6 @@
 package com.example.webflux;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.aot.SpringAotApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,16 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class WebfluxApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WebfluxApplication.class, args);
+		SpringAotApplication.run(WebfluxApplication.class, args);
 	}
 
 	@RestController
-	class Foo {
+	static class Foo {
 
 		@GetMapping("/")
 		public String greet() {
 			return "hi!";
 		}
 	}
-
 }
