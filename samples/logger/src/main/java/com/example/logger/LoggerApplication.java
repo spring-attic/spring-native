20be0ea2afef1a59482f1473585cb6c8599940b4
Petr Hejl <petr.hejl@memsource.com>
2021-07-29 00:11:32 +0200

    Fix imports

--- samples/logger/src/main/java/com/example/logger/LoggerApplication.java
+++ samples/logger/src/main/java/com/example/logger/LoggerApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.util.Optional;
-
 @SpringBootApplication
 public class LoggerApplication {
 
