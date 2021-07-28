f27e71c3241dc74f07805bad20fedc56a703be18
Petr Hejl <petr.hejl@memsource.com>
2021-07-29 00:09:57 +0200

    Provide sample project

--- samples/logger/src/main/java/com/example/logger/LoggerApplication.java
+++ samples/logger/src/main/java/com/example/logger/LoggerApplication.java
@@ -2,14 +2,28 @@ package com.example.logger;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import java.util.Optional;
+
+@SpringBootApplication
 public class LoggerApplication {
 
 	public static void main(String[] args) throws InterruptedException {
-		Log LOGGER = LogFactory.getLog(LoggerApplication.class);
-		LOGGER.info("info");
-		LOGGER.error("ouch", new RuntimeException());
+		SpringApplication.run(LoggerApplication.class);
 		Thread.currentThread().join(); // To be able to measure memory consumption
+	}
+
+	@Bean
+	public CommandLineRunner runner() {
+		return args -> {
+			Log LOGGER = LogFactory.getLog(LoggerApplication.class);
+			LOGGER.info("info");
+			LOGGER.error("ouch", new RuntimeException());
+		};
 	}
 
 }
