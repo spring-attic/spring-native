a4a528a1ab324de1f2050b736ee328fecd89e25b
Stephane Nicoll <snicoll@vmware.com>
2021-08-09 16:26:40 +0200

    Register reflection metadata for @EventListener
    Closes gh-961

--- spring-aot/src/test/java/org/springframework/context/bootstrap/generator/event/EventListenerMethodRegistrationGeneratorTests.java
+++ spring-aot/src/test/java/org/springframework/context/bootstrap/generator/event/EventListenerMethodRegistrationGeneratorTests.java
@@ -1,18 +1,26 @@
 package org.springframework.context.bootstrap.generator.event;
 
+import java.util.List;
+
+import com.squareup.javapoet.CodeBlock;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.annotation.AnnotationConfigUtils;
+import org.springframework.context.bootstrap.generator.BootstrapClass;
+import org.springframework.context.bootstrap.generator.BootstrapWriterContext;
+import org.springframework.context.bootstrap.generator.reflect.RuntimeReflectionEntry;
 import org.springframework.context.bootstrap.generator.sample.SimpleConfiguration;
 import org.springframework.context.bootstrap.generator.sample.event.AnotherEventListener;
 import org.springframework.context.bootstrap.generator.sample.event.SingleEventListener;
 import org.springframework.context.bootstrap.generator.sample.event.SingleTransactionalEventListener;
 import org.springframework.context.bootstrap.generator.test.CodeSnippet;
 import org.springframework.transaction.event.TransactionalEventListenerFactory;
+import org.springframework.util.ReflectionUtils;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -67,6 +75,31 @@ class EventListenerMethodRegistrationGeneratorTests {
 				"));");
 	}
 
+	@Test
+	void writeEventListenersRegistrationRegisterReflectionMetadata() {
+		DefaultListableBeanFactory beanFactory = prepareBeanFactory();
+		beanFactory.registerBeanDefinition("test", BeanDefinitionBuilder.rootBeanDefinition(SingleEventListener.class)
+				.getBeanDefinition());
+		beanFactory.registerBeanDefinition("another", BeanDefinitionBuilder.rootBeanDefinition(AnotherEventListener.class)
+				.getBeanDefinition());
+		EventListenerMethodRegistrationGenerator processor = new EventListenerMethodRegistrationGenerator(beanFactory);
+		BootstrapWriterContext context = new BootstrapWriterContext(new BootstrapClass("com.example", "Test"));
+		processor.writeEventListenersRegistration(context, CodeBlock.builder());
+		List<RuntimeReflectionEntry> entries = context.getRuntimeReflectionRegistry().getEntries();
+		assertThat(entries).hasSize(2);
+		assertThat(entries).anySatisfy((entry) -> {
+			assertThat(entry.getType()).isEqualTo(SingleEventListener.class);
+			assertThat(entry.getMethods()).containsOnly(
+					ReflectionUtils.findMethod(SingleEventListener.class, "onStartup", ApplicationStartedEvent.class));
+			assertThat(entry.getFields()).isEmpty();
+		});
+		assertThat(entries).anySatisfy((entry) -> {
+			assertThat(entry.getType()).isEqualTo(AnotherEventListener.class);
+			assertThat(entry.getMethods()).containsOnly(
+					ReflectionUtils.findMethod(AnotherEventListener.class, "onRefresh"));
+			assertThat(entry.getFields()).isEmpty();
+		});
+	}
 
 	private DefaultListableBeanFactory prepareBeanFactory() {
 		DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
@@ -76,7 +109,8 @@ class EventListenerMethodRegistrationGeneratorTests {
 
 	private CodeSnippet generateCode(ConfigurableListableBeanFactory beanFactory) {
 		EventListenerMethodRegistrationGenerator processor = new EventListenerMethodRegistrationGenerator(beanFactory);
-		return CodeSnippet.of(processor::writeEventListenersRegistration);
+		BootstrapWriterContext context = new BootstrapWriterContext(new BootstrapClass("com.example", "Test"));
+		return CodeSnippet.of((code) -> processor.writeEventListenersRegistration(context, code));
 	}
 
 }
