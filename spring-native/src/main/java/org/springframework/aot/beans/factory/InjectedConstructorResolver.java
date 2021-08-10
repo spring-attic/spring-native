e59d62e9015a02b482f282000e4779b3e5c67927
Stephane Nicoll <snicoll@vmware.com>
2021-08-10 14:22:54 +0200

    Support for ConstructorArgumentValues in constructor instantiation

--- spring-native/src/main/java/org/springframework/aot/beans/factory/InjectedConstructorResolver.java
+++ spring-native/src/main/java/org/springframework/aot/beans/factory/InjectedConstructorResolver.java
@@ -9,8 +9,12 @@ import java.util.Set;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.TypeConverter;
 import org.springframework.beans.factory.InjectionPoint;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.UnsatisfiedDependencyException;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanReference;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.config.ConstructorArgumentValues.ValueHolder;
 import org.springframework.beans.factory.config.DependencyDescriptor;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.core.MethodParameter;
@@ -43,17 +47,51 @@ class InjectedConstructorResolver implements InjectedElementResolver {
 		TypeConverter typeConverter = beanFactory.getTypeConverter();
 		for (int i = 0; i < argumentCount; i++) {
 			MethodParameter methodParam = new MethodParameter(this.constructor, i);
-			DependencyDescriptor depDescriptor = new DependencyDescriptor(methodParam, true);
-			depDescriptor.setContainingClass(this.beanType);
-			try {
-				Object arg = beanFactory.resolveDependency(depDescriptor, beanName, autowiredBeans, typeConverter);
-				arguments.add(arg);
+			Object userValue = resolveUserArgument(context, methodParam);
+			if (userValue != null) {
+				arguments.add(userValue);
 			}
-			catch (BeansException ex) {
-				throw new UnsatisfiedDependencyException(null, beanName, new InjectionPoint(methodParam), ex);
+			else {
+				DependencyDescriptor depDescriptor = new DependencyDescriptor(methodParam, true);
+				depDescriptor.setContainingClass(this.beanType);
+				try {
+					Object arg = beanFactory.resolveDependency(depDescriptor, beanName, autowiredBeans, typeConverter);
+					arguments.add(arg);
+				}
+				catch (BeansException ex) {
+					throw new UnsatisfiedDependencyException(null, beanName, new InjectionPoint(methodParam), ex);
+				}
 			}
 		}
 		return new InjectedElementAttributes(arguments);
+	}
+
+	private Object resolveUserArgument(GenericApplicationContext context, MethodParameter parameter) {
+		BeanDefinition beanDefinition = safeGetBeanDefinition(context);
+		if (beanDefinition == null || !beanDefinition.hasConstructorArgumentValues()) {
+			return null;
+		}
+		ValueHolder userValue = beanDefinition.getConstructorArgumentValues().getIndexedArgumentValue(
+				parameter.getParameterIndex(), parameter.getParameterType());
+		if (userValue != null) {
+			Object value = userValue.getValue();
+			if (value instanceof BeanReference) {
+				return context.getBean(((BeanReference) value).getBeanName(), parameter.getParameterType());
+			}
+			else {
+				return value;
+			}
+		}
+		return null;
+	}
+
+	private BeanDefinition safeGetBeanDefinition(GenericApplicationContext context) {
+		try {
+			return context.getBeanDefinition(this.beanName);
+		}
+		catch (NoSuchBeanDefinitionException ex) {
+			return null;
+		}
 	}
 
 }
