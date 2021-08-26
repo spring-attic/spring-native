02a7abca2ac7daa80200d32ccea8dc8c1aeac176
Brian Clozel <bclozel@vmware.com>
2021-08-26 10:14:12 +0200

    Configure Environment for AOT generation
    Prior to this commit, the `ContextBootstrapContributor` would prepare an
    empty `Environment` and only apply some defaults to it. This lead to
    several sample applications failing as properties provided by the
    application (in `application.properties`) would not be considered.
    
    As a consequence, all `@ConditionalOnProperty` on `*.enabled`
    configuration keys would be discarded at build time if not enabled by
    default in Spring Boot.
    
    This commit loads a minimal infrastructure and prepares an `Environment`
    that partially loads the application configuration at build time.
    
    There are many limitations with this current approach:
    
    * this doesn't support many customizations on the `SpringApplication`
    * this doesn't load `Bootstrapper` so will fail with configuration
      trying to resolve properties using `Bootstrappers`, like Eureka.
    
    Fixes gh-992

--- spring-aot/src/main/java/org/springframework/aot/context/bootstrap/ContextBootstrapContributor.java
+++ spring-aot/src/main/java/org/springframework/aot/context/bootstrap/ContextBootstrapContributor.java
@@ -25,20 +25,27 @@ import org.springframework.aot.BootstrapContributor;
 import org.springframework.aot.BuildContext;
 import org.springframework.aot.SourceFiles;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.boot.DefaultBootstrapContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.context.config.ConfigDataEnvironmentPostProcessor;
+import org.springframework.boot.context.properties.source.ConfigurationPropertySources;
+import org.springframework.boot.convert.ApplicationConversionService;
+import org.springframework.boot.logging.DeferredLogs;
 import org.springframework.boot.web.reactive.context.AnnotationConfigReactiveWebServerApplicationContext;
+import org.springframework.boot.web.reactive.context.StandardReactiveWebEnvironment;
 import org.springframework.boot.web.servlet.context.AnnotationConfigServletWebServerApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.BuildTimeBeanDefinitionsRegistrar;
 import org.springframework.context.bootstrap.generator.BootstrapGenerationResult;
 import org.springframework.context.bootstrap.generator.ContextBootstrapGenerator;
 import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.PropertiesPropertySource;
 import org.springframework.core.env.StandardEnvironment;
-import org.springframework.core.type.classreading.ClassDescriptor;
-import org.springframework.core.type.classreading.TypeSystem;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.nativex.AotOptions;
 import org.springframework.util.ClassUtils;
+import org.springframework.web.context.support.StandardServletEnvironment;
 
 /**
  * @author Brian Clozel
@@ -48,45 +55,41 @@ import org.springframework.util.ClassUtils;
 public class ContextBootstrapContributor implements BootstrapContributor {
 
 	// Copied from Spring Boot WebApplicationType
-	private static final String[] SERVLET_INDICATOR_CLASSES = { "javax.servlet.Servlet",
-			"org.springframework.web.context.ConfigurableWebApplicationContext" };
+	private static final String[] SERVLET_INDICATOR_CLASSES = {"javax.servlet.Servlet",
+			"org.springframework.web.context.ConfigurableWebApplicationContext"};
+
 	private static final String WEBMVC_INDICATOR_CLASS = "org.springframework.web.servlet.DispatcherServlet";
+
 	private static final String WEBFLUX_INDICATOR_CLASS = "org.springframework.web.reactive.DispatcherHandler";
+
 	private static final String JERSEY_INDICATOR_CLASS = "org.glassfish.jersey.servlet.ServletContainer";
 
 	private static final Log logger = LogFactory.getLog(ContextBootstrapContributor.class);
 
 	@Override
 	public void contribute(BuildContext context, AotOptions aotOptions) {
-		TypeSystem typeSystem = context.getTypeSystem();
-		ClassLoader classLoader = typeSystem.getResourceLoader().getClassLoader();
-
-		// TODO: detect correct type of application context
-		GenericApplicationContext applicationContext = createApplicationContext(typeSystem);
-		applicationContext.setResourceLoader(typeSystem.getResourceLoader());
-
-		// TODO: pre-compute environment from properties?
-		ConfigurableEnvironment environment = new StandardEnvironment();
-		Properties properties = new Properties();
-		properties.put("spring.aop.proxy-target-class", "false"); // Not supported in native images
-		properties.put("spring.cloud.refresh.enabled", "false"); // Sampler is a class and can't be proxied
-		properties.put("spring.sleuth.async.enabled", "false"); // Too much proxy created
-		properties.put("spring.devtools.restart.enabled", "false"); // Deactivate dev tools
-		environment.getPropertySources().addFirst(new PropertiesPropertySource("native", properties));
-		applicationContext.setEnvironment(environment);
-
-		// TODO: auto-detect main class
+		ResourceLoader resourceLoader = context.getTypeSystem().getResourceLoader();
+		ClassLoader classLoader = resourceLoader.getClassLoader();
+		Class<?> mainClass;
 		String mainClassName = context.getMainClass();
-		if (mainClassName != null) {
-			ClassDescriptor mainClass = typeSystem.resolveClass(mainClassName);
-			logger.info("Detected main class: " + mainClass.getCanonicalClassName());
-			try {
-				applicationContext.registerBean(ClassUtils.forName(mainClass.getCanonicalClassName(), classLoader));
-			}
-			catch (ClassNotFoundException exc) {
-				throw new IllegalStateException("Could not load main class" + mainClass.getCanonicalClassName(), exc);
-			}
+		try {
+			logger.info("Detected main class: " + mainClassName);
+			mainClass = ClassUtils.forName(mainClassName, classLoader);
 		}
+		catch (ClassNotFoundException exc) {
+			throw new IllegalStateException("Could not load main class" + mainClassName, exc);
+		}
+
+		WebApplicationType webApplicationType = deduceWebApplicationType(classLoader);
+		GenericApplicationContext applicationContext = createApplicationContext(webApplicationType);
+		applicationContext.setResourceLoader(resourceLoader);
+
+		StandardEnvironment environment = createEnvironment(webApplicationType);
+		configureEnvironment(environment, resourceLoader, mainClass);
+
+		applicationContext.setEnvironment(environment);
+		applicationContext.registerBean(mainClass);
+
 		ConfigurableListableBeanFactory beanFactory = new BuildTimeBeanDefinitionsRegistrar().processBeanDefinitions(applicationContext);
 		ContextBootstrapGenerator bootstrapGenerator = new ContextBootstrapGenerator(classLoader);
 		BootstrapGenerationResult bootstrapGenerationResult = bootstrapGenerator.generateBootstrapClass(beanFactory, "org.springframework.aot");
@@ -94,18 +97,56 @@ public class ContextBootstrapContributor implements BootstrapContributor {
 		context.describeReflection((reflectionDescriptor) -> bootstrapGenerationResult.getClassDescriptors().forEach(reflectionDescriptor::merge));
 	}
 
+	private void configureEnvironment(StandardEnvironment environment, ResourceLoader resourceLoader, Class<?> mainClass) {
+		environment.setConversionService(new ApplicationConversionService());
+		ConfigurationPropertySources.attach(environment);
+		ConfigDataEnvironmentPostProcessor configDataEnvironmentPostProcessor =
+				new ConfigDataEnvironmentPostProcessor(new DeferredLogs(), new DefaultBootstrapContext());
+
+		SpringApplication application = new SpringApplication(resourceLoader, mainClass);
+		configDataEnvironmentPostProcessor.postProcessEnvironment(environment, application);
+
+		Properties properties = new Properties();
+		properties.put("spring.aop.proxy-target-class", "false"); // Not supported in native images
+		properties.put("spring.cloud.refresh.enabled", "false"); // Sampler is a class and can't be proxied
+		properties.put("spring.sleuth.async.enabled", "false"); // Too much proxy created
+		properties.put("spring.devtools.restart.enabled", "false"); // Deactivate dev tools
+		environment.getPropertySources().addFirst(new PropertiesPropertySource("native", properties));
+	}
+
 	// TODO Avoid duplication with WebApplicationType and SpringAotApplication.AOT_FACTORY
-	private GenericApplicationContext createApplicationContext(TypeSystem typeSystem) {
-		if (typeSystem.resolveClass(WEBFLUX_INDICATOR_CLASS) != null && typeSystem.resolveClass(WEBMVC_INDICATOR_CLASS) == null
-				&& typeSystem.resolveClass(JERSEY_INDICATOR_CLASS) == null) {
-			return ReactiveContextDelegate.createApplicationContext();
+	private GenericApplicationContext createApplicationContext(WebApplicationType webApplicationType) {
+		switch (webApplicationType) {
+			case REACTIVE:
+				return ReactiveContextDelegate.createApplicationContext();
+			case SERVLET:
+				return ServletContextDelegate.createApplicationContext();
+		}
+		return new AnnotationConfigApplicationContext();
+	}
+
+	private StandardEnvironment createEnvironment(WebApplicationType webApplicationType) {
+		switch (webApplicationType) {
+			case SERVLET:
+				return ServletEnvironmentDelegate.createServletEnvironment();
+			case REACTIVE:
+				return new StandardReactiveWebEnvironment();
+			default:
+				return new StandardEnvironment();
+		}
+	}
+
+	private WebApplicationType deduceWebApplicationType(ClassLoader classLoader) {
+		if (ClassUtils.isPresent(WEBFLUX_INDICATOR_CLASS, classLoader) && !ClassUtils.isPresent(WEBMVC_INDICATOR_CLASS, classLoader)
+				&& !ClassUtils.isPresent(JERSEY_INDICATOR_CLASS, classLoader)) {
+			return WebApplicationType.REACTIVE;
 		}
 		for (String className : SERVLET_INDICATOR_CLASSES) {
-			if (typeSystem.resolveClass(className) == null) {
-				return new AnnotationConfigApplicationContext();
+			if (!ClassUtils.isPresent(className, classLoader)) {
+				return WebApplicationType.NONE;
 			}
 		}
-		return ServletContextDelegate.createApplicationContext();
+		return WebApplicationType.SERVLET;
 	}
 
 	// To avoid NoClassDefFoundError:
@@ -113,6 +154,14 @@ public class ContextBootstrapContributor implements BootstrapContributor {
 
 		public static GenericApplicationContext createApplicationContext() {
 			return new AnnotationConfigServletWebServerApplicationContext();
+		}
+	}
+
+	// To avoid NoClassDefFoundError:
+	static class ServletEnvironmentDelegate {
+
+		public static StandardEnvironment createServletEnvironment() {
+			return new StandardServletEnvironment();
 		}
 	}
 
