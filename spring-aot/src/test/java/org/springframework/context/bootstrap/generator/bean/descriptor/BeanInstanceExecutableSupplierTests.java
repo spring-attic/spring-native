96a681a7898b9c77a73774e68ca5fb3bec442c73
Brian Clozel <bclozel@vmware.com>
2021-09-08 11:48:34 +0200

    Select appropriate constructor for bean instantiation
    Prior to this commit, the `BeanInstanceExecutableSupplier` used the
    resolved factory method or constructor method information to find the
    Executable to be called for instantiation.
    
    If no info is available at the bean definition level, this was trying to
    detect a suitable constructor - by applying a simple resolution
    algorithm that detects a single constructor or a constructor annotated
    with @Autowired.
    
    This commit adds a simple algorithm for finding a suitable constructor
    that fits the given constructor arguments.
    
    Fixes gh-1015

--- spring-aot/src/test/java/org/springframework/context/bootstrap/generator/bean/descriptor/BeanInstanceExecutableSupplierTests.java
+++ spring-aot/src/test/java/org/springframework/context/bootstrap/generator/bean/descriptor/BeanInstanceExecutableSupplierTests.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.context.bootstrap.generator.sample.constructor.SampleBeanWithConstructors;
 import org.springframework.context.bootstrap.generator.sample.factory.SampleFactory;
 import org.springframework.util.ReflectionUtils;
 
@@ -39,6 +40,30 @@ class BeanInstanceExecutableSupplierTests {
 				.addConstructorArgReference("testBean").getBeanDefinition();
 		Executable executable = detectBeanInstanceExecutable(beanFactory, beanDefinition);
 		assertThat(executable).isNotNull().isEqualTo(ReflectionUtils.findMethod(SampleFactory.class, "create", Number.class, String.class));
+	}
+
+	@Test
+	void beanDefinitionWithConstructorArgsForMultipleConstructors() throws Exception {
+		DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+		beanFactory.registerSingleton("testNumber", 1L);
+		beanFactory.registerSingleton("testBean", "test");
+		BeanDefinition beanDefinition = BeanDefinitionBuilder.rootBeanDefinition(SampleBeanWithConstructors.class.getName())
+				.addConstructorArgReference("testNumber")
+				.addConstructorArgReference("testBean").getBeanDefinition();
+		Executable executable = detectBeanInstanceExecutable(beanFactory, beanDefinition);
+		assertThat(executable).isNotNull().isEqualTo(SampleBeanWithConstructors.class.getDeclaredConstructor(String.class, Number.class));
+	}
+
+	@Test
+	void genericBeanDefinitionWithConstructorArgsForMultipleConstructors() throws Exception {
+		DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+		beanFactory.registerSingleton("testNumber", 1L);
+		beanFactory.registerSingleton("testBean", "test");
+		BeanDefinition beanDefinition = BeanDefinitionBuilder.genericBeanDefinition(SampleBeanWithConstructors.class.getName())
+				.addConstructorArgReference("testNumber")
+				.addConstructorArgReference("testBean").getBeanDefinition();
+		Executable executable = detectBeanInstanceExecutable(beanFactory, beanDefinition);
+		assertThat(executable).isNotNull().isEqualTo(SampleBeanWithConstructors.class.getDeclaredConstructor(String.class, Number.class));
 	}
 
 	private Executable detectBeanInstanceExecutable(DefaultListableBeanFactory beanFactory, BeanDefinition beanDefinition) {
