afd781cec75ffe37546a8310b985b47b041ead53
Stephane Nicoll <snicoll@vmware.com>
2021-09-10 14:57:52 +0200

    Generation reflection entries for bean annotations
    This commit adds additional entries for Spring Framework annotations
    presents on injection points and at class-level.
    
    Closes gh-1023

--- spring-aot/src/main/java/org/springframework/context/bootstrap/generator/infrastructure/BeanRuntimeResourcesRegistrar.java
+++ spring-aot/src/main/java/org/springframework/context/bootstrap/generator/infrastructure/BeanRuntimeResourcesRegistrar.java
@@ -4,11 +4,18 @@ import java.lang.reflect.Executable;
 import java.lang.reflect.Field;
 import java.lang.reflect.Member;
 import java.lang.reflect.Method;
+import java.util.List;
 
 import org.springframework.context.bootstrap.generator.bean.descriptor.BeanInstanceDescriptor;
 import org.springframework.context.bootstrap.generator.bean.descriptor.BeanInstanceDescriptor.MemberDescriptor;
 import org.springframework.context.bootstrap.generator.bean.descriptor.BeanInstanceDescriptor.PropertyDescriptor;
 import org.springframework.context.bootstrap.generator.infrastructure.reflect.RuntimeReflectionRegistry;
+import org.springframework.core.annotation.MergedAnnotation;
+import org.springframework.core.annotation.MergedAnnotations;
+import org.springframework.core.annotation.MergedAnnotations.SearchStrategy;
+import org.springframework.nativex.hint.Flag;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Indexed;
 
 /**
  * Handle reflection and resources metadata necessary at runtime.
@@ -17,6 +24,8 @@ import org.springframework.context.bootstrap.generator.infrastructure.reflect.Ru
  */
 public class BeanRuntimeResourcesRegistrar {
 
+	private static List<Class<?>> IGNORED_TYPES = List.of(Indexed.class, Component.class);
+
 	/**
 	 * Register the reflection and resources information necessary to instantiate the
 	 * bean defined by the specified {@link BeanInstanceDescriptor}.
@@ -24,23 +33,52 @@ public class BeanRuntimeResourcesRegistrar {
 	 * @param descriptor the descriptor of the bean instance ot handle
 	 */
 	public void register(RuntimeReflectionRegistry registry, BeanInstanceDescriptor descriptor) {
+		addClass(registry, descriptor.getUserBeanClass());
 		MemberDescriptor<Executable> instanceCreator = descriptor.getInstanceCreator();
-		registry.addMethod(instanceCreator.getMember());
+		if (instanceCreator != null) {
+			addMethod(registry, instanceCreator.getMember());
+		}
 		for (MemberDescriptor<?> injectionPoint : descriptor.getInjectionPoints()) {
 			Member member = injectionPoint.getMember();
 			if (member instanceof Executable) {
-				registry.addMethod((Method) member);
+				addMethod(registry, (Method) member);
 			}
 			else if (member instanceof Field) {
-				registry.addField((Field) member);
+				addField(registry, (Field) member);
 			}
 		}
 		for (PropertyDescriptor property : descriptor.getProperties()) {
 			Method writeMethod = property.getWriteMethod();
 			if (writeMethod != null) {
-				registry.addMethod(writeMethod);
+				addMethod(registry, writeMethod);
 			}
 		}
+	}
+
+	private void addClass(RuntimeReflectionRegistry registry, Class<?> type) {
+		registerAnnotations(registry, MergedAnnotations.from(type, SearchStrategy.INHERITED_ANNOTATIONS));
+	}
+
+	private void addMethod(RuntimeReflectionRegistry registry, Executable executable) {
+		registry.addMethod(executable);
+		registerAnnotations(registry, MergedAnnotations.from(executable));
+	}
+
+	private void addField(RuntimeReflectionRegistry registry, Field field) {
+		registry.addField(field);
+		registerAnnotations(registry, MergedAnnotations.from(field));
+	}
+
+	private void registerAnnotations(RuntimeReflectionRegistry registry, MergedAnnotations annotations) {
+		annotations.stream().filter(this::isRuntimeFrameworkAnnotation)
+				.forEach((ann) -> registry.add(ann.getType()).withFlags(Flag.allDeclaredMethods));
+	}
+
+	private boolean isRuntimeFrameworkAnnotation(MergedAnnotation<?> annotation) {
+		String name = annotation.getType().getName();
+		boolean candidate = name.startsWith("org.springframework.") &&
+				!name.startsWith("org.springframework.context.annotation");
+		return candidate && !IGNORED_TYPES.contains(annotation.getType());
 	}
 
 }
--- spring-aot/src/test/java/org/springframework/context/bootstrap/generator/infrastructure/BeanRuntimeResourcesRegistrarTests.java
+++ spring-aot/src/test/java/org/springframework/context/bootstrap/generator/infrastructure/BeanRuntimeResourcesRegistrarTests.java
@@ -1,16 +1,23 @@
 package org.springframework.context.bootstrap.generator.infrastructure;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.function.Consumer;
 
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.PropertyValue;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.bootstrap.generator.bean.descriptor.BeanInstanceDescriptor;
+import org.springframework.context.bootstrap.generator.infrastructure.reflect.RuntimeReflectionEntry;
 import org.springframework.context.bootstrap.generator.infrastructure.reflect.RuntimeReflectionRegistry;
+import org.springframework.context.bootstrap.generator.sample.callback.AsyncConfiguration;
 import org.springframework.context.bootstrap.generator.sample.injection.InjectionComponent;
 import org.springframework.context.bootstrap.generator.sample.injection.InjectionConfiguration;
+import org.springframework.nativex.hint.Flag;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.util.ReflectionUtils;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -42,11 +49,16 @@ class BeanRuntimeResourcesRegistrarTests {
 		Method injectionPoint = ReflectionUtils.findMethod(InjectionComponent.class, "setCounter", Integer.class);
 		RuntimeReflectionRegistry registry = register(BeanInstanceDescriptor.of(InjectionComponent.class)
 				.withInstanceCreator(instanceCreator).withInjectionPoint(injectionPoint, false).build());
-		assertThat(registry.getEntries()).singleElement().satisfies((entry) -> {
+		assertThat(registry.getEntries()).anySatisfy((entry) -> {
 			assertThat(entry.getType()).isEqualTo(InjectionComponent.class);
 			assertThat(entry.getMethods()).containsOnly(instanceCreator, injectionPoint);
 			assertThat(entry.getFields()).isEmpty();
 		});
+		assertThat(registry.getEntries()).anySatisfy((entry) -> {
+			assertThat(entry.getType()).isEqualTo(Autowired.class);
+			assertThat(entry.getFlags()).containsOnly(Flag.allDeclaredMethods);
+		});
+		assertThat(registry.getEntries()).hasSize(2);
 	}
 
 	@Test
@@ -75,6 +87,21 @@ class BeanRuntimeResourcesRegistrarTests {
 			assertThat(entry.getMethods()).containsOnly(instanceCreator, nameWriteMethod, counterWriteMethod);
 			assertThat(entry.getFields()).isEmpty();
 		});
+	}
+
+	@Test
+	void registerReflectionEntriesForClassRuntimeAnnotations() {
+		RuntimeReflectionRegistry registry = register(BeanInstanceDescriptor.of(AsyncConfiguration.class).build());
+		assertThat(registry.getEntries()).singleElement().satisfies(annotation(EnableAsync.class));
+	}
+
+	private Consumer<RuntimeReflectionEntry> annotation(Class<? extends Annotation> annotationType) {
+		return (entry) -> {
+			assertThat(entry.getType()).isEqualTo(annotationType);
+			assertThat(entry.getFlags()).containsOnly(Flag.allDeclaredMethods);
+			assertThat(entry.getMethods()).isEmpty();
+			assertThat(entry.getFields()).isEmpty();
+		};
 	}
 
 	private RuntimeReflectionRegistry register(BeanInstanceDescriptor descriptor) {
