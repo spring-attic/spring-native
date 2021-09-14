3f5a40f667db0b19fa1e2c37baf3ed71848d0d15
Stephane Nicoll <snicoll@vmware.com>
2021-09-14 13:06:18 +0200

    Associate target type for members rather than declaring class

--- spring-aot/src/main/java/org/springframework/context/bootstrap/generator/nativex/DefaultBeanNativeConfigurationProcessor.java
+++ spring-aot/src/main/java/org/springframework/context/bootstrap/generator/nativex/DefaultBeanNativeConfigurationProcessor.java
@@ -17,7 +17,7 @@ import org.springframework.context.bootstrap.generator.bean.descriptor.BeanInsta
 import org.springframework.context.bootstrap.generator.bean.descriptor.DefaultBeanInstanceDescriptorFactory;
 import org.springframework.context.bootstrap.generator.infrastructure.nativex.BeanNativeConfigurationProcessor;
 import org.springframework.context.bootstrap.generator.infrastructure.nativex.NativeConfigurationRegistry;
-import org.springframework.context.bootstrap.generator.infrastructure.nativex.NativeConfigurationRegistry.ReflectionConfiguration;
+import org.springframework.context.bootstrap.generator.infrastructure.nativex.NativeReflectionEntry.Builder;
 
 /**
  * Register the reflection entries for each {@link BeanInstanceDescriptor}.
@@ -36,24 +36,24 @@ class DefaultBeanNativeConfigurationProcessor implements BeanNativeConfiguration
 
 	@Override
 	public void process(BeanInstanceDescriptor descriptor, NativeConfigurationRegistry registry) {
-		ReflectionConfiguration reflectionConfiguration = registry.reflection();
+		Builder builder = registry.reflection().forType(descriptor.getUserBeanClass());
 		MemberDescriptor<Executable> instanceCreator = descriptor.getInstanceCreator();
 		if (instanceCreator != null) {
-			reflectionConfiguration.addExecutable(instanceCreator.getMember());
+			registry.reflection().addExecutable(instanceCreator.getMember());
 		}
 		for (MemberDescriptor<?> injectionPoint : descriptor.getInjectionPoints()) {
 			Member member = injectionPoint.getMember();
 			if (member instanceof Executable) {
-				reflectionConfiguration.addExecutable((Method) member);
+				builder.withMethods((Method) member);
 			}
 			else if (member instanceof Field) {
-				reflectionConfiguration.addField((Field) member);
+				builder.withFields((Field) member);
 			}
 		}
 		for (PropertyDescriptor property : descriptor.getProperties()) {
 			Method writeMethod = property.getWriteMethod();
 			if (writeMethod != null) {
-				reflectionConfiguration.addExecutable(writeMethod);
+				builder.withMethods(writeMethod);
 			}
 			Object value = property.getPropertyValue().getValue();
 			if (value instanceof BeanDefinition) {
