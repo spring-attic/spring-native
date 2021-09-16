f64f445fede31141028fd2c1305a76eaccfa8d8c
Stephane Nicoll <snicoll@vmware.com>
2021-09-16 15:20:20 +0200

    Fix detection of constructor with multiple matches
    This commit uses the resolved parameter types, including detection of
    inner bean definition, rather than query ConstructorArgumentValues if
    it can process a given type. The latter will transparently convert the
    value if necessary, leading to detecting a constructor that will not
    ultimately match.
    
    Closes gh-1035

--- spring-aot/src/test/java/org/springframework/context/bootstrap/generator/sample/constructor/SampleBeanWithConstructors.java
+++ spring-aot/src/test/java/org/springframework/context/bootstrap/generator/sample/constructor/SampleBeanWithConstructors.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2021 the original author or authors.
+ * Copyright 2019-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,18 +16,16 @@
 
 package org.springframework.context.bootstrap.generator.sample.constructor;
 
+@SuppressWarnings("unused")
 public class SampleBeanWithConstructors {
 
 	public SampleBeanWithConstructors() {
-
 	}
 
 	public SampleBeanWithConstructors(String name) {
-
 	}
 
-	public SampleBeanWithConstructors(String name, Number number) {
-
+	public SampleBeanWithConstructors(Number number, String name) {
 	}
 
 }
