0e239b97ab3477faa9b30637aafb830eea4eecd0
Sébastien Deleuze <sdeleuze@vmware.com>
2021-09-21 19:07:17 +0200

    Fix validator sample verification in AOT mode

--- samples/validator/verify.sh
+++ samples/validator/verify.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
-RESPONSE=`./target/validator -Dapp.value=123 2>&1 | grep "Reason: Invalid lastname"`
+
+if [ -f ./target/validator ]; then
+  EXECUTABLE="./target/validator -Dapp.value=123"
+else
+  EXECUTABLE="java -DspringAot=true -Dapp.value=123 -jar target/*.jar"
+fi
+
+RESPONSE=`$EXECUTABLE 2>&1 | grep "Reason: Invalid lastname"`
 if [[ "$RESPONSE" == *"Reason: Invalid lastname"* ]]; then
     RESPONSE2=`curl -d 'testIntMin=-1' -s localhost:8080/validateForm`
     if [[ "$RESPONSE2" == 'Validation failed: -1' ]]; then
