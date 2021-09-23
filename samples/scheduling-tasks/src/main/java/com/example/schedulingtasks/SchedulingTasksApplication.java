e67b7ec7507ba4ca44d1bb1ad9b0b0565d4e4e73
Andy Clement <aclement@pivotal.io>
2021-09-22 19:38:42 -0700

    Remove instantiation loop

--- samples/scheduling-tasks/src/main/java/com/example/schedulingtasks/SchedulingTasksApplication.java
+++ samples/scheduling-tasks/src/main/java/com/example/schedulingtasks/SchedulingTasksApplication.java
@@ -1,6 +1,5 @@
 package com.example.schedulingtasks;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -9,9 +8,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class SchedulingTasksApplication {
-	
-	@Autowired
-	ScheduledTasksNonComponent scheduledTasks;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SchedulingTasksApplication.class);
