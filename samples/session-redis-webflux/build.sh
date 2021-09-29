9a7eae3f785020df384908ef387d4f04bf0392e4
Eleftheria Stein <estein@vmware.com>
2021-09-29 10:40:06 +0200

    Use docker compose to run session-redis-webflux sample

--- samples/session-redis-webflux/build.sh
+++ samples/session-redis-webflux/build.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-${PWD%/*samples/*}/scripts/compileWithMaven.sh $* &&  ${PWD%/*samples/*}/scripts/test.sh $*
+RC=0
+
+docker-compose up -d
+${PWD%/*samples/*}/scripts/compileWithMaven.sh $* &&  ${PWD%/*samples/*}/scripts/test.sh $* || RC=$?
+docker-compose down
+
+exit $RC
