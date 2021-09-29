de4415e9394df20d604165ca68f78c91fe991f76
Olga MaciaszekSharma <olga.maciaszek@gmail.com>
2021-09-29 14:59:41 +0200

    Reduce EurekaClient hints. Add actuator to sample.

--- spring-native-configuration/src/main/java/org/springframework/cloud/netflix/eureka/EurekaClientHints.java
+++ spring-native-configuration/src/main/java/org/springframework/cloud/netflix/eureka/EurekaClientHints.java
@@ -16,19 +16,12 @@
 
 package org.springframework.cloud.netflix.eureka;
 
-import com.netflix.appinfo.ApplicationInfoManager;
-import com.netflix.appinfo.DataCenterInfo;
-import com.netflix.appinfo.EurekaInstanceConfig;
 import com.netflix.appinfo.InstanceInfo;
 import com.netflix.appinfo.LeaseInfo;
-import com.netflix.appinfo.MyDataCenterInfo;
 import com.netflix.discovery.DiscoveryClient;
 import com.netflix.discovery.converters.jackson.DataCenterTypeInfoResolver;
 import com.netflix.discovery.converters.jackson.builder.ApplicationsJacksonBuilder;
-import com.netflix.discovery.shared.Application;
-import com.netflix.discovery.shared.Applications;
 
-import org.springframework.cloud.netflix.eureka.http.EurekaApplications;
 import org.springframework.cloud.netflix.eureka.loadbalancer.EurekaLoadBalancerClientConfiguration;
 import org.springframework.cloud.netflix.eureka.loadbalancer.LoadBalancerEurekaAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.reactive.EurekaReactiveDiscoveryClientConfiguration;
@@ -37,7 +30,6 @@ import org.springframework.nativex.hint.NativeHint;
 import org.springframework.nativex.hint.TypeHint;
 import org.springframework.nativex.type.NativeConfiguration;
 
-// TODO Not sure AccessBits.ALL is needed on these, to be checked
 @NativeHint(trigger = LoadBalancerEurekaAutoConfiguration.class, types = {
 		@TypeHint(types = {
 				EurekaLoadBalancerClientConfiguration.class,
@@ -46,22 +38,11 @@ import org.springframework.nativex.type.NativeConfiguration;
 })
 @NativeHint(trigger = EurekaClientAutoConfiguration.class, types = {
 		@TypeHint(types = {
-				ApplicationInfoManager.class,
-				EurekaInstanceConfig.class,
 				DiscoveryClient.class,
-				EurekaApplications.class,
-				Applications.class,
-				Application.class,
 				ApplicationsJacksonBuilder.class,
-				DataCenterInfo.class,
-				DataCenterInfo.Name.class,
-				MyDataCenterInfo.class,
 				InstanceInfo.class,
 				DataCenterTypeInfoResolver.class,
 				LeaseInfo.class
-		}, typeNames = {
-				"com.netflix.discovery.DiscoveryClient$EurekaTransport",
-				"com.netflix.appinfo.InstanceInfo$PortWrapper"
 		}, access = AccessBits.ALL)
 })
 @NativeHint(trigger = EurekaDiscoveryClientConfiguration.class,
