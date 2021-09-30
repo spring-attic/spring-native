496fdcc73543261a5b362fe46de962046ee18049
Olga MaciaszekSharma <olga.maciaszek@gmail.com>
2021-09-30 15:46:34 +0200

    Reduce Sleuth hints.

--- samples/zipkin-sleuth/src/main/java/com/example/zipkin/sleuth/ZipkinSleuthApplication.java
+++ samples/zipkin-sleuth/src/main/java/com/example/zipkin/sleuth/ZipkinSleuthApplication.java
@@ -2,10 +2,8 @@ package com.example.zipkin.sleuth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.nativex.hint.NativeHint;
 
 @SpringBootApplication
-@NativeHint(options = "--enable-url-protocols=http")
 public class ZipkinSleuthApplication {
 
 	public static void main(String[] args) {
