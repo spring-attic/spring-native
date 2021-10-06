7f234eca15a47143478383cc2a7c961c283ba8f9
Eleftheria Stein <estein@vmware.com>
2021-10-06 16:36:02 +0200

    Improve verification for security samples

--- samples/security-webflux/verify.sh
+++ samples/security-webflux/verify.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 source ${PWD%/*samples/*}/scripts/wait.sh
-wait_log target/native/test-output.txt "Started SecurityWebfluxApplication"
+trap 'wait_log target/native/test-output.txt "Started SecurityWebfluxApplication"' ERR
+trap 'wait_http localhost:8080/hello "Unauthorized"' ERR
+trap 'wait_http user:password@localhost:8080/hello "Hello!"' ERR
+trap 'wait_http user:password@localhost:8080/admin "Forbidden"' ERR
+wait_http admin:password@localhost:8080/admin "Welcome to the administrator page!"
