e181249648b7af0fa5789df31a6be0b4ae0094fc
Eleftheria Stein <estein@vmware.com>
2021-10-06 16:36:02 +0200

    Improve verification for security samples

--- samples/security-method/verify.sh
+++ samples/security-method/verify.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 source ${PWD%/*samples/*}/scripts/wait.sh
-wait_log target/native/test-output.txt "Started MethodSecurityApplication"
+trap 'wait_log target/native/test-output.txt "Started MethodSecurityApplication"' ERR
+trap 'wait_http localhost:8080/hello "Unauthorized"' ERR
+trap 'wait_http user:password@localhost:8080/hello "Hello!"' ERR
+wait_http admin:password@localhost:8080/admin/hello "Goodbye!"
--- samples/security/verify.sh
+++ samples/security/verify.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 source ${PWD%/*samples/*}/scripts/wait.sh
-wait_log target/native/test-output.txt "Started SecuringWebApplication"
+trap 'wait_log target/native/test-output.txt "Started SecuringWebApplication"' ERR
+trap 'wait_http localhost:8080/home "Welcome"' ERR
+wait_command_output 'curl -I localhost:8080/hello' "HTTP/1.1 302"
