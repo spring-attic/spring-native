dd1b4561d22439e51e3eb849f94396131d838e9e
Sébastien Deleuze <sdeleuze@vmware.com>
2021-10-22 09:27:46 +0200

    Upgrade to Spring Boot 2.6.0-RC1
    Closes gh-1128

--- samples/webmvc-kotlin/build.gradle.kts
+++ samples/webmvc-kotlin/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 
 plugins {
-    id("org.springframework.boot") version "2.6.0-SNAPSHOT"
+    id("org.springframework.boot") version "2.6.0-RC1"
     id("io.spring.dependency-management") version "1.0.10.RELEASE"
     id("org.springframework.experimental.aot") version "0.11.0-SNAPSHOT"
     kotlin("jvm") version "1.5.21"
