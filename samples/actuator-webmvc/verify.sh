e6296daf7cdb0cd44aa0d7fee5fd7e968bd925c2
Petr Hejl <petr.hejl@memsource.com>
2021-10-24 18:24:06 +0200

    1178 - Custom configured readiness probes not working in native image - test

--- samples/actuator-webmvc/verify.sh
+++ samples/actuator-webmvc/verify.sh
@@ -3,6 +3,7 @@ source ${PWD%/*samples/*}/scripts/wait.sh
 RC=0
 
 wait_log target/native/test-output.txt "Started ActuatorApplication in" || RC=$?
+wait_http localhost:8080/actuator/health/readiness "customIndicator" || RC=$?
 wait_http localhost:8080/actuator/health "UP" || RC=$?
 
 exit $RC
