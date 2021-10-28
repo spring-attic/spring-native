b4f1ea62a0c10e7ba51054a81b42f75a00864df8
Sébastien Deleuze <sdeleuze@vmware.com>
2021-10-28 10:52:10 +0200

    Fix quotes in rsocket verify.sh

--- samples/rsocket/verify.sh
+++ samples/rsocket/verify.sh
@@ -2,7 +2,7 @@
 source ${PWD%/*samples/*}/scripts/wait.sh
 RC=0
 
-wait_command_output "rsc --debug --request --data \"{\"origin\":\"Client\",\"interaction\":\"Request\"}\" --route request-response tcp://localhost:7000" "Frame => Stream ID: 1 Type: NEXT_COMPLETE Flags: 0b1100000 Length: 81" || RC=$?
+wait_command_output 'rsc --debug --request --data "{\"origin\":\"Client\",\"interaction\":\"Request\"}" --route "request-response" tcp://localhost:7000' "Frame => Stream ID: 1 Type: NEXT_COMPLETE Flags: 0b1100000 Length: 81" || RC=$?
 wait_command_output 'rsc --debug --request --data "{\"origin\":\"Client\",\"interaction\":\"Request\"}" --route mono-request-response tcp://localhost:7000' "Frame => Stream ID: 1 Type: NEXT_COMPLETE Flags: 0b1100000 Length: 81" || RC=$?
 
 exit $RC
