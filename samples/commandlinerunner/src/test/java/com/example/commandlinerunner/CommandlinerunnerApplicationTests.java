2674d526cbae70a6b1e22a64de3ac37b230016ba
Sam Brannen <sam@sambrannen.com>
2021-11-05 14:53:51 +0100

    Ensure CLR tests fail in native image if ApplicationContext doesn't load
    Prior to this commit, the CommandLineRunner (CLR) sample applications
    each had a CommandlinerunnerApplicationTests test class that did not
    assert anything.
    
    Consequently, since the switch to AOT testing support, the tests have
    been passing even though the ApplicationContext was not loaded in the
    native image, which results in false positives in the build.
    
    This commit addresses this issue by modifying each of those test classes
    to implement ApplicationContextAware and then check that the supplied
    context is not null.
    
    This is intentionally done via the ApplicationContextAware callback
    instead of autowiring, since we will address proper auto-wiring support
    in the near future.
    
    Closes gh-1216

--- samples/commandlinerunner/src/test/java/com/example/commandlinerunner/CommandlinerunnerApplicationTests.java
+++ samples/commandlinerunner/src/test/java/com/example/commandlinerunner/CommandlinerunnerApplicationTests.java
@@ -1,14 +1,28 @@
+
 package com.example.commandlinerunner;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 
+import org.springframework.beans.BeansException;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
 @SpringBootTest
-public class CommandlinerunnerApplicationTests {
+public class CommandlinerunnerApplicationTests implements ApplicationContextAware {
+
+	private ApplicationContext applicationContext;
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.applicationContext = applicationContext;
+	}
 
 	@Test
 	public void contextLoads() {
+		assertNotNull(this.applicationContext, "ApplicationContext");
 	}
 
 }
