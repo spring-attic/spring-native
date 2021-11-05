8143661342e1bcb4038d729df41087f90de58969
Stephane Nicoll <snicoll@vmware.com>
2021-11-05 14:32:40 +0100

    Polish

--- samples/petclinic-jpa/src/test/java/org/springframework/samples/petclinic/PetclinicIntegrationTests.java
+++ samples/petclinic-jpa/src/test/java/org/springframework/samples/petclinic/PetclinicIntegrationTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2019 the original author or authors.
+ * Copyright 2012-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,6 +18,7 @@ package org.springframework.samples.petclinic;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.samples.petclinic.vet.VetRepository;
@@ -25,7 +26,6 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-@Disabled // TODO Profiles not supported yet, see https://github.com/spring-projects-experimental/spring-native/issues/764
 class PetclinicIntegrationTests {
 
 	@Autowired
