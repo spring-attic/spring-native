1f4e2bd82f1d00091488ce808ea53d7ba660cb31
Sébastien Deleuze <sdeleuze@vmware.com>
2021-11-16 19:41:49 +0100

    Fix build-key-samples.sh return code

--- ci/scripts/build-key-samples.sh
+++ ci/scripts/build-key-samples.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+RC=0
+
 echo "GraalVM build information"
 echo ""
 cat $JAVA_HOME/release
@@ -10,5 +12,10 @@ if [[ -n $DOCKER_HUB_USERNAME ]]; then
 	echo "$DOCKER_HUB_PASSWORD" | docker login -u $DOCKER_HUB_USERNAME --password-stdin
 fi
 cd git-repo
-./build.sh
-./build-key-samples.sh
+if ! (./build.sh); then
+    RC=1
+fi
+if ! (./build-key-samples.sh); then
+    RC=1
+fi
+exit $RC
