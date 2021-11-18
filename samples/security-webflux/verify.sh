9e168b8ac69372a0be159ba7b00032350b48a517
Eleftheria Stein <estein@vmware.com>
2021-11-18 17:05:47 +0100

    Fix verification script for security WebFlux sample

--- samples/security-webflux/verify.sh
+++ samples/security-webflux/verify.sh
@@ -3,9 +3,9 @@ source ${PWD%/*samples/*}/scripts/wait.sh
 RC=0
 
 wait_log target/native/test-output.txt "Started SecurityWebfluxApplication" || RC=$?
-wait_http localhost:8080/hello "Unauthorized" || RC=$?
+wait_command_output 'curl -I  localhost:8080/hello' "Unauthorized" || RC=$?
 wait_http user:password@localhost:8080/hello "Hello!" || RC=$?
-wait_http user:password@localhost:8080/admin "Forbidden" || RC=$?
+wait_command_output 'curl -I user:password@localhost:8080/admin' "Forbidden" || RC=$?
 wait_http admin:password@localhost:8080/admin "Welcome to the administrator page!" || RC=$?
 
 exit $RC
