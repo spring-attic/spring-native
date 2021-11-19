fe099831cf2132b1b39570b3d1d4aa5f08cb2eef
Sébastien Deleuze <sdeleuze@vmware.com>
2021-11-19 10:58:09 +0100

    Fix verify scripts for security-method-* samples

--- samples/security-method/verify.sh
+++ samples/security-method/verify.sh
@@ -3,8 +3,8 @@ source ${PWD%/*samples/*}/scripts/wait.sh
 RC=0
 
 wait_log target/native/test-output.txt "Started MethodSecurityApplication" || RC=$?
-wait_http localhost:8080/hello "Unauthorized" || RC=$?
-wait_http localhost:8080/admin/private "Unauthorized" || RC=$?
+wait_command_output "curl -I localhost:8080/hello" "HTTP/1.1 401" || RC=$?
+wait_command_output "curl -I localhost:8080/admin/private" "HTTP/1.1 401" || RC=$?
 wait_http user:password@localhost:8080/hello "Hello!" || RC=$?
 wait_http admin:password@localhost:8080/admin/hello "Goodbye!" || RC=$?
 wait_http admin:password@localhost:8080/admin/private "bye" || RC=$?
