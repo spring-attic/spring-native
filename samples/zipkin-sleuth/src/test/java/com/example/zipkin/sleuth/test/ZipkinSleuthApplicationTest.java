ac5218ae179795ed5da5f6fb056afbc3678ab816
Olga MaciaszekSharma <olga.maciaszek@gmail.com>
2021-12-01 16:37:16 +0100

    Add context tests for ZipkinSleuthApplication.

--- samples/zipkin-sleuth/src/test/java/com/example/zipkin/sleuth/test/ZipkinSleuthApplicationTest.java
+++ samples/zipkin-sleuth/src/test/java/com/example/zipkin/sleuth/test/ZipkinSleuthApplicationTest.java
@@ -2,13 +2,36 @@ package com.example.zipkin.sleuth.test;
 
 import org.junit.jupiter.api.Test;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.sleuth.autoconfig.SleuthAnnotationConfiguration;
+import org.springframework.cloud.sleuth.autoconfig.TraceConfiguration;
+import org.springframework.cloud.sleuth.autoconfig.brave.BraveAutoConfiguration;
+import org.springframework.cloud.sleuth.autoconfig.brave.SleuthProperties;
+import org.springframework.cloud.sleuth.autoconfig.zipkin2.ZipkinAutoConfiguration;
+import org.springframework.cloud.sleuth.zipkin2.ZipkinProperties;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 public class ZipkinSleuthApplicationTest {
 
+	@Autowired
+	ApplicationContext applicationContext;
+
 	@Test
 	public void contextLoads() {
+		assertThat(applicationContext.getBeansOfType(ZipkinAutoConfiguration.class))
+				.isNotNull();
+		assertThat(applicationContext.getBeansOfType(ZipkinProperties.class)).isNotNull();
+		assertThat(applicationContext.getBeansOfType(TraceConfiguration.class))
+				.isNotNull();
+		assertThat(applicationContext.getBeansOfType(SleuthAnnotationConfiguration.class))
+				.isNotNull();
+		assertThat(applicationContext.getBeansOfType(SleuthProperties.class)).isNotNull();
+		assertThat(applicationContext.getBeansOfType(BraveAutoConfiguration.class))
+				.isNotNull();
 	}
 
 }
