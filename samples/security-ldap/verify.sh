3ecba5879e002e915d6d27a6c3e09c81909f4b7f
Eleftheria Stein <estein@vmware.com>
2021-12-01 17:12:59 +0100

    Fix LDAP sample verification

--- samples/security-ldap/verify.sh
+++ samples/security-ldap/verify.sh
@@ -3,7 +3,7 @@ source ${PWD%/*samples/*}/scripts/wait.sh
 RC=0
 
 wait_log target/native/test-output.txt "Started SecurityLdapApplication" || RC=$?
-wait_http localhost:8080/ "Unauthorized" || RC=$?
+wait_command_output 'curl -I  localhost:8080/' "401" || RC=$?
 wait_http user:password@localhost:8080/ "Hello, user!" || RC=$?
 wait_http user:password@localhost:8080/friendly "Hello, Dianne Emu!" || RC=$?
 
