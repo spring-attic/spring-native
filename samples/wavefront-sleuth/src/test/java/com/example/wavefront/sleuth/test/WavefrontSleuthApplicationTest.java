13da39b8a981df945675cb641030ccc17f00a690
Stephane Nicoll <snicoll@vmware.com>
2021-12-02 12:09:16 +0100

    Polish

--- samples/wavefront-sleuth/src/test/java/com/example/wavefront/sleuth/test/WavefrontSleuthApplicationTest.java
+++ samples/wavefront-sleuth/src/test/java/com/example/wavefront/sleuth/test/WavefrontSleuthApplicationTest.java
@@ -1,9 +1,25 @@
+/*
+ * Copyright 2019-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.wavefront.sleuth.test;
 
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.assertj.AssertableApplicationContext;
 import org.springframework.cloud.sleuth.autoconfig.TraceConfiguration;
 import org.springframework.cloud.sleuth.autoconfig.brave.SleuthPropagationProperties;
 import org.springframework.cloud.sleuth.autoconfig.brave.SleuthProperties;
@@ -11,31 +27,26 @@ import org.springframework.cloud.sleuth.autoconfig.brave.instrument.web.BraveHtt
 import org.springframework.cloud.sleuth.autoconfig.instrument.messaging.SleuthMessagingProperties;
 import org.springframework.cloud.sleuth.autoconfig.instrument.web.TraceWebAutoConfiguration;
 import org.springframework.cloud.sleuth.autoconfig.instrument.web.client.TraceWebAsyncClientAutoConfiguration;
-import org.springframework.context.ApplicationContext;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.context.ConfigurableApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@TestPropertySource(properties = "management.metrics.export.wavefront.enabled=false")
 @SpringBootTest
 public class WavefrontSleuthApplicationTest {
 
 	@Autowired
-	ApplicationContext applicationContext;
+	ConfigurableApplicationContext applicationContext;
 
 	@Test
 	public void contextLoads() {
-		assertThat(applicationContext.getBean(BraveHttpConfiguration.class)).isNotNull();
-		assertThat(applicationContext.getBean(TraceConfiguration.class)).isNotNull();
-		assertThat(applicationContext.getBean(SleuthPropagationProperties.class))
-				.isNotNull();
-		assertThat(applicationContext.getBean(SleuthProperties.class)).isNotNull();
-		assertThat(applicationContext.getBean(SleuthMessagingProperties.class))
-				.isNotNull();
-		assertThat(applicationContext.getBean(TraceWebAsyncClientAutoConfiguration.class))
-				.isNotNull();
-		assertThat(applicationContext.getBean(TraceWebAutoConfiguration.class))
-				.isNotNull();
+		AssertableApplicationContext context = AssertableApplicationContext.get(() -> applicationContext);
+		assertThat(context).hasSingleBean(BraveHttpConfiguration.class)
+				.hasSingleBean(TraceConfiguration.class)
+				.hasSingleBean(SleuthPropagationProperties.class)
+				.hasSingleBean(SleuthProperties.class)
+				.hasSingleBean(SleuthMessagingProperties.class)
+				.hasSingleBean(TraceWebAsyncClientAutoConfiguration.class)
+				.hasSingleBean(TraceWebAutoConfiguration.class);
 	}
 
 }
