9a9c4cabbd64e80c9ce936ff615da16024f3af63
Sam Brannen <sbrannen@vmware.com>
2021-12-04 17:40:51 +0100

    Polishing

--- samples/spring-tcf/verify.sh
+++ samples/spring-tcf/verify.sh
@@ -2,7 +2,4 @@
 source ${PWD%/*samples/*}/scripts/wait.sh
 RC=0
 
-wait_log target/native/test-output.txt "commandlinerunner running!" || RC=$?
-wait_log target/native/test-output.txt "ApplicationContextAware callback invoked" || RC=$?
-
 exit $RC
