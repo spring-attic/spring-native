c61da657af4e4dd5a08aec98404c9afe1105eed3
Sébastien Deleuze <sdeleuze@vmware.com>
2021-12-06 17:46:52 +0100

    Use JavaVersion.VERSION_11 in build.gradle.kts

--- samples/security-kotlin/build.gradle.kts
+++ samples/security-kotlin/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_1_8
+java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
     mavenLocal()
