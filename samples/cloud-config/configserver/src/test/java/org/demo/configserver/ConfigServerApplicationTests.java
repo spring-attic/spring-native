70496eeb73dad385f725702ff73917ed5883cfbc
Stephane Nicoll <snicoll@vmware.com>
2021-12-10 07:04:19 +0100

    Polish "Use AssertableApplicationContext in Spring Cloud sample tests"
    See gh-1362

--- samples/cloud-config/configserver/src/test/java/org/demo/configserver/ConfigServerApplicationTests.java
+++ samples/cloud-config/configserver/src/test/java/org/demo/configserver/ConfigServerApplicationTests.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.demo.configserver;
 
 import org.junit.jupiter.api.Test;
@@ -29,13 +45,13 @@ class ConfigServerApplicationTests {
 	void shouldLoadContext() {
 		AssertableApplicationContext context = AssertableApplicationContext
 				.get(() -> applicationContext);
-		assertThat(context).hasSingleBean(ConfigServerConfiguration.class);
-		assertThat(context).hasSingleBean(CompositeConfiguration.class);
-		assertThat(context).hasSingleBean(ResourceRepositoryConfiguration.class);
-		assertThat(context).hasSingleBean(ConfigServerEncryptionConfiguration.class);
-		assertThat(context).hasSingleBean(ConfigServerMvcConfiguration.class);
-		assertThat(context).hasSingleBean(ConfigServerAutoConfiguration.class);
-		assertThat(context).hasSingleBean(ConfigServerProperties.class);
+		assertThat(context).hasSingleBean(ConfigServerConfiguration.class)
+				.hasSingleBean(CompositeConfiguration.class)
+				.hasSingleBean(ResourceRepositoryConfiguration.class)
+				.hasSingleBean(ConfigServerEncryptionConfiguration.class)
+				.hasSingleBean(ConfigServerMvcConfiguration.class)
+				.hasSingleBean(ConfigServerAutoConfiguration.class)
+				.hasSingleBean(ConfigServerProperties.class);
 	}
 
 }
--- samples/zipkin-sleuth/src/test/java/com/example/zipkin/sleuth/test/ZipkinSleuthApplicationTest.java
+++ samples/zipkin-sleuth/src/test/java/com/example/zipkin/sleuth/test/ZipkinSleuthApplicationTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.zipkin.sleuth.test;
 
 import org.junit.jupiter.api.Test;
@@ -25,12 +41,12 @@ public class ZipkinSleuthApplicationTest {
 	public void contextLoads() {
 		AssertableApplicationContext context = AssertableApplicationContext
 				.get(() -> applicationContext);
-		assertThat(context).hasSingleBean(ZipkinAutoConfiguration.class);
-		assertThat(context).hasSingleBean(ZipkinProperties.class);
-		assertThat(context).hasSingleBean(TraceConfiguration.class);
-		assertThat(context).hasSingleBean(SleuthAnnotationConfiguration.class);
-		assertThat(context).hasSingleBean(SleuthProperties.class);
-		assertThat(context).hasSingleBean(BraveAutoConfiguration.class);
+		assertThat(context).hasSingleBean(ZipkinAutoConfiguration.class)
+				.hasSingleBean(ZipkinProperties.class)
+				.hasSingleBean(TraceConfiguration.class)
+				.hasSingleBean(SleuthAnnotationConfiguration.class)
+				.hasSingleBean(SleuthProperties.class)
+				.hasSingleBean(BraveAutoConfiguration.class);
 	}
 
 }
