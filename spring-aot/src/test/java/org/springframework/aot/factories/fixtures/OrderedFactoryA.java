10e59db2262ae647f065431a9a5022b9bc48fc65
Tadaya Tsuyukubo <tadaya@ttddyy.net>
2022-01-04 19:45:25 -0800

    Sort the factories in "spring.factories"
    The Spring Framework considers the `@Order` and `Ordered` in factory classes in
    `spring.factories`.
    
    This commit applies the same ordering algorithm to the `SpringFactory`
    instances; so that, the generated `StaticSpringFactories` holds the proper
    orderings.
    
    Signed-off-by: Tadaya Tsuyukubo <tadaya@ttddyy.net>

---
+++ spring-aot/src/test/java/org/springframework/aot/factories/fixtures/OrderedFactoryA.java
@@ -0,0 +1,8 @@
+package org.springframework.aot.factories.fixtures;
+
+import org.springframework.core.annotation.Order;
+
+@Order(20)
+public class OrderedFactoryA implements OrderedFactory {
+
+}
---
+++ spring-aot/src/test/java/org/springframework/aot/factories/fixtures/OrderedFactoryC.java
@@ -0,0 +1,8 @@
+package org.springframework.aot.factories.fixtures;
+
+import org.springframework.core.annotation.Order;
+
+@Order(10)
+public class OrderedFactoryC implements OrderedFactory {
+
+}
