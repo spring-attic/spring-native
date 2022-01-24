b61c258fdb027c5d9625e539b224627a60131e9b
Stephane Nicoll <snicoll@vmware.com>
2022-01-24 13:51:11 +0100

    Revert manual hint
    This hint is no longer required now that gh-1459 has been reverted.

--- spring-native-configuration/src/main/java/org/springframework/boot/SpringBootHints.java
+++ spring-native-configuration/src/main/java/org/springframework/boot/SpringBootHints.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2021 the original author or authors.
+ * Copyright 2019-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -51,7 +51,6 @@ import org.springframework.nativex.type.NativeConfiguration;
 			})
 	},
 	types = {
-		@TypeHint(typeNames = "org.springframework.boot.context.properties.ConfigurationPropertiesBinder$Factory"),
 		@TypeHint(types = {
 			LogManager.class,
 			JavaLoggingSystem.class
