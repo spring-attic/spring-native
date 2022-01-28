1fcd49b66323e2c4a3ac3ef115d1246e4a4ad541
Stephane Nicoll <snicoll@vmware.com>
2022-01-28 09:22:37 +0100

    Polish formatting for field injection

--- spring-aot/src/main/java/org/springframework/aot/context/bootstrap/generator/bean/InjectionPointWriter.java
+++ spring-aot/src/main/java/org/springframework/aot/context/bootstrap/generator/bean/InjectionPointWriter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2021 the original author or authors.
+ * Copyright 2019-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -209,7 +209,7 @@ class InjectionPointWriter {
 		else {
 			code.add(" bean.$L = attributes.get(0)", injectionPoint.getName());
 		}
-		code.add(")");
+		code.add(")").indent().indent();
 		return code.build();
 	}
 
