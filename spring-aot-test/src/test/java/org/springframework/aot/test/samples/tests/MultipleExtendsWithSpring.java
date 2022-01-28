00dcce1af984e5c38bd68adf1140a1339a80e9bb
GregoireW <24318548+GregoireW@users.noreply.github.com>
2022-01-28 16:51:26 +0100

    Filter in test with multiple Extend or Nested
    Signed-off-by: GregoireW <24318548+GregoireW@users.noreply.github.com>

---
+++ spring-aot-test/src/test/java/org/springframework/aot/test/samples/tests/MultipleExtendsWithSpring.java
@@ -0,0 +1,10 @@
+package org.springframework.aot.test.samples.tests;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.system.OutputCaptureExtension;
+
+@SpringBootTest
+@ExtendWith(OutputCaptureExtension.class)
+public class MultipleExtendsWithSpring {
+}
