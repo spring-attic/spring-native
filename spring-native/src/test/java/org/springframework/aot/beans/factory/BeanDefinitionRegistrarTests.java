8f88a0bff4fe09a0f1742ef78fa6d344218f01e7
Stephane Nicoll <snicoll@vmware.com>
2022-02-04 14:50:14 +0100

    Polish

--- spring-native/src/test/java/org/springframework/aot/beans/factory/BeanDefinitionRegistrarTests.java
+++ spring-native/src/test/java/org/springframework/aot/beans/factory/BeanDefinitionRegistrarTests.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.aot.beans.factory;
 
 import java.io.IOException;
@@ -310,13 +326,11 @@ class BeanDefinitionRegistrarTests {
 
 	@Test
 	void registerWithInvalidMethod() {
-		GenericApplicationContext context = new GenericApplicationContext();
-		DefaultListableBeanFactory beanFactory = context.getDefaultListableBeanFactory();
+		DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
 		assertThatThrownBy(() -> {
 					BeanDefinitionRegistrar.of("test", InjectionSample.class).instanceSupplier((instanceContext) ->
 							instanceContext.method("setEnvironment", Object.class).resolve(beanFactory)).register(beanFactory);
-					context.refresh();
-					context.getBean(ConstructorSample.class);
+					beanFactory.getBean(InjectionSample.class);
 				}
 		).isInstanceOf(BeanCreationException.class)
 				.hasMessageContaining("No method '%s' with type(s) [%s] found", "setEnvironment", Object.class.getName())
