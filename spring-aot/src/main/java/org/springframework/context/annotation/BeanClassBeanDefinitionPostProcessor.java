e184df910c9045104fa6c4681718dd5e082a9fa1
Stephane Nicoll <snicoll@vmware.com>
2022-02-08 16:46:45 +0100

    Resolve bean class of inner beans
    This commit updates BeanClassBeanDefinitionPostProcessor so that the
    bean class of inner bean definitions are resolved upfront as well.
    
    Closes gh-1481

--- spring-aot/src/main/java/org/springframework/context/annotation/BeanClassBeanDefinitionPostProcessor.java
+++ spring-aot/src/main/java/org/springframework/context/annotation/BeanClassBeanDefinitionPostProcessor.java
@@ -1,15 +1,34 @@
+/*
+ * Copyright 2019-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.context.annotation;
 
 import org.springframework.beans.BeansException;
+import org.springframework.beans.PropertyValue;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.BeanFactoryAware;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.beans.factory.config.ConstructorArgumentValues.ValueHolder;
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
 import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 
 /**
- * Ensure that {@link RootBeanDefinition#hasBeanClass()} can be safely used by bean
+ * Ensure that {@link AbstractBeanDefinition#hasBeanClass()} can be safely used by bean
  * definition processors.
  *
  * @author Stephane Nicoll
@@ -26,6 +45,22 @@ class BeanClassBeanDefinitionPostProcessor implements BeanDefinitionPostProcesso
 
 	@Override
 	public void postProcessBeanDefinition(String beanName, RootBeanDefinition beanDefinition) {
+		safeResolveBeanClas(beanDefinition);
+		for (PropertyValue propertyValue : beanDefinition.getPropertyValues().getPropertyValueList()) {
+			Object value = propertyValue.getValue();
+			if (value instanceof AbstractBeanDefinition) {
+				safeResolveBeanClas((AbstractBeanDefinition) value);
+			}
+		}
+		for (ValueHolder valueHolder : beanDefinition.getConstructorArgumentValues().getIndexedArgumentValues().values()) {
+			Object value = valueHolder.getValue();
+			if (value instanceof AbstractBeanDefinition) {
+				safeResolveBeanClas((AbstractBeanDefinition) value);
+			}
+		}
+	}
+
+	private void safeResolveBeanClas(AbstractBeanDefinition beanDefinition) {
 		if (!beanDefinition.hasBeanClass()) {
 			try {
 				beanDefinition.resolveBeanClass(this.classLoader);
--- spring-aot/src/test/java/org/springframework/context/annotation/BeanClassBeanDefinitionPostProcessorTests.java
+++ spring-aot/src/test/java/org/springframework/context/annotation/BeanClassBeanDefinitionPostProcessorTests.java
@@ -1,8 +1,27 @@
+/*
+ * Copyright 2019-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.context.annotation;
 
 import org.junit.jupiter.api.Test;
 
+import org.springframework.beans.MutablePropertyValues;
+import org.springframework.beans.factory.config.ConstructorArgumentValues;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.support.GenericBeanDefinition;
 import org.springframework.beans.factory.support.RootBeanDefinition;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -31,8 +50,12 @@ class BeanClassBeanDefinitionPostProcessorTests {
 	void postProcessBeanDefinitionWithBeanClassIsIgnored() {
 		RootBeanDefinition beanDefinition = mock(RootBeanDefinition.class);
 		given(beanDefinition.hasBeanClass()).willReturn(true);
+		given(beanDefinition.getPropertyValues()).willReturn(new MutablePropertyValues());
+		given(beanDefinition.getConstructorArgumentValues()).willReturn(new ConstructorArgumentValues());
 		postProcess(beanDefinition);
 		verify(beanDefinition).hasBeanClass();
+		verify(beanDefinition).getPropertyValues();
+		verify(beanDefinition).getConstructorArgumentValues();
 		verifyNoMoreInteractions(beanDefinition);
 	}
 
@@ -41,6 +64,33 @@ class BeanClassBeanDefinitionPostProcessorTests {
 		RootBeanDefinition beanDefinition = new RootBeanDefinition("com.example.DoesNotExist");
 		postProcess(beanDefinition);
 		assertThat(beanDefinition.hasBeanClass()).isFalse();
+	}
+
+	@Test
+	void postProcessInnerBeanDefinitionProperty() {
+		RootBeanDefinition beanDefinition = new RootBeanDefinition(String.class);
+		GenericBeanDefinition innerBeanDefinition = new GenericBeanDefinition();
+		innerBeanDefinition.setBeanClassName("java.lang.Integer");
+		assertThat(innerBeanDefinition.hasBeanClass()).isFalse();
+		beanDefinition.getPropertyValues().addPropertyValue("counter", innerBeanDefinition);
+		postProcess(beanDefinition);
+		assertThat(beanDefinition.getPropertyValues().get("counter")).isEqualTo(innerBeanDefinition);
+		assertThat(innerBeanDefinition.hasBeanClass()).isTrue();
+		assertThat(innerBeanDefinition.getBeanClass()).isEqualTo(Integer.class);
+	}
+
+	@Test
+	void postProcessInnerBeanDefinitionArgument() {
+		RootBeanDefinition beanDefinition = new RootBeanDefinition(String.class);
+		GenericBeanDefinition innerBeanDefinition = new GenericBeanDefinition();
+		innerBeanDefinition.setBeanClassName("java.lang.Integer");
+		assertThat(innerBeanDefinition.hasBeanClass()).isFalse();
+		beanDefinition.getConstructorArgumentValues().addIndexedArgumentValue(0, innerBeanDefinition);
+		postProcess(beanDefinition);
+		assertThat(beanDefinition.getConstructorArgumentValues().getIndexedArgumentValues().get(0).getValue())
+				.isEqualTo(innerBeanDefinition);
+		assertThat(innerBeanDefinition.hasBeanClass()).isTrue();
+		assertThat(innerBeanDefinition.getBeanClass()).isEqualTo(Integer.class);
 	}
 
 	private void postProcess(RootBeanDefinition beanDefinition) {
