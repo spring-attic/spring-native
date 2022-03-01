5bc60e75866d546bdccd0f44956c46476b91a6bd
Sébastien Deleuze <sdeleuze@vmware.com>
2022-03-01 13:02:42 +0100

    Update version to 0.11.4-SNAPSHOT

--- samples/webmvc-kotlin/build.gradle.kts
+++ samples/webmvc-kotlin/build.gradle.kts
@@ -4,7 +4,7 @@ import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 plugins {
     id("org.springframework.boot") version "2.6.4"
     id("io.spring.dependency-management") version "1.0.10.RELEASE"
-    id("org.springframework.experimental.aot") version "0.11.3-SNAPSHOT"
+    id("org.springframework.experimental.aot") version "0.11.4-SNAPSHOT"
     kotlin("jvm") version "1.6.10"
     kotlin("plugin.serialization") version "1.6.10"
     kotlin("plugin.spring") version "1.6.10"
