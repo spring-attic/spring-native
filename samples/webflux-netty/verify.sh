d1c1f0d4833e0ae77c697628ad498188e3ef9724
Sébastien Deleuze <sdeleuze@vmware.com>
2022-04-05 14:39:45 +0200

    Revert bogus change in webflux-netty
    See gh-1539

--- samples/webflux-netty/verify.sh
+++ samples/webflux-netty/verify.sh
@@ -2,6 +2,8 @@
 source ${PWD%/*samples/*}/scripts/wait.sh
 RC=0
 
-wait_http localhost:8080/ "session" || RC=$?
+wait_http localhost:8080/ "hi!" || RC=$?
+wait_http localhost:8080/x "hix!" || RC=$?
+wait_http localhost:8080/hello "World" || RC=$?
 
 exit $RC
