5057a3d3d82b1acfd14703364cb43794ec4cab91
Sébastien Deleuze <sdeleuze@vmware.com>
2022-04-19 10:06:26 +0200

    Remove unneeded AOT proxy hint
    Closes gh-459

--- samples/batch-io/src/main/java/com/example/batch/BatchApplication.java
+++ samples/batch-io/src/main/java/com/example/batch/BatchApplication.java
@@ -1,25 +1,15 @@
 package com.example.batch;
 
-import java.io.Serializable;
-
 import com.example.batch.domain.Person;
 
-import org.springframework.aop.framework.AopInfrastructureBean;
-import org.springframework.aop.scope.ScopedObject;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.nativex.hint.AotProxyHint;
 import org.springframework.nativex.hint.TypeAccess;
 import org.springframework.nativex.hint.TypeHint;
 
 // TODO Infer those hints
 @TypeHint(types = Person.class, access = { TypeAccess.DECLARED_CONSTRUCTORS, TypeAccess.PUBLIC_METHODS })
-@AotProxyHint(targetClass = ItemReaderListener.class, interfaces= {
-		ScopedObject.class,
-		Serializable.class,
-		AopInfrastructureBean.class
-})
 @EnableBatchProcessing
 @SpringBootApplication
 public class BatchApplication {
