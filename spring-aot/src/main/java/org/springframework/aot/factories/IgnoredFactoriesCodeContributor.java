8fecd2f40cb2493b70039421ced0171e489668d1
Stephane Nicoll <snicoll@vmware.com>
2022-04-25 10:47:27 +0200

    Ignore Spring Boot's ApplicationContextFactory entries
    Closes gh-1593

--- spring-aot/src/main/java/org/springframework/aot/factories/IgnoredFactoriesCodeContributor.java
+++ spring-aot/src/main/java/org/springframework/aot/factories/IgnoredFactoriesCodeContributor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2021 the original author or authors.
+ * Copyright 2019-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -40,6 +40,7 @@ class IgnoredFactoriesCodeContributor implements FactoriesCodeContributor {
 			"org.springframework.aot.test.context.bootstrap");
 
 	private static final Predicate<SpringFactory> IGNORED_FACTORY_TYPES = factoryTypes(
+			"org.springframework.boot.ApplicationContextFactory",
 			"org.springframework.boot.autoconfigure.AutoConfigurationImportListener",
 			"org.springframework.boot.autoconfigure.AutoConfigurationImportFilter",
 			"org.springframework.boot.autoconfigure.EnableAutoConfiguration",
