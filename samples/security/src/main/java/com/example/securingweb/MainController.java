96e4b363b0e0747c343b43f606c086fd688db5c9
Moritz Halbritter <mkammerer@vmware.com>
2022-06-08 16:03:33 +0200

    Adapt security sample to Spring Boot 3.0

--- samples/security/src/main/java/com/example/securingweb/MainController.java
+++ samples/security/src/main/java/com/example/securingweb/MainController.java
@@ -1,7 +1,12 @@
 package com.example.securingweb;
 
+import java.util.Map;
+
+import jakarta.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
@@ -15,10 +20,10 @@ public class MainController {
         return "home";
     }
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "hello";
-    }
+	@GetMapping("/hello")
+	public ModelAndView hello(HttpServletRequest httpServletRequest) {
+		return new ModelAndView("hello", Map.of("user", httpServletRequest.getRemoteUser()));
+	}
 
     @GetMapping("/login")
     public String login() {
--- samples/security/src/main/java/com/example/securingweb/WebSecurityConfig.java
+++ samples/security/src/main/java/com/example/securingweb/WebSecurityConfig.java
@@ -4,47 +4,46 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-			.authorizeRequests(authorize -> authorize
-				.antMatchers("/", "/home").permitAll()
-				.antMatchers("/admin").hasRole("ADMIN")
-				.anyRequest().authenticated()
-			)
-			.formLogin(formLogin -> formLogin
-				.loginPage("/login")
-				.permitAll()
-			)
-			.logout(logout -> logout
-				.permitAll()
-			);
+public class WebSecurityConfig {
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		return http
+				.authorizeRequests(authorize -> authorize
+						.antMatchers("/", "/home").permitAll()
+						.antMatchers("/admin").hasRole("ADMIN")
+						.anyRequest().authenticated()
+				)
+				.formLogin(formLogin -> formLogin
+						.loginPage("/login")
+						.permitAll()
+				)
+				.logout(LogoutConfigurer::permitAll)
+				.build();
 	}
 
 	@Bean
-	@Override
 	public UserDetailsService userDetailsService() {
 		UserDetails user =
-			 User.withDefaultPasswordEncoder()
-				.username("user")
-				.password("password")
-				.roles("USER")
-				.build();
+				User.withDefaultPasswordEncoder()
+						.username("user")
+						.password("password")
+						.roles("USER")
+						.build();
 		UserDetails admin =
-			 User.withDefaultPasswordEncoder()
-				.username("admin")
-				.password("password")
-				.roles("ADMIN")
-				.build();
+				User.withDefaultPasswordEncoder()
+						.username("admin")
+						.password("password")
+						.roles("ADMIN")
+						.build();
 
 		return new InMemoryUserDetailsManager(user, admin);
 	}
