58811aec4e2c85a12465c60a46ac4b041b6eb367
Moritz Halbritter <mkammerer@vmware.com>
2022-06-08 16:22:39 +0200

    Adapt security-kotlin sample to Spring Boot 3.0

--- samples/security-kotlin/src/main/kotlin/com/example/kotlinSecurity/SecurityConfig.kt
+++ samples/security-kotlin/src/main/kotlin/com/example/kotlinSecurity/SecurityConfig.kt
@@ -1,10 +1,8 @@
 package com.example.kotlinSecurity
 
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.web.servlet.invoke
 import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.provisioning.InMemoryUserDetailsManager
@@ -13,26 +11,22 @@ import org.springframework.security.web.SecurityFilterChain
 @EnableWebSecurity
 class SecurityConfig {
 
-    @Bean
-    fun filterChain(http: HttpSecurity): SecurityFilterChain {
-        http {
-            authorizeRequests {
-                authorize(anyRequest, authenticated)
-            }
-            formLogin { }
-            httpBasic { }
-        }
-        return http.build()
-    }
+	@Bean
+	fun filterChain(http: HttpSecurity): SecurityFilterChain {
+		return http.authorizeRequests().anyRequest().authenticated()
+			.and().formLogin()
+			.and().httpBasic()
+			.and().build()
+	}
 
-    @Suppress("DEPRECATION")
-    @Bean
-    fun userDetailsService(): UserDetailsService {
-        val user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
-                .roles("USER")
-                .build()
-        return InMemoryUserDetailsManager(user)
-    }
+	@Suppress("DEPRECATION")
+	@Bean
+	fun userDetailsService(): UserDetailsService {
+		val user = User.withDefaultPasswordEncoder()
+			.username("user")
+			.password("password")
+			.roles("USER")
+			.build()
+		return InMemoryUserDetailsManager(user)
+	}
 }
