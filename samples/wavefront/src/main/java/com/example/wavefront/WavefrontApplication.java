9007de338926258e170c431f397a0fe56b38e7a7
Moritz Halbritter <mkammerer@vmware.com>
2022-06-13 15:45:55 +0200

    Fix compile error with newest SB 3.x

--- samples/wavefront/src/main/java/com/example/wavefront/WavefrontApplication.java
+++ samples/wavefront/src/main/java/com/example/wavefront/WavefrontApplication.java
@@ -1,10 +1,9 @@
 package com.example.wavefront;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.wavefront.WavefrontAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = WavefrontAutoConfiguration.class)
+@SpringBootApplication
 public class WavefrontApplication {
 
 	public static void main(String[] args) {
