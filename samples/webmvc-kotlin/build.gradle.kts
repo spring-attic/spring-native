51bc8d08af5b156926bb77859a789d72808040ed
Moritz Halbritter <mkammerer@vmware.com>
2022-06-23 09:21:51 +0200

    Build against Spring Boot 2.7.1
    Closes gh-1637

--- samples/webmvc-kotlin/build.gradle.kts
+++ samples/webmvc-kotlin/build.gradle.kts
@@ -2,8 +2,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 
 plugins {
-    id("org.springframework.boot") version "2.7.0"
-    id("io.spring.dependency-management") version "1.0.10.RELEASE"
+    id("org.springframework.boot") version "2.7.1"
+	id("io.spring.dependency-management") version "1.0.10.RELEASE"
     id("org.springframework.experimental.aot") version "0.12.1-SNAPSHOT"
     kotlin("jvm") version "1.6.10"
     kotlin("plugin.serialization") version "1.6.10"
