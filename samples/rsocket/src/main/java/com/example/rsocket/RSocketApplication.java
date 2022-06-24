53e36f8a86d0667168e7cadb96ac58ed2cad5e75
Moritz Halbritter <mkammerer@vmware.com>
2022-06-24 16:57:54 +0200

    Hacking

--- samples/rsocket/src/main/java/com/example/rsocket/RSocketApplication.java
+++ samples/rsocket/src/main/java/com/example/rsocket/RSocketApplication.java
@@ -1,12 +1,18 @@
 package com.example.rsocket;
 
+import io.rsocket.internal.UnboundedProcessor;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportRuntimeHints;
 
 @SpringBootApplication
+@ImportRuntimeHints(NettyRuntimeHints.class)
 public class RSocketApplication {
 
 	public static void main(String[] args) {
+		// TODO MH: Remove
+		new UnboundedProcessor();
 		SpringApplication.run(RSocketApplication.class, args);
 	}
 
