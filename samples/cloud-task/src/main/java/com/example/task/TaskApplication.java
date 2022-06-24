92fdaf996c06a6c86b58ac4b5438566865ccb219
Moritz Halbritter <mkammerer@vmware.com>
2022-06-24 10:07:19 +0200

    Add hikari runtime hints to cloud-task sample

--- samples/cloud-task/src/main/java/com/example/task/TaskApplication.java
+++ samples/cloud-task/src/main/java/com/example/task/TaskApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.task.configuration.EnableTask;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ImportRuntimeHints;
 
 @SpringBootApplication
 @EnableTask
+@ImportRuntimeHints(HikariRuntimeHintsRegistrar.class)
 public class TaskApplication {
 
 	@Bean
