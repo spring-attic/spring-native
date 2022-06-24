fc3516ed2fb4f09ca8c52dd50edd1cab87579579
Moritz Halbritter <mkammerer@vmware.com>
2022-06-24 10:18:22 +0200

    Add hikari runtime hints to events sample

--- samples/events/src/main/java/com/example/events/EventApplication.java
+++ samples/events/src/main/java/com/example/events/EventApplication.java
@@ -2,8 +2,10 @@ package com.example.events;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportRuntimeHints;
 
 @SpringBootApplication
+@ImportRuntimeHints(HikariRuntimeHintsRegistrar.class)
 public class EventApplication {
 	
 	public static void main(String[] args) throws InterruptedException {
