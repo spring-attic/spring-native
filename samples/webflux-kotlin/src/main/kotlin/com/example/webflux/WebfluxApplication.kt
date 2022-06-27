44bf40f94cc913a5ee3c0dafbdb8f83e2c718abd
Moritz Halbritter <mkammerer@vmware.com>
2022-06-27 11:43:26 +0200

    Add netty hints to webflux-kotlin sample

--- samples/webflux-kotlin/src/main/kotlin/com/example/webflux/WebfluxApplication.kt
+++ samples/webflux-kotlin/src/main/kotlin/com/example/webflux/WebfluxApplication.kt
@@ -1,9 +1,12 @@
 package com.example.webflux
 
+import org.springframework.aot.thirdpartyhints.NettyRuntimeHints
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ImportRuntimeHints
 
 @SpringBootApplication
+@ImportRuntimeHints(NettyRuntimeHints::class)
 class WebfluxApplication
 
 fun main(args: Array<String>) {
