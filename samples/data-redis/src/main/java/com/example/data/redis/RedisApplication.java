52107266a1a94c205b343ee06ff8ce0738832742
Moritz Halbritter <mkammerer@vmware.com>
2022-06-27 13:47:44 +0200

    Add netty hints to data-redis sample

--- samples/data-redis/src/main/java/com/example/data/redis/RedisApplication.java
+++ samples/data-redis/src/main/java/com/example/data/redis/RedisApplication.java
@@ -15,10 +15,13 @@
  */
 package com.example.data.redis;
 
+import org.springframework.aot.thirdpartyhints.NettyRuntimeHints;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportRuntimeHints;
 
 @SpringBootApplication
+@ImportRuntimeHints(NettyRuntimeHints.class)
 public class RedisApplication {
 
 	public static void main(String[] args) throws Exception {
