46b96120e2df466274ba13c7d9092fcf8bcda934
Moritz Halbritter <mkammerer@vmware.com>
2022-06-27 11:07:21 +0200

    Add netty hints to security-webflux sample

--- samples/security-webflux/src/main/java/com/example/webflux/SecurityWebfluxApplication.java
+++ samples/security-webflux/src/main/java/com/example/webflux/SecurityWebfluxApplication.java
@@ -1,11 +1,12 @@
 package com.example.webflux;
 
+import org.springframework.aot.thirdpartyhints.NettyRuntimeHints;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.ImportRuntimeHints;
 
-@SpringBootApplication	
+@SpringBootApplication
+@ImportRuntimeHints(NettyRuntimeHints.class)
 public class SecurityWebfluxApplication {
 
 	public static void main(String[] args) {
