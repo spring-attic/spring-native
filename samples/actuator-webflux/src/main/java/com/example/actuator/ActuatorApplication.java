2dd9fba60176d856eddd757a9c7d870cd70ec7bb
Moritz Halbritter <mkammerer@vmware.com>
2022-06-27 10:13:03 +0200

    Add netty hints to actuator-webflux sample

--- samples/actuator-webflux/src/main/java/com/example/actuator/ActuatorApplication.java
+++ samples/actuator-webflux/src/main/java/com/example/actuator/ActuatorApplication.java
@@ -1,9 +1,12 @@
 package com.example.actuator;
 
+import org.springframework.aot.thirdpartyhints.NettyRuntimeHints;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportRuntimeHints;
 
 @SpringBootApplication
+@ImportRuntimeHints(NettyRuntimeHints.class)
 public class ActuatorApplication {
 
 	public static void main(String[] args) {
