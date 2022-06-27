eb26d15f52eb6d2e54d426113ea98c31e12857e0
Moritz Halbritter <mkammerer@vmware.com>
2022-06-27 11:16:52 +0200

    Add netty hints to session-without-security sample

--- samples/session-without-security/src/main/java/com/example/demo/DemoApplication.java
+++ samples/session-without-security/src/main/java/com/example/demo/DemoApplication.java
@@ -6,12 +6,13 @@ import org.springframework.aot.hint.MemberCategory;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.aot.hint.TypeReference;
+import org.springframework.aot.thirdpartyhints.NettyRuntimeHints;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportRuntimeHints;
 
 @SpringBootApplication
-@ImportRuntimeHints(Registrar.class)
+@ImportRuntimeHints({ Registrar.class, NettyRuntimeHints.class })
 public class DemoApplication {
 
 	public static void main(String[] args) {
