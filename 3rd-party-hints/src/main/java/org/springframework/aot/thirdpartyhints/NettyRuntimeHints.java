8fdb2f6e83d257aabb90bf6993f5e0a821d342d8
Moritz Halbritter <mkammerer@vmware.com>
2022-06-27 11:10:55 +0200

    Add missing netty runtime hint

--- 3rd-party-hints/src/main/java/org/springframework/aot/thirdpartyhints/NettyRuntimeHints.java
+++ 3rd-party-hints/src/main/java/org/springframework/aot/thirdpartyhints/NettyRuntimeHints.java
@@ -114,6 +114,7 @@ public class NettyRuntimeHints implements RuntimeHintsRegistrar {
                 TypeReference.of("io.netty.util.internal.shaded.org.jctools.queues.SpscArrayQueueProducerIndexFields"),
                 hint -> hint.withMembers(MemberCategory.DECLARED_FIELDS));
         hints.reflection().registerType(TypeReference.of("io.netty.channel.socket.nio.NioSocketChannel"), hint -> hint.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS));
+        hints.reflection().registerType(TypeReference.of("io.netty.channel.socket.nio.NioDatagramChannel"), hint -> hint.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS));
     }
 
 }
