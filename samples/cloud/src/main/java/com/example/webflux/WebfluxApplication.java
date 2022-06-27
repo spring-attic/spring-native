9107f44cb50af2840e41449a9823df4bc9121214
Moritz Halbritter <mkammerer@vmware.com>
2022-06-27 10:21:54 +0200

    Add netty hints to cloud sample

--- samples/cloud/src/main/java/com/example/webflux/WebfluxApplication.java
+++ samples/cloud/src/main/java/com/example/webflux/WebfluxApplication.java
@@ -1,12 +1,15 @@
 package com.example.webflux;
 
+import org.springframework.aot.thirdpartyhints.NettyRuntimeHints;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportRuntimeHints;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@ImportRuntimeHints(NettyRuntimeHints.class)
 public class WebfluxApplication {
 
 	public static void main(String[] args) {
