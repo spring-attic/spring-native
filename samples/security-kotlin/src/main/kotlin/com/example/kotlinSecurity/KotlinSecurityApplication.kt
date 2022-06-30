a53f703b0bca2d43302ff597fc9af3160d971b5d
Moritz Halbritter <mkammerer@vmware.com>
2022-06-30 16:02:09 +0200

    Add kotlin runtime hints to security-kotlin sample

--- samples/security-kotlin/src/main/kotlin/com/example/kotlinSecurity/KotlinSecurityApplication.kt
+++ samples/security-kotlin/src/main/kotlin/com/example/kotlinSecurity/KotlinSecurityApplication.kt
@@ -1,9 +1,12 @@
 package com.example.kotlinSecurity
 
+import org.springframework.aot.thirdpartyhints.KotlinRuntimeHints
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ImportRuntimeHints
 
 @SpringBootApplication
+@ImportRuntimeHints(KotlinRuntimeHints::class)
 class KotlinSecurityApplication
 
 fun main(args: Array<String>) {
