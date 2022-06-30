8a7f82b8008f70295396419da0f7fc687e60206c
Sébastien Deleuze <sdeleuze@vmware.com>
2022-06-30 10:05:20 +0200

    Update for spring-projects/spring-framework#28701

--- samples/webflux-netty/src/main/java/com/example/webflux/WebfluxApplication.java
+++ samples/webflux-netty/src/main/java/com/example/webflux/WebfluxApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@ImportRuntimeHints({ NettyRuntimeHints.class, WebfluxRuntimeHints.class })
+@ImportRuntimeHints(NettyRuntimeHints.class)
 public class WebfluxApplication {
 
     public static void main(String[] args) {
