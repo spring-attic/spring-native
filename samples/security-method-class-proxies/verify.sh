28449651d0f1ea4c78658e3550c03bd2afbafc07
Marcus Da Coregio <marcusdacoregio@gmail.com>
2022-07-01 11:31:59 -0300

    Fix tests in security-method-class-proxies sample

--- samples/security-method-class-proxies/verify.sh
+++ samples/security-method-class-proxies/verify.sh
@@ -4,6 +4,7 @@ RC=0
 
 wait_log target/native/test-output.txt "Started MethodSecurityApplication" || RC=$?
 wait_command_output "curl -I localhost:8080/hello" "HTTP/1.1 401" || RC=$?
+wait_command_output "curl -I -u user:password localhost:8080/admin/hello" "HTTP/1.1 403" || RC=$?
 wait_http user:password@localhost:8080/hello "Hello!" || RC=$?
 wait_http admin:password@localhost:8080/admin/hello "Goodbye!" || RC=$?
 
