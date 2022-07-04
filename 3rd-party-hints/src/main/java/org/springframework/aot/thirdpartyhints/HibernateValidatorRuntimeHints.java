03a45ffe608aa8ffe8ebf9a7fead5d8fea355ab7
Moritz Halbritter <mkammerer@vmware.com>
2022-07-04 11:32:02 +0200

    Add hibernate validator 3rd party hints

---
+++ 3rd-party-hints/src/main/java/org/springframework/aot/thirdpartyhints/HibernateValidatorRuntimeHints.java
@@ -0,0 +1,24 @@
+package org.springframework.aot.thirdpartyhints;
+
+import org.springframework.aot.hint.MemberCategory;
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import org.springframework.aot.hint.TypeReference;
+import org.springframework.util.ClassUtils;
+
+/**
+ * @author Moritz Halbritter
+ */
+// TODO: Contribute these hints to graalvm-reachability-metadata repository
+public class HibernateValidatorRuntimeHints implements RuntimeHintsRegistrar {
+
+	@Override
+	public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+		if (!ClassUtils.isPresent("org.hibernate.validator.HibernateValidator", classLoader)) {
+			return;
+		}
+		hints.reflection().registerType(TypeReference.of("org.hibernate.validator.internal.util.logging.Log_$logger"),
+				hint -> hint.withMembers(MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS));
+	}
+
+}
