7d4b104c1b5813c4836fb0866563b8491bd9c25e
Marcus Da Coregio <marcusdacoregio@gmail.com>
2022-07-06 10:03:10 -0300

    Import thymeleaf hints in session-jdbc sample

--- samples/session-jdbc/src/main/java/com/example/session/SessionJdbcApplication.java
+++ samples/session-jdbc/src/main/java/com/example/session/SessionJdbcApplication.java
@@ -1,9 +1,12 @@
 package com.example.session;
 
+import org.springframework.aot.thirdpartyhints.ThymeleafRuntimeHints;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportRuntimeHints;
 
 @SpringBootApplication
+@ImportRuntimeHints({ThymeleafRuntimeHints.class})
 public class SessionJdbcApplication {
 
 	public static void main(String[] args) throws Throwable {
