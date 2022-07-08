d9fc726e1795da07ac9be677b714ecd0cdaf213d
Sébastien Deleuze <sdeleuze@vmware.com>
2022-07-08 16:51:25 +0200

    Fix jdbc-tx sample

--- samples/jdbc-tx/src/main/java/app/main/SampleApplication.java
+++ samples/jdbc-tx/src/main/java/app/main/SampleApplication.java
@@ -2,16 +2,24 @@ package app.main;
 
 import app.main.model.Foo;
 
+import org.springframework.aot.hint.MemberCategory;
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import org.springframework.aot.thirdpartyhints.HikariRuntimeHints;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportRuntimeHints;
 import org.springframework.web.servlet.function.RouterFunction;
 
+import static app.main.SampleApplication.*;
 import static org.springframework.web.servlet.function.RequestPredicates.GET;
 import static org.springframework.web.servlet.function.RouterFunctions.route;
 import static org.springframework.web.servlet.function.ServerResponse.ok;
 
 @SpringBootApplication
+@ImportRuntimeHints({ HikariRuntimeHints.class, Registrar.class })
 public class SampleApplication {
 
 	@Bean
@@ -21,6 +29,18 @@ public class SampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SampleApplication.class, args);
+	}
+
+	static class Registrar implements RuntimeHintsRegistrar {
+
+		@Override
+		public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+			// TODO https://github.com/spring-projects/spring-boot/issues/31533
+			hints.resources().registerPattern("schema.sql");
+
+			hints.reflection().registerType(Foo.class,
+					builder -> builder.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_DECLARED_METHODS));
+		}
 	}
 
 }
