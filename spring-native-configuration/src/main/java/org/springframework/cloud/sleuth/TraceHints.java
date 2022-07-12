bed1a657b43021f467190533a9174e4bc70d5bba
Moritz Halbritter <mkammerer@vmware.com>
2022-07-12 16:12:28 +0200

    Polish "Add hint for Sleuth R2dbcTransactionManager instrumentation"

--- spring-native-configuration/src/main/java/org/springframework/cloud/sleuth/TraceHints.java
+++ spring-native-configuration/src/main/java/org/springframework/cloud/sleuth/TraceHints.java
@@ -1,16 +1,18 @@
 package org.springframework.cloud.sleuth;
 
-import org.springframework.nativex.hint.AotProxyHint;
-import org.springframework.nativex.hint.ProxyBits;
 import zipkin2.reporter.AsyncReporter;
 
 import org.springframework.cloud.sleuth.autoconfig.brave.BraveAutoConfiguration;
+import org.springframework.cloud.sleuth.autoconfig.instrument.tx.TraceReactiveTransactionManagerBeanPostProcessor;
+import org.springframework.nativex.hint.AotProxyHint;
 import org.springframework.nativex.hint.JdkProxyHint;
 import org.springframework.nativex.hint.NativeHint;
+import org.springframework.nativex.hint.ProxyBits;
 import org.springframework.nativex.hint.TypeHint;
 import org.springframework.nativex.type.NativeConfiguration;
+import org.springframework.r2dbc.connection.R2dbcTransactionManager;
 
-@AotProxyHint(targetClass=org.springframework.r2dbc.connection.R2dbcTransactionManager.class, proxyFeatures = ProxyBits.IS_STATIC)
+@NativeHint(trigger = TraceReactiveTransactionManagerBeanPostProcessor.class, aotProxies = @AotProxyHint(targetClass = R2dbcTransactionManager.class, proxyFeatures = ProxyBits.IS_STATIC))
 @NativeHint(trigger = AsyncReporter.class, options = "--enable-url-protocols=http")
 @NativeHint(trigger = BraveAutoConfiguration.class, types = {
 		@TypeHint(typeNames = {"org.springframework.cloud.context.scope.GenericScope$LockedScopedProxyFactoryBean", "brave.kafka.clients.TracingProducer", "brave.kafka.clients.TracingConsumer"})
