84b99dc4963137547583671481dca4d0b8d5abef
Moritz Halbritter <mkammerer@vmware.com>
2022-07-13 14:03:10 +0200

    Move 'actuator-webmvc' sample to Spring AOT smoke tests

--- samples/actuator-webmvc/verify.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-source ${PWD%/*samples/*}/scripts/wait.sh
-RC=0
-
-wait_log target/native/test-output.txt "Started ActuatorApplication in" || RC=$?
-wait_http localhost:8080/actuator/health "UP" || RC=$?
-wait_http localhost:8080/actuator/health/readiness "customIndicator" || RC=$?
-
-exit $RC
