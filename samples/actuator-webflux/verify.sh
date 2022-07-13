f5726e83425472b6cfec38e117c720e888e6b9d1
Moritz Halbritter <mkammerer@vmware.com>
2022-07-13 14:02:34 +0200

    Move 'actuator-webflux' sample to Spring AOT smoke tests

--- samples/actuator-webflux/verify.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-source ${PWD%/*samples/*}/scripts/wait.sh
-RC=0
-
-wait_log target/native/test-output.txt "Started ActuatorApplication in" || RC=$?
-wait_http localhost:8080/actuator/health "UP" || RC=$?
-
-exit $RC
