d25a517306810c5eda53b0e3a63a37c46bc0cc4d
Moritz Halbritter <mkammerer@vmware.com>
2022-07-13 14:06:12 +0200

    Move 'scheduling-tasks' sample to Spring AOT smoke tests

--- samples/scheduling-tasks/verify.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-source ${PWD%/*samples/*}/scripts/wait.sh
-RC=0
-
-wait_log target/native/test-output.txt "The number is now 6" || RC=$?
-wait_log target/native/test-output.txt "The other number is now 6" || RC=$?
-
-exit $RC
