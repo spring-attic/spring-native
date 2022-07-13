800228de840c6a52c5abb6f5cfa5752ae4e087e5
Moritz Halbritter <mkammerer@vmware.com>
2022-07-13 14:04:09 +0200

    Move 'class-proxies-aop' sample to Spring AOT smoke tests

--- samples/class-proxies-aop/verify.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-source ${PWD%/*samples/*}/scripts/wait.sh
-RC=0
-
-wait_log target/native/test-output.txt "methodA: A-from-aspect" || RC=$?
-wait_log target/native/test-output.txt "methodB: B" || RC=$?
-
-exit $RC
