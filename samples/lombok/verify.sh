82f4525ced0055ee365d0a216186fffe4ee755b8
Moritz Halbritter <mkammerer@vmware.com>
2022-07-22 14:06:53 +0200

    Remove more obsolete samples
    The functionality of these samples is covered by other AOT smoke tests
    or the functionality tested is not from Spring projects.

--- samples/lombok/verify.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-source ${PWD%/*samples/*}/scripts/wait.sh
-RC=0
-
-wait_log target/native/test-output.txt "Hello, Apache Logging world!" || RC=$?
-wait_log target/native/test-output.txt "Hello, Slf4j world!" || RC=$?
-
-exit $RC
