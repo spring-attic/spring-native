ea9cc875de687e5bb53e2a3f959fd01b8cc6e6bd
Moritz Halbritter <mkammerer@vmware.com>
2022-07-22 09:57:23 +0200

    Remove more obsolete samples
    The functionality of these samples is covered by other AOT smoke tests
    or the functionality tested is not from Spring projects.

--- samples/grpc/verify.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-source ${PWD%/*samples/*}/scripts/wait.sh
-RC=0
-
-wait_command_output "grpcurl -plaintext localhost:50051 describe demo.Greeter 2>&1" "demo.Greeter is a service:" || RC=$?
-wait_command_output "grpcurl -plaintext -d '{}' localhost:50051 demo.Greeter/Hello 2>&1" "\"firstName\": \"Josh\"," || RC=$?
-
-exit $RC
