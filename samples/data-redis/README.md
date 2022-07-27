9d6c47193f683251feb0ef10cf8e38d4ee6ae02d
Moritz Halbritter <mkammerer@vmware.com>
2022-07-27 13:18:32 +0200

    Move 'data-redis' sample to Spring AOT smoke tests

--- samples/data-redis/README.md
+++ samples/data-redis/README.md
@@ -1,1 +1,2 @@
-Spring Boot project with Spring Data Redis.
+This sample has been migrated to
+the [Spring AOT Smoke Tests](https://github.com/spring-projects/spring-aot-smoke-tests/tree/main/data-redis)
--- samples/data-redis/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-RC=0
-
-docker-compose up -d
-${PWD%/*samples/*}/scripts/install3rdPartyHints.sh && ${PWD%/*samples/*}/scripts/compileWithMaven.sh $* &&  ${PWD%/*samples/*}/scripts/test.sh $* || RC=$?
-docker-compose down
-
-exit $RC
