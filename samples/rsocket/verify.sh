6913cbfdb6c2bdec51e75410f53e0a3ca437883a
Moritz Halbritter <mkammerer@vmware.com>
2022-07-28 11:58:15 +0200

    Move 'rsocket' sample to Spring AOT smoke tests

--- samples/rsocket/verify.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-source ${PWD%/*samples/*}/scripts/wait.sh
-RC=0
-
-wait_command_output 'rsc --debug --request --data "{\"origin\":\"Client\",\"interaction\":\"Request\"}" --route "request-response" tcp://localhost:7000' "Frame => Stream ID: 1 Type: NEXT_COMPLETE Flags: 0b1100000 Length: 81" || RC=$?
-wait_command_output 'rsc --debug --request --data "{\"origin\":\"Client\",\"interaction\":\"Request\"}" --route mono-request-response tcp://localhost:7000' "Frame => Stream ID: 1 Type: NEXT_COMPLETE Flags: 0b1100000 Length: 81" || RC=$?
-
-exit $RC
