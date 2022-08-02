1cf8826e3ff066b225250ec9ef3da051399d95d3
Moritz Halbritter <mkammerer@vmware.com>
2022-08-02 15:43:19 +0200

    Move 'zipkin' sample to Spring AOT smoke tests

--- samples/zipkin/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-RC=0
-
-docker-compose up -d
-${PWD%/*samples/*}/scripts/compileWithMaven.sh $* &&  ${PWD%/*samples/*}/scripts/test.sh $*
-docker-compose down
-
-exit $RC
--- samples/zipkin/verify.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-source ${PWD%/*samples/*}/scripts/wait.sh
-wait_log target/native/test-output.txt "Started ZipkinApplication"
-curl --fail -s localhost:8080/ -H "X-B3-TraceId: 1bf6ce8439eb15b8" -H "X-B3-SpanId: 1bf6ce8439eb15b8"
-sleep 5 # sending of spans is done asynchronously - will wait 5 second to be sure
-echo "Checking if the trace is in Zipkin"
-curl --fail http://localhost:9411/api/v2/trace/1bf6ce8439eb15b8
