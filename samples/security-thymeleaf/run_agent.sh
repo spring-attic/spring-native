eed81ddf95f4072a2f378f60063f2e192f0e3a81
Moritz Halbritter <mkammerer@vmware.com>
2022-08-03 10:13:04 +0200

    Move 'security-thymeleaf' sample to Spring AOT smoke tests

--- samples/security-thymeleaf/run_agent.sh
+++
@@ -1,1 +0,0 @@
-java -agentlib:native-image-agent=experimental-class-loader-support,config-output-dir=src/main/resources/META-INF/native-image -jar target/security-thymeleaf-0.0.1-SNAPSHOT.jar
--- samples/security-thymeleaf/verify.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-source ${PWD%/*samples/*}/scripts/wait.sh
-wait_log target/native/test-output.txt "Started SecurityThymeleafApplication"
-wait_http localhost:8080/ "Welcome!" || RC=$?
-wait_http user:password@localhost:8080/hello "Hello <span>user</span>" || RC=$?
-wait_command_output 'curl -s -I localhost:8080/hello' "HTTP/1.1 401" || RC=$?
-
-exit $RC
